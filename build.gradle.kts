b31cef68b56b002ad87f0a9689ea0634d3bc4ae7
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2022-01-07 13:40:15 +0000

    Swagger (#4)
    * wip
    
    * wip
    
    * wip
    
    * swagger and test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1-beta"
   kotlin("plugin.spring") version "1.6.0"
 }
 
@@ -9,6 +9,15 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  // implementation("org.springframework.boot:spring-boot-starter-security")
+  // implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  // implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.4")
+
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.0.29")
 }
 
 java {
