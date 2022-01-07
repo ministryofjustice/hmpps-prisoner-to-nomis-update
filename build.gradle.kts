271b5ceece3138aaf1483673cadaffb9c058448b
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2022-01-07 13:31:49 +0000

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
- // implementation("org.springframework.boot:spring-boot-starter-security")
- // implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
- // implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  // implementation("org.springframework.boot:spring-boot-starter-security")
+  // implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  // implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
