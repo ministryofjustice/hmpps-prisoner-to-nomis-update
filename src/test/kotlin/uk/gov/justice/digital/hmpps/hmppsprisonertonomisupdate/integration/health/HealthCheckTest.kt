2b597b484c31298b7a8663f1be10f20f1a36e004
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2022-01-07 16:46:28 +0000

    healthcheck and tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonertonomisupdate/integration/health/HealthCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonertonomisupdate/integration/health/HealthCheckTest.kt
@@ -13,6 +13,8 @@ class HealthCheckTest : IntegrationTestBase() {
 
   @Test
   fun `Health page reports ok`() {
+    stubPingWithResponse(200)
+
     webTestClient.get()
       .uri("/health")
       .exchange()
