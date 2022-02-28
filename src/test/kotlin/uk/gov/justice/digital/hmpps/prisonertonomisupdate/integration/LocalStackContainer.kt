136f4dc6ea22d8c2aa1acbda9c1f69b0d827fbb6
petergphillips <peter@greenthistle.co.uk>
2022-02-28 11:55:19 +0000

    SDI-71: 📝 Copy date time fix and security improvements (#43)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/integration/LocalStackContainer.kt
@@ -25,7 +25,7 @@ object LocalStackContainer {
     if (localstackIsRunning()) return null
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("0.12.10")
+      DockerImageName.parse("localstack/localstack").withTag("0.14.0")
     ).apply {
       withServices(LocalStackContainer.Service.SNS, LocalStackContainer.Service.SQS)
       withEnv("HOSTNAME_EXTERNAL", "localhost")
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/resource/OpenApiDocsTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.resource
 
 import io.swagger.v3.parser.OpenAPIV3Parser
+import net.minidev.json.JSONArray
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.web.server.LocalServerPort
@@ -46,8 +47,7 @@ class OpenApiDocsTest : IntegrationTestBase() {
   fun `the open api json is valid and contains documentation`() {
     val result = OpenAPIV3Parser().readLocation("http://localhost:$port/v3/api-docs", null, null)
     assertThat(result.messages).isEmpty()
-    // TODO: need to uncomment once we have a path
-    // assertThat(result.openAPI.paths).isNotEmpty
+    assertThat(result.openAPI.paths).isNotEmpty
   }
 
   @Test
@@ -58,5 +58,36 @@ class OpenApiDocsTest : IntegrationTestBase() {
       .exchange()
       .expectStatus().isOk
       .expectBody().jsonPath("info.version").isEqualTo(DateTimeFormatter.ISO_DATE.format(LocalDate.now()))
+  }
+
+  @Test
+  fun `the generated swagger for date times hasn't got the time zone`() {
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("$.components.schemas.VSIPVisit.properties.lastRevisedTimestamp.example").isEqualTo("2021-07-05T10:35:17")
+      .jsonPath("$.components.schemas.VSIPVisit.properties.lastRevisedTimestamp.type").isEqualTo("string")
+      .jsonPath("$.components.schemas.VSIPVisit.properties.lastRevisedTimestamp.pattern")
+      .isEqualTo("""^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}${'$'}""")
+      .jsonPath("$.components.schemas.VSIPVisit.properties.lastRevisedTimestamp.format").doesNotExist()
+  }
+
+  @Test
+  fun `the security scheme is setup for bearer tokens`() {
+    val bearerJwts = JSONArray()
+    bearerJwts.addAll(listOf("read", "write"))
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("$.components.securitySchemes.bearer-jwt")
+      .isEqualTo(mapOf("type" to "http", "scheme" to "bearer", "bearerFormat" to "JWT"))
+      .jsonPath("$.security[0].bearer-jwt")
+      .isEqualTo(bearerJwts)
   }
 }
