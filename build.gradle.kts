20bfc7b53c42ae1ee729ec7fd7d29a85d16b5125
petergphillips <peter@greenthistle.co.uk>
2022-04-22 13:37:40 +0100

    SDI-171: 🔒️ Upgrade spring boot and grab latest suppressions (#65)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta-2"
   kotlin("plugin.spring") version "1.6.21"
 }
 
@@ -14,15 +14,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.3")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.8")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.mockito:mockito-inline:4.5.0")
+  testImplementation("org.mockito:mockito-inline:4.5.1")
   testImplementation("org.testcontainers:localstack:1.17.1")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
 }
