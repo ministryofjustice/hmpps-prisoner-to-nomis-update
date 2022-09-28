43db705aad17f18bf6df96097e88a6c3685d7457
petergphillips <peter@greenthistle.co.uk>
2022-09-28 14:28:57 +0100

    SDI-394: ⬆️  Grab latest spring boot gradle plugin (#111)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.1-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.2-beta"
   kotlin("plugin.spring") version "1.7.10"
 }
 
@@ -12,7 +12,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.8")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.10")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
@@ -20,7 +20,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-security:1.6.11")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.2")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.3")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.mockito:mockito-inline:4.8.0")
   testImplementation("org.testcontainers:localstack:1.17.3")
