c9296ead707ae05241ae4d679c5e3e65c313150c
petergphillips <peter@greenthistle.co.uk>
2022-10-25 08:10:02 +0100

    SDI-426: ⬆️ Upgrade to spring boot 2.7.5 (#121)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.5"
-  kotlin("plugin.spring") version "1.7.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.6-beta"
+  kotlin("plugin.spring") version "1.7.20"
 }
 
 configurations {
@@ -14,15 +14,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.12")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.12")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.3")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.6")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.mockito:mockito-inline:4.8.0")
+  testImplementation("org.mockito:mockito-inline:4.8.1")
   testImplementation("org.testcontainers:localstack:1.17.5")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
 }
