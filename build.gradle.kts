dc004e83f7b354f7ca37356b1a9d0d48bc127007
petergphillips <peter@greenthistle.co.uk>
2022-11-02 09:30:28 +0000

    SDI-438: ⬆️  Upgrade to latest for spring security 5.7.5 (#125)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7-beta"
   kotlin("plugin.spring") version "1.7.20"
 }
 
@@ -20,7 +20,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-security:1.6.12")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.6")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.7")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.mockito:mockito-inline:4.8.1")
   testImplementation("org.testcontainers:localstack:1.17.5")
