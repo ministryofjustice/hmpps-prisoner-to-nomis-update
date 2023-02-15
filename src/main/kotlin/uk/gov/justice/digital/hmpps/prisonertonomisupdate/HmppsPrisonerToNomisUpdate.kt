ec5549ec2f53eb138c5b723e94e493ff2689f20b
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-02-15 10:57:03 +0000

    SDI-632 Gather startup metrics

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/HmppsPrisonerToNomisUpdate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/HmppsPrisonerToNomisUpdate.kt
@@ -1,11 +1,12 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
 class HmppsPrisonerToNomisUpdate
 
 fun main(args: Array<String>) {
-  runApplication<HmppsPrisonerToNomisUpdate>(*args)
+  runApplication<HmppsPrisonerToNomisUpdate>(*args) { applicationStartup = BufferingApplicationStartup(10000) }
 }
