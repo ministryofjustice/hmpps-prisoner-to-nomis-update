3609ec91d9fb764559d78d4a311ff77790246157
Andy Marke <andy.marke@digital.justice.gov.uk>
2023-03-03 15:08:19 +0000

    SDIT-642 Retry mapping made generic (#208)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/services/SynchronisationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/services/SynchronisationService.kt
@@ -2,10 +2,40 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.services
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.microsoft.applicationinsights.TelemetryClient
 
-abstract class SynchronisationService(internal val objectMapper: ObjectMapper) {
+abstract class SynchronisationService(
+  internal val objectMapper: ObjectMapper,
+  internal val telemetryClient: TelemetryClient,
+  private val retryQueueService: RetryQueueService,
+) {
   abstract suspend fun retryCreateMapping(message: String)
 
   internal inline fun <reified T> String.fromJson(): T =
     objectMapper.readValue(this)
+
+  suspend fun <T : Any> tryCreateMapping(
+    mapping: T,
+    telemetry: MappingTelemetry,
+    createMapping: suspend (mapping: T) -> Unit,
+  ) {
+    kotlin.runCatching {
+      createMapping(mapping)
+    }
+      .onFailure {
+        retryQueueService.sendMessage(mapping, telemetry.attributes)
+      }
+      .onSuccess {
+        telemetryClient.trackEvent(
+          telemetry.name,
+          telemetry.attributes,
+          null,
+        )
+      }
+  }
 }
+
+data class MappingTelemetry(
+  val name: String,
+  val attributes: Map<String, String>,
+)
