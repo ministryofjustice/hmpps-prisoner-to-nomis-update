7b00920a200d16ba0a2fd4610718f9a234b205db
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2023-03-24 09:56:13 +0000

    sdi-702 temporarily making field optional until available from client… (#234)
    * sdi-702 temporarily making field optional until available from client service
    
    * sdi-702 temporarily making field optional until available from client service
    
    ---------
    
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/sentencing/SentencingAdjustmentsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/sentencing/SentencingAdjustmentsApiService.kt
@@ -21,7 +21,7 @@ class SentencingAdjustmentsApiService(private val sentenceAdjustmentsApiWebClien
 }
 
 data class AdjustmentDetails(
-  val adjustmentDate: LocalDate = LocalDate.now(),
+  val adjustmentDate: LocalDate? = LocalDate.now(), // TODO remove optional when implemented in adjustments api
   val adjustmentFromDate: LocalDate?,
   val adjustmentDays: Long,
   val bookingId: Long,
