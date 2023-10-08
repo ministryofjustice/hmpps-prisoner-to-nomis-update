8e3dcb8de6ed6b310479e3889df8f3b8e8e46742
steverendell <steve.rendell@syscon.net>
2023-10-08 14:20:27 +0100

    SDIT-1128 Upgrade localstack in SQS lib

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/integration/LocalStackContainer.kt
@@ -19,13 +19,16 @@ object LocalStackContainer {
   }
 
   private fun startLocalstackIfNotRunning(): LocalStackContainer? {
-    if (localstackIsRunning()) return null
+    if (localstackIsRunning()) {
+      log.warn("Using existing localstack instance")
+      return null
+    }
+    log.info("Creating a localstack instance")
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("1.4"),
+      DockerImageName.parse("localstack/localstack").withTag("2.3"),
     ).apply {
       withServices(LocalStackContainer.Service.SNS, LocalStackContainer.Service.SQS)
-      withEnv("HOSTNAME_EXTERNAL", "localhost")
       withEnv("DEFAULT_REGION", "eu-west-2")
       waitingFor(
         Wait.forLogMessage(".*Ready.*", 1),
