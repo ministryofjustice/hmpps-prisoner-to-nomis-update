2dbe044667f8faedc451f1403aa593a4ac42cb2a
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-10-09 09:57:56 +0100

    SDIT-1113 Respond to Syscon security report (#402)
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/sentencing/MockSentencingResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/sentencing/MockSentencingResource.kt
@@ -3,12 +3,14 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.sentencing
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 
 @RestController
+@PreAuthorize("hasRole('ROLE_VIEW_SENTENCE_ADJUSTMENTS')")
 class MockSentencingResource(
   private val mockSentenceAdjustmentData: MockSentenceAdjustmentData,
   private val mockKeyDateAdjustmentData: MockKeyDateAdjustmentData,
