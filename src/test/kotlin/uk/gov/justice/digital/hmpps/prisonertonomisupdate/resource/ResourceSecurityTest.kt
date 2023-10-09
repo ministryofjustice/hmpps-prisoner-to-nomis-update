cabbbcb90f6d8671f8ab2c6570b115edaa607672
steverendell <steve.rendell@syscon.net>
2023-10-09 09:24:42 +0100

    SDIT-1113 Respond to Syscon security report

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/resource/ResourceSecurityTest.kt
@@ -0,0 +1,56 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.resource
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.reactive.result.method.RequestMappingInfo
+import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.integration.IntegrationTestBase
+import java.io.File
+
+class ResourceSecurityTest : IntegrationTestBase() {
+  @Autowired
+  private lateinit var context: ApplicationContext
+
+  private val unprotectedDefaultMethods = setOf(
+    "GET /v3/api-docs.yaml",
+    "GET /swagger-ui.html",
+    "GET /v3/api-docs",
+    "GET /v3/api-docs/swagger-config",
+    " /error",
+  )
+
+  @Test
+  fun `Ensure all endpoints protected with PreAuthorize`() {
+    // need to exclude any that are forbidden in helm configuration
+    val exclusions = File("helm_deploy").walk().filter { it.name.equals("values.yaml") }.flatMap { file ->
+      file.readLines().map { line ->
+        line.takeIf { it.contains("location") }?.substringAfter("location ")?.substringBefore(" {")
+      }
+    }.filterNotNull().flatMap { path -> listOf("GET", "POST", "PUT", "DELETE").map { "$it $path" } }
+      .toMutableSet().also {
+        it.addAll(unprotectedDefaultMethods)
+      }
+
+    context.getBeansOfType(RequestMappingHandlerMapping::class.java).forEach { (_, mapping) ->
+      mapping.handlerMethods.forEach { (mappingInfo, method) ->
+        val classAnnotation = method.beanType.getAnnotation(PreAuthorize::class.java)
+        val annotation = method.getMethodAnnotation(PreAuthorize::class.java)
+        if (classAnnotation == null && annotation == null) {
+          mappingInfo.getMappings().forEach {
+            Assertions.assertThat(exclusions.contains(it)).withFailMessage {
+              "Found $mappingInfo of type $method with no PreAuthorize annotation"
+            }.isTrue()
+          }
+        }
+      }
+    }
+  }
+}
+
+private fun RequestMappingInfo.getMappings() =
+  methodsCondition.methods.map { it.name }.flatMap { method ->
+    patternsCondition.patterns.map { "$method $it" }
+  }
