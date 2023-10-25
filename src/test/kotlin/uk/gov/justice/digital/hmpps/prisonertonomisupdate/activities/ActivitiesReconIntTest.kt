2795c852de56c1a7484c735e438ed3708fff4b81
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2023-10-25 09:23:47 +0100

    SDIT-1136 Add prisoner details to allocation recon failure telemetry (#432)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/activities/ActivitiesReconIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/activities/ActivitiesReconIntTest.kt
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.activities
 
+import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.untilAsserted
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.check
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.isNull
 import org.mockito.kotlin.verify
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.ActivitiesApiExtension.Companion.activitiesApi
@@ -18,7 +22,7 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
       @Test
       fun `should publish success telemetry if no differences`() {
         stubGetPrisons("BXI")
-        stubBookingCounts("BXI", BookingCountStub(bookingId = 1234567, nomisCount = 1, dpsCount = 1))
+        stubBookingCounts("BXI", BookingDetailsStub(bookingId = 1234567, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 1))
 
         webTestClient.post().uri("/allocations/reports/reconciliation")
           .exchange()
@@ -43,7 +47,7 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
       @Test
       fun `should publish failed telemetry if there are differences between NOMIS and DPS`() {
         stubGetPrisons("BXI")
-        stubBookingCounts("BXI", BookingCountStub(bookingId = 1234567, nomisCount = 1, dpsCount = 2))
+        stubBookingCounts("BXI", BookingDetailsStub(bookingId = 1234567, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 2))
 
         webTestClient.post().uri("/allocations/reports/reconciliation")
           .exchange()
@@ -51,9 +55,19 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
 
         await untilAsserted {
           verify(telemetryClient).trackEvent(
-            "activity-allocation-reconciliation-report-failed",
-            mapOf("prison" to "BXI", "different_count" to "[1234567]"),
-            null,
+            eq("activity-allocation-reconciliation-report-failed"),
+            check {
+              assertThat(it).containsExactlyInAnyOrderEntriesOf(
+                mapOf(
+                  "prison" to "BXI",
+                  "type" to "different_count",
+                  "bookingId" to "1234567",
+                  "offenderNo" to "A1234AA",
+                  "location" to "BXI",
+                ),
+              )
+            },
+            isNull(),
           )
         }
       }
@@ -61,8 +75,8 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
       @Test
       fun `should publish telemetry for multiple prisons`() {
         stubGetPrisons("BXI", "MDI")
-        stubBookingCounts("BXI", BookingCountStub(bookingId = 1234567, nomisCount = 1, dpsCount = 1))
-        stubBookingCounts("MDI", BookingCountStub(bookingId = 2345678, nomisCount = 1, dpsCount = 2))
+        stubBookingCounts("BXI", BookingDetailsStub(bookingId = 1234567, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 1))
+        stubBookingCounts("MDI", BookingDetailsStub(bookingId = 2345678, offenderNo = "A1234BB", location = "MDI", nomisCount = 1, dpsCount = 2))
 
         webTestClient.post().uri("/allocations/reports/reconciliation")
           .exchange()
@@ -84,9 +98,19 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
         }
         await untilAsserted {
           verify(telemetryClient).trackEvent(
-            "activity-allocation-reconciliation-report-failed",
-            mapOf("prison" to "MDI", "different_count" to "[2345678]"),
-            null,
+            eq("activity-allocation-reconciliation-report-failed"),
+            check {
+              assertThat(it).containsExactlyInAnyOrderEntriesOf(
+                mapOf(
+                  "prison" to "MDI",
+                  "type" to "different_count",
+                  "bookingId" to "2345678",
+                  "offenderNo" to "A1234BB",
+                  "location" to "MDI",
+                ),
+              )
+            },
+            isNull(),
           )
         }
       }
@@ -111,7 +135,7 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
       fun `should publish error telemetry if fails to get recon data from NOMIS`() {
         stubGetPrisons("BXI")
         nomisApi.stubAllocationReconciliationWithError("BXI", 500)
-        stubBookingCounts("BXI", BookingCountStub(bookingId = 1234567, nomisCount = null, dpsCount = 1))
+        stubBookingCounts("BXI", BookingDetailsStub(bookingId = 1234567, offenderNo = "A1234AA", location = "BXI", nomisCount = null, dpsCount = 1))
 
         webTestClient.post().uri("/allocations/reports/reconciliation")
           .exchange()
@@ -132,65 +156,65 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
           )
         }
       }
-    }
 
-    @Test
-    fun `should publish error telemetry if fails to get recon data from DPS`() {
-      stubGetPrisons("BXI")
-      stubBookingCounts("BXI", BookingCountStub(bookingId = 1234567, nomisCount = 1, dpsCount = null))
-      activitiesApi.stubAllocationReconciliationWithError("BXI", 400)
+      @Test
+      fun `should publish error telemetry if fails to get recon data from DPS`() {
+        stubGetPrisons("BXI")
+        stubBookingCounts("BXI", BookingDetailsStub(bookingId = 1234567, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = null))
+        activitiesApi.stubAllocationReconciliationWithError("BXI", 400)
 
-      webTestClient.post().uri("/allocations/reports/reconciliation")
-        .exchange()
-        .expectStatus().isAccepted
+        webTestClient.post().uri("/allocations/reports/reconciliation")
+          .exchange()
+          .expectStatus().isAccepted
 
-      await untilAsserted {
-        verify(telemetryClient).trackEvent(
-          "activity-allocation-reconciliation-report-requested",
-          mapOf("prisons" to "[BXI]"),
-          null,
-        )
+        await untilAsserted {
+          verify(telemetryClient).trackEvent(
+            "activity-allocation-reconciliation-report-requested",
+            mapOf("prisons" to "[BXI]"),
+            null,
+          )
+        }
+        await untilAsserted {
+          verify(telemetryClient).trackEvent(
+            "activity-allocation-reconciliation-report-error",
+            mapOf("prison" to "BXI"),
+            null,
+          )
+        }
       }
-      await untilAsserted {
-        verify(telemetryClient).trackEvent(
-          "activity-allocation-reconciliation-report-error",
-          mapOf("prison" to "BXI"),
-          null,
-        )
-      }
-    }
 
-    @Test
-    fun `should continue to report on other prisons if one fails`() {
-      stubGetPrisons("BXI", "MDI")
-      stubBookingCounts("BXI", BookingCountStub(bookingId = 1234567, nomisCount = 1, dpsCount = null))
-      activitiesApi.stubAllocationReconciliationWithError("BXI", 400)
-      stubBookingCounts("MDI", BookingCountStub(bookingId = 2345678, nomisCount = 1, dpsCount = 1))
+      @Test
+      fun `should continue to report on other prisons if one fails`() {
+        stubGetPrisons("BXI", "MDI")
+        stubBookingCounts("BXI", BookingDetailsStub(bookingId = 1234567, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = null))
+        activitiesApi.stubAllocationReconciliationWithError("BXI", 400)
+        stubBookingCounts("MDI", BookingDetailsStub(bookingId = 2345678, offenderNo = "A1234BB", location = "MDI", nomisCount = 1, dpsCount = 1))
 
-      webTestClient.post().uri("/allocations/reports/reconciliation")
-        .exchange()
-        .expectStatus().isAccepted
+        webTestClient.post().uri("/allocations/reports/reconciliation")
+          .exchange()
+          .expectStatus().isAccepted
 
-      await untilAsserted {
-        verify(telemetryClient).trackEvent(
-          "activity-allocation-reconciliation-report-requested",
-          mapOf("prisons" to "[BXI, MDI]"),
-          null,
-        )
-      }
-      await untilAsserted {
-        verify(telemetryClient).trackEvent(
-          "activity-allocation-reconciliation-report-error",
-          mapOf("prison" to "BXI"),
-          null,
-        )
-      }
-      await untilAsserted {
-        verify(telemetryClient).trackEvent(
-          "activity-allocation-reconciliation-report-success",
-          mapOf("prison" to "MDI"),
-          null,
-        )
+        await untilAsserted {
+          verify(telemetryClient).trackEvent(
+            "activity-allocation-reconciliation-report-requested",
+            mapOf("prisons" to "[BXI, MDI]"),
+            null,
+          )
+        }
+        await untilAsserted {
+          verify(telemetryClient).trackEvent(
+            "activity-allocation-reconciliation-report-error",
+            mapOf("prison" to "BXI"),
+            null,
+          )
+        }
+        await untilAsserted {
+          verify(telemetryClient).trackEvent(
+            "activity-allocation-reconciliation-report-success",
+            mapOf("prison" to "MDI"),
+            null,
+          )
+        }
       }
     }
 
@@ -199,7 +223,7 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
       nomisApi.stubGetServicePrisons("ACTIVITY", """ [ ${prisonsResponse.joinToString(",")} ] """)
     }
 
-    private fun stubBookingCounts(prisonId: String, vararg bookingCounts: BookingCountStub) {
+    private fun stubBookingCounts(prisonId: String, vararg bookingCounts: BookingDetailsStub) {
       bookingCounts
         .filterNot { it.nomisCount == null }
         .map { """{ "bookingId": ${it.bookingId}, "count": ${it.nomisCount} }""" }
@@ -220,6 +244,14 @@ class ActivitiesReconIntTest : IntegrationTestBase() {
             prisonId,
             """ { "prisonCode": "$prisonId", "bookings": [ ${this.joinToString(",")} ] } """,
           )
+        }
+
+      bookingCounts
+        .filterNot { it.nomisCount == it.dpsCount }
+        .map { """{ "bookingId": ${it.bookingId}, "offenderNo": "${it.offenderNo}", "location": "${it.location}" }""" }
+        .takeIf { it.isNotEmpty() }
+        ?.run {
+          nomisApi.stubGetPrisonerDetails(""" [ ${this.joinToString(",")} ] """)
         }
     }
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/activities/ActivitiesReconTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/activities/ActivitiesReconTest.kt
@@ -6,12 +6,21 @@ import com.microsoft.applicationinsights.TelemetryClient
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.tuple
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.anyString
-import org.mockito.Mockito.mock
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.isNull
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyBlocking
 import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.PrisonerDetails
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.NomisApiService
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.activities.model.AllocationReconciliationResponse as DpsResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.activities.model.BookingCount as DpsBookingCount
@@ -28,9 +37,13 @@ class ActivitiesReconTest {
 
   @Nested
   inner class AllocationReconciliationReport {
+
+    // Using check when the call being verified is inside a coroutine doesn't bubble the errors up to fail the test - so we use a captor instead
+    private val telemetryCaptor = argumentCaptor<Map<String, String>>()
+
     @Test
     fun `should publish success telemetry if no differences found`() = runTest {
-      stubBookingCounts(prisonId = "BXI", BookingCountStub(bookingId = 11, nomisCount = 1, dpsCount = 1))
+      stubBookingCounts(prisonId = "BXI", BookingDetailsStub(bookingId = 11, offenderNo = "A1234AA", location = "TRN", nomisCount = 1, dpsCount = 1))
 
       activitiesReconService.allocationsReconciliationReport("BXI")
 
@@ -47,17 +60,38 @@ class ActivitiesReconTest {
     fun `should publish fail telemetry for bookings found in Nomis only`() = runTest {
       stubBookingCounts(
         prisonId = "BXI",
-        BookingCountStub(bookingId = 11, nomisCount = 1, dpsCount = 1),
-        BookingCountStub(12, 1, null),
-        BookingCountStub(13, 1, null),
+        BookingDetailsStub(bookingId = 11, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 1),
+        BookingDetailsStub(12, "A1234BB", "TRN", 1, null),
+        BookingDetailsStub(13, "A1234CC", "OUT", 1, null),
       )
 
       activitiesReconService.allocationsReconciliationReport("BXI")
 
-      verify(telemetryClient).trackEvent(
-        "activity-allocation-reconciliation-report-failed",
-        mapOf("prison" to "BXI", "NOMIS_only" to "[12, 13]"),
-        null,
+      verifyBlocking(telemetryClient, times(2)) {
+        trackEvent(
+          eq("activity-allocation-reconciliation-report-failed"),
+          telemetryCaptor.capture(),
+          isNull(),
+        )
+      }
+
+      assertThat(telemetryCaptor.firstValue).containsExactlyInAnyOrderEntriesOf(
+        mapOf(
+          "prison" to "BXI",
+          "type" to "NOMIS_only",
+          "bookingId" to "12",
+          "offenderNo" to "A1234BB",
+          "location" to "TRN",
+        ),
+      )
+      assertThat(telemetryCaptor.secondValue).containsExactlyInAnyOrderEntriesOf(
+        mapOf(
+          "prison" to "BXI",
+          "type" to "NOMIS_only",
+          "bookingId" to "13",
+          "offenderNo" to "A1234CC",
+          "location" to "OUT",
+        ),
       )
     }
 
@@ -65,30 +99,59 @@ class ActivitiesReconTest {
     fun `should publish fail telemetry for bookings found in DPS only`() = runTest {
       stubBookingCounts(
         prisonId = "BXI",
-        BookingCountStub(bookingId = 11, nomisCount = 1, dpsCount = 1),
-        BookingCountStub(12, null, 1),
-        BookingCountStub(13, null, 1),
+        BookingDetailsStub(bookingId = 11, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 1),
+        BookingDetailsStub(12, "A1234BB", "TRN", null, 1),
+        BookingDetailsStub(13, "A1234CC", "OUT", null, 1),
       )
 
       activitiesReconService.allocationsReconciliationReport("BXI")
 
-      verify(telemetryClient).trackEvent(
-        "activity-allocation-reconciliation-report-failed",
-        mapOf("prison" to "BXI", "DPS_only" to "[12, 13]"),
-        null,
+      verify(telemetryClient, times(2)).trackEvent(
+        eq("activity-allocation-reconciliation-report-failed"),
+        telemetryCaptor.capture(),
+        isNull(),
+      )
+
+      assertThat(telemetryCaptor.firstValue).containsAllEntriesOf(
+        mapOf(
+          "prison" to "BXI",
+          "type" to "DPS_only",
+          "bookingId" to "12",
+          "offenderNo" to "A1234BB",
+          "location" to "TRN",
+        ),
+      )
+      assertThat(telemetryCaptor.secondValue).containsAllEntriesOf(
+        mapOf(
+          "prison" to "BXI",
+          "type" to "DPS_only",
+          "bookingId" to "13",
+          "offenderNo" to "A1234CC",
+          "location" to "OUT",
+        ),
       )
     }
 
     @Test
     fun `should publish fail telemetry for bookings with different counts in NOMIS and DPS`() = runTest {
-      stubBookingCounts(prisonId = "BXI", BookingCountStub(bookingId = 11, nomisCount = 1, dpsCount = 2))
+      stubBookingCounts(prisonId = "BXI", BookingDetailsStub(bookingId = 11, offenderNo = "A1234AA", location = "TRN", nomisCount = 1, dpsCount = 2))
 
       activitiesReconService.allocationsReconciliationReport("BXI")
 
       verify(telemetryClient).trackEvent(
-        "activity-allocation-reconciliation-report-failed",
-        mapOf("prison" to "BXI", "different_count" to "[11]"),
-        null,
+        eq("activity-allocation-reconciliation-report-failed"),
+        telemetryCaptor.capture(),
+        isNull(),
+      )
+
+      assertThat(telemetryCaptor.firstValue).containsAllEntriesOf(
+        mapOf(
+          "prison" to "BXI",
+          "type" to "different_count",
+          "bookingId" to "11",
+          "offenderNo" to "A1234AA",
+          "location" to "TRN",
+        ),
       )
     }
 
@@ -96,28 +159,32 @@ class ActivitiesReconTest {
     fun `should publish fail telemetry for bookings with a combination of different reasons`() = runTest {
       stubBookingCounts(
         prisonId = "BXI",
-        BookingCountStub(bookingId = 11, nomisCount = 1, dpsCount = 1),
-        BookingCountStub(12, 1, null),
-        BookingCountStub(13, null, 1),
-        BookingCountStub(14, 1, 1),
-        BookingCountStub(15, 1, 2),
-        BookingCountStub(16, 1, 1),
-        BookingCountStub(17, 1, null),
-        BookingCountStub(18, null, 1),
-        BookingCountStub(19, 1, 2),
+        BookingDetailsStub(bookingId = 11, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 1),
+        BookingDetailsStub(12, "A1234BB", "TRN", 1, null),
+        BookingDetailsStub(13, "A1234CC", "OUT", null, 1),
+        BookingDetailsStub(14, "A1234DD", "BXI", 1, 1),
+        BookingDetailsStub(15, "A1234EE", "TRN", 1, 2),
+        BookingDetailsStub(16, "A1234FF", "OUT", 1, 1),
+        BookingDetailsStub(17, "A1234GG", "BXI", 1, null),
+        BookingDetailsStub(18, "A1234HH", "TRN", null, 1),
+        BookingDetailsStub(19, "A1234II", "OUT", 1, 2),
       )
 
       activitiesReconService.allocationsReconciliationReport("BXI")
 
-      verify(telemetryClient).trackEvent(
-        "activity-allocation-reconciliation-report-failed",
-        mapOf(
-          "prison" to "BXI",
-          "NOMIS_only" to "[12, 17]",
-          "DPS_only" to "[13, 18]",
-          "different_count" to "[15, 19]",
-        ),
-        null,
+      verify(telemetryClient, times(6)).trackEvent(
+        eq("activity-allocation-reconciliation-report-failed"),
+        telemetryCaptor.capture(),
+        isNull(),
+      )
+
+      assertThat(telemetryCaptor.allValues).extracting("bookingId", "type").containsExactly(
+        tuple("12", "NOMIS_only"),
+        tuple("13", "DPS_only"),
+        tuple("15", "different_count"),
+        tuple("17", "NOMIS_only"),
+        tuple("18", "DPS_only"),
+        tuple("19", "different_count"),
       )
     }
 
@@ -125,32 +192,37 @@ class ActivitiesReconTest {
     fun `should publish fail telemetry for bookings that are not in order`() = runTest {
       stubBookingCounts(
         prisonId = "BXI",
-        BookingCountStub(bookingId = 19, nomisCount = 1, dpsCount = 2),
-        BookingCountStub(18, null, 1),
-        BookingCountStub(17, 1, null),
-        BookingCountStub(16, 1, 1),
-        BookingCountStub(15, 1, 2),
-        BookingCountStub(14, 1, 1),
-        BookingCountStub(13, null, 1),
-        BookingCountStub(12, 1, null),
-        BookingCountStub(11, 1, 1),
+        BookingDetailsStub(bookingId = 19, offenderNo = "A1234AA", location = "BXI", nomisCount = 1, dpsCount = 2),
+        BookingDetailsStub(18, "A1234BB", "TRN", null, 1),
+        BookingDetailsStub(17, "A1234CC", "OUT", 1, null),
+        BookingDetailsStub(16, "A1234DD", "BXI", 1, 1),
+        BookingDetailsStub(15, "A1234EE", "TRN", 1, 2),
+        BookingDetailsStub(14, "A1234FF", "OUT", 1, 1),
+        BookingDetailsStub(13, "A1234GG", "BCI", null, 1),
+        BookingDetailsStub(12, "A1234HH", "TRN", 1, null),
+        BookingDetailsStub(11, "A1234II", "OUT", 1, 1),
       )
 
       activitiesReconService.allocationsReconciliationReport("BXI")
 
-      verify(telemetryClient).trackEvent(
-        "activity-allocation-reconciliation-report-failed",
-        mapOf(
-          "prison" to "BXI",
-          "NOMIS_only" to "[12, 17]",
-          "DPS_only" to "[13, 18]",
-          "different_count" to "[15, 19]",
-        ),
-        null,
+      verify(telemetryClient, times(6)).trackEvent(
+        eq("activity-allocation-reconciliation-report-failed"),
+        telemetryCaptor.capture(),
+        isNull(),
+      )
+
+      assertThat(telemetryCaptor.allValues).extracting("bookingId", "type").containsExactly(
+        tuple("12", "NOMIS_only"),
+        tuple("13", "DPS_only"),
+        tuple("15", "different_count"),
+        tuple("17", "NOMIS_only"),
+        tuple("18", "DPS_only"),
+        tuple("19", "different_count"),
       )
     }
 
-    private fun stubBookingCounts(prisonId: String, vararg bookingCounts: BookingCountStub) = runTest {
+    private fun stubBookingCounts(prisonId: String, vararg bookingCounts: BookingDetailsStub) = runTest {
+      // stub the booking allocations counts for NOMIS
       bookingCounts
         .filterNot { it.nomisCount == null }
         .map { NomisBookingCount(it.bookingId, it.nomisCount!!) }
@@ -159,6 +231,7 @@ class ActivitiesReconTest {
             .thenReturn(NomisResponse(prisonId, it))
         }
 
+      // stub the booking allocations counts for DPS
       bookingCounts
         .filterNot { it.dpsCount == null }
         .map { DpsBookingCount(it.bookingId, it.dpsCount!!) }
@@ -166,8 +239,14 @@ class ActivitiesReconTest {
           whenever(activitiesApiService.getAllocationReconciliation(anyString()))
             .thenReturn(DpsResponse(prisonId, it))
         }
+
+      // stub the booking details calls for all will be reported on (that don't match counts)
+      bookingCounts
+        .filterNot { it.nomisCount == it.dpsCount }
+        .map { PrisonerDetails(it.offenderNo, it.bookingId, it.location) }
+        .also { whenever(nomisApiService.getPrisonerDetails(any())).thenReturn(it) }
     }
   }
 }
 
-internal data class BookingCountStub(val bookingId: Long, val nomisCount: Long?, val dpsCount: Long?)
+internal data class BookingDetailsStub(val bookingId: Long, val offenderNo: String, val location: String, val nomisCount: Long?, val dpsCount: Long?)
