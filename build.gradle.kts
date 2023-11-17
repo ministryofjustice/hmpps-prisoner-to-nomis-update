9516c578e46ae31f1d9636a92a55441afe1851a0
petergphillips <peter@greenthistle.co.uk>
2023-11-17 12:19:21 +0000

    SDIT-1255: ⬆️ Upgrade for reactor netty http vulnerability (#466)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.9.0"
   kotlin("plugin.spring") version "1.9.20"
-  id("org.openapi.generator") version "7.0.1"
+  id("org.openapi.generator") version "7.1.0"
 }
 
 configurations {
@@ -25,7 +25,6 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.1")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk9")
@@ -34,14 +33,14 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.18") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.18")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.19")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.3")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
 
-  testImplementation("org.wiremock:wiremock:3.2.0")
+  testImplementation("org.wiremock:wiremock:3.3.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.19.1")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.578")
+  testImplementation("org.testcontainers:localstack:1.19.2")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.591")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
