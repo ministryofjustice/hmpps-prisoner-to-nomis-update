4c96809177f144a4cc31a22555c43fdad42d4c8c
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-01-31 14:40:28 +0000

    SDIT-1458 Add repair functionality to adjudications service
    This commit introduces a repair function on the adjudications service that handles punishment resynchronization from the DPS back to NOMIS. This functionality aims to repair data when the domain event adjudication.punishments.updated goes missing. There were also additions to the integration tests to ensure this repair functionality works as expected.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/adjudications/AdjudicationsDataRepairResource.kt
@@ -0,0 +1,40 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.adjudications
+
+import com.microsoft.applicationinsights.TelemetryClient
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AdjudicationsDataRepairResource(
+  private val adjudicationService: AdjudicationsService,
+  private val telemetryClient: TelemetryClient,
+) {
+  @PostMapping("/prisons/{prisonId}/prisoners/{offenderNo}/adjudication/dps-charge-number/{chargeNumber}/punishments/repair")
+  @ResponseStatus(HttpStatus.OK)
+  @PreAuthorize("hasRole('NOMIS_ADJUDCIATIONS')")
+  @Operation(
+    summary = "Resynchronises punishments for the given adjudication from DPS back to NOMIS",
+    description = "Used when a domain event adjudication.punishments.updated has gone missing, so emergency use only. Requires ROLE_NOMIS_ADJUDICATIONS",
+  )
+  suspend fun repairPunishments(
+    @PathVariable prisonId: String,
+    @PathVariable offenderNo: String,
+    @PathVariable chargeNumber: String,
+  ) {
+    adjudicationService.updatePunishments(chargeNumber = chargeNumber, offenderNo = offenderNo, prisonId = prisonId)
+    telemetryClient.trackEvent(
+      "adjudication-punishment-repair",
+      mapOf(
+        "prisonId" to prisonId,
+        "chargeNumber" to chargeNumber,
+        "offenderNo" to offenderNo,
+      ),
+      null,
+    )
+  }
+}
