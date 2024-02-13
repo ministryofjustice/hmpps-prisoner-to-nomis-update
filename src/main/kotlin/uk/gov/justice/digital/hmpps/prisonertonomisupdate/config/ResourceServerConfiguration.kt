705da1cfa68b113ad77d4cb1746f1f882eca7b46
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-13 08:47:50 +0000

    SDIT-1335 Implement hmpps-kotlin-spring-boot-starter lib

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/config/ResourceServerConfiguration.kt
@@ -2,36 +2,22 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.invoke
-import org.springframework.security.web.server.SecurityWebFilterChain
+import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
 
 @Configuration
-@EnableWebFluxSecurity
-@EnableReactiveMethodSecurity(useAuthorizationManager = false)
 class ResourceServerConfiguration {
   @Bean
-  fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
-    http {
-      // Can't have CSRF protection as requires session
-      csrf { disable() }
-      authorizeExchange {
-        listOf(
-          "/webjars/**", "/favicon.ico", "/csrf",
-          "/health/**", "/info", "/startup", "/h2-console/**",
-          "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
-          "/queue-admin/retry-all-dlqs",
-          "/incentives/reports/reconciliation",
-          "/non-associations/reports/reconciliation",
-          "/allocations/reports/reconciliation",
-          "/attendances/reports/reconciliation",
-          "/sentencing/reports/reconciliation",
-          "/adjudications/reports/reconciliation",
-        ).forEach { authorize(it, permitAll) }
-        authorize(anyExchange, authenticated)
-      }
-      oauth2ResourceServer { jwt { jwtAuthenticationConverter = AuthAwareTokenConverter() } }
+  fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer {
+    unauthorizedRequestPaths {
+      addPaths = setOf(
+        "/queue-admin/retry-all-dlqs",
+        "/incentives/reports/reconciliation",
+        "/non-associations/reports/reconciliation",
+        "/allocations/reports/reconciliation",
+        "/attendances/reports/reconciliation",
+        "/sentencing/reports/reconciliation",
+        "/adjudications/reports/reconciliation",
+      )
     }
+  }
 }
