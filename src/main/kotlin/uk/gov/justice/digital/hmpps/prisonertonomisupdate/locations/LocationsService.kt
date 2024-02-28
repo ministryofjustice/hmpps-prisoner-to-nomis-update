2b31db8d7c42f63c81ed7f9fe58709d6e3bc64b1
steverendell <steve.rendell@syscon.net>
2024-02-28 16:38:46 +0000

    SDIT-1497 To Nomis sync updates
    Add attributes and usages

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/locations/LocationsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/locations/LocationsService.kt
@@ -7,12 +7,15 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations.model.Location
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations.model.NonResidentialUsageDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.LocationMappingDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.CreateLocationRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.DeactivateRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.ProfileRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpdateCapacityRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpdateCertificationRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpdateLocationRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UsageRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.CreateMappingRetryMessage
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.CreateMappingRetryable
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.CreatingSystem
@@ -31,7 +34,10 @@ class LocationsService(
 ) : CreateMappingRetryable {
 
   suspend fun createLocation(event: LocationDomainEvent) {
-    val telemetryMap = mapOf("dpsId" to event.additionalInformation.id)
+    val telemetryMap = mutableMapOf(
+      "key" to event.additionalInformation.key,
+      "dpsLocationId" to event.additionalInformation.id,
+    )
     if (isDpsCreated(event.additionalInformation)) {
       synchronise {
         name = "location"
@@ -46,8 +52,7 @@ class LocationsService(
           locationsApiService.getLocation(event.additionalInformation.id).run {
             val request = toCreateLocationRequest(this)
 
-            eventTelemetry += "key" to key
-            eventTelemetry += "prisonId" to prisonId
+            telemetryMap["prisonId"] = prisonId
 
             LocationMappingDto(
               dpsLocationId = id.toString(),
@@ -97,12 +102,12 @@ class LocationsService(
   }
 
   suspend fun softDeleteLocation(event: LocationDomainEvent) {
-    val telemetryMap = mutableMapOf("dpsId" to event.additionalInformation.id)
+    val telemetryMap = mutableMapOf("dpsLocationId" to event.additionalInformation.id)
     telemetryMap["key"] = event.additionalInformation.key
     if (isDpsCreated(event.additionalInformation)) {
       runCatching {
         mappingService.getMappingGivenDpsId(event.additionalInformation.id).apply {
-          telemetryMap["nomisId"] = nomisLocationId.toString()
+          telemetryMap["nomisLocationId"] = nomisLocationId.toString()
           nomisApiService.deactivateLocation(nomisLocationId, DeactivateRequest())
           nomisApiService.updateLocationCapacity(nomisLocationId, UpdateCapacityRequest(0, 0))
           nomisApiService.updateLocationCertification(nomisLocationId, UpdateCertificationRequest(0, false))
@@ -145,17 +150,153 @@ class LocationsService(
   private suspend fun toCreateLocationRequest(instance: Location) = CreateLocationRequest(
     locationCode = instance.code,
     certified = instance.certification?.certified ?: false,
+    cnaCapacity = instance.certification?.capacityOfCertifiedCell,
     locationType = CreateLocationRequest.LocationType.valueOf(toLocationType(instance.locationType)),
     comment = instance.comments,
     parentLocationId = instance.parentId?.let { mappingService.getMappingGivenDpsId(it.toString()).nomisLocationId },
     prisonId = instance.prisonId,
-    // TODO added to allow compilation after generated code change
-    description = instance.key,
+    description = "${instance.prisonId}-${instance.pathHierarchy}",
     operationalCapacity = instance.capacity?.operationalCapacity,
     userDescription = instance.description,
     capacity = instance.capacity?.capacity,
     listSequence = instance.orderWithinParentLocation,
     unitType = instance.residentialHousingType?.let { CreateLocationRequest.UnitType.valueOf(toUnitType(it)) },
+    profiles = instance.attributes?.map { toAttribute(it) },
+    usages = instance.usage?.map { toUsage(it) },
+  )
+
+  private fun toAttribute(attribute: Location.Attributes): ProfileRequest {
+    val fullAttribute = ResidentialAttributeValue.valueOf(attribute.name)
+    var code: String? = null
+    val type: ProfileRequest.ProfileType = when (fullAttribute.type) {
+      ResidentialAttributeType.SANITATION_FITTINGS -> {
+        code = when (attribute) {
+          Location.Attributes.ANTI_BARRICADE_DOOR -> "ABD"
+          Location.Attributes.AUDITABLE_CELL_BELL -> "ACB"
+          Location.Attributes.FIXED_BED -> "FIB"
+          Location.Attributes.METAL_DOOR -> "MD"
+          Location.Attributes.MOVABLE_BED -> "MOB"
+          Location.Attributes.PRIVACY_CURTAIN -> "PC"
+          Location.Attributes.PRIVACY_SCREEN -> "PS"
+          Location.Attributes.STANDARD_CELL_BELL -> "SCB"
+          Location.Attributes.SEPARATE_TOILET -> "SETO"
+          Location.Attributes.WOODEN_DOOR -> "WD"
+          else -> null
+        }
+        ProfileRequest.ProfileType.HOU_SANI_FIT
+      }
+
+      ResidentialAttributeType.LOCATION_ATTRIBUTE -> {
+        code = when (attribute) {
+          Location.Attributes.CAT_A_CELL -> "A"
+          Location.Attributes.DOUBLE_OCCUPANCY -> "DO"
+          Location.Attributes.E_LIST_CELL -> "ELC"
+          Location.Attributes.GATED_CELL -> "GC"
+          Location.Attributes.LISTENER_CELL -> "LC"
+          Location.Attributes.LOCATE_FLAT -> "LF"
+          Location.Attributes.MULTIPLE_OCCUPANCY -> "MO"
+          Location.Attributes.NON_SMOKER_CELL -> "NSMC"
+          Location.Attributes.OBSERVATION_CELL -> "OC"
+          Location.Attributes.SAFE_CELL -> "SAFE_CELL"
+          Location.Attributes.SINGLE_OCCUPANCY -> "SO"
+          Location.Attributes.SPECIAL_CELL -> "SPC"
+          Location.Attributes.WHEELCHAIR_ACCESS -> "WA"
+          else -> null
+        }
+        ProfileRequest.ProfileType.HOU_UNIT_ATT
+      }
+
+      ResidentialAttributeType.USED_FOR -> {
+        code = when (attribute) {
+          Location.Attributes.UNCONVICTED_JUVENILES -> "1"
+          Location.Attributes.SENTENCED_JUVENILES -> "2"
+          Location.Attributes.UNCONVICTED_18_20 -> "3"
+          Location.Attributes.SENTENCED_18_20 -> "4"
+          Location.Attributes.UNCONVICTED_ADULTS -> "5"
+          Location.Attributes.SENTENCED_ADULTS -> "6"
+          Location.Attributes.VULNERABLE_PRISONER_UNIT -> "7"
+          Location.Attributes.SPECIAL_UNIT -> "8"
+          Location.Attributes.RESETTLEMENT_HOSTEL -> "9"
+          Location.Attributes.HEALTHCARE_CENTRE -> "10"
+          Location.Attributes.NATIONAL_RESOURCE_HOSPITAL -> "11"
+          Location.Attributes.OTHER_SPECIFIED -> "12"
+          Location.Attributes.REMAND_CENTRE -> "A"
+          Location.Attributes.LOCAL_PRISON -> "B"
+          Location.Attributes.CLOSED_PRISON -> "C"
+          Location.Attributes.OPEN_TRAINING -> "D"
+          Location.Attributes.HOSTEL -> "E"
+          Location.Attributes.CLOSED_YOUNG_OFFENDER -> "I"
+          Location.Attributes.OPEN_YOUNG_OFFENDER -> "J"
+          Location.Attributes.REMAND_UNDER_18 -> "K"
+          Location.Attributes.SENTENCED_UNDER_18 -> "L"
+          Location.Attributes.ECL_COMPONENT -> "R"
+          Location.Attributes.ADDITIONAL_SPECIAL_UNIT -> "T"
+          Location.Attributes.SECOND_CLOSED_TRAINER -> "Y"
+          Location.Attributes.IMMIGRATION_DETAINEES -> "Z"
+          else -> null
+        }
+        ProfileRequest.ProfileType.HOU_USED_FOR
+      }
+
+      ResidentialAttributeType.SECURITY -> {
+        code = when (attribute) {
+          Location.Attributes.CAT_A -> "A"
+          Location.Attributes.CAT_A_EX -> "E"
+          Location.Attributes.CAT_A_HI -> "H"
+          Location.Attributes.CAT_B -> "B"
+          Location.Attributes.CAT_C -> "C"
+          Location.Attributes.CAT_D -> "D"
+          Location.Attributes.ELIGIBLE -> null
+          Location.Attributes.PAROLE_GRANTED -> "GRANTED"
+          Location.Attributes.INELIGIBLE -> null
+          Location.Attributes.YOI_CLOSED -> "I"
+          Location.Attributes.YOI_OPEN -> "J"
+          Location.Attributes.YOI_RESTRICTED -> "V"
+          Location.Attributes.YOI_SHORT_SENTENCE -> "K"
+          Location.Attributes.YOI_LONG_TERM_CLOSED -> "L"
+          Location.Attributes.UNCLASSIFIED -> "Z"
+          Location.Attributes.UNCATEGORISED_SENTENCED_MALE -> "X"
+          Location.Attributes.LOW -> "LOW"
+          Location.Attributes.MEDIUM -> "MED"
+          Location.Attributes.HIGH -> "HI"
+          Location.Attributes.NOT_APPLICABLE -> "N/A"
+          Location.Attributes.PROV_A -> "P"
+          Location.Attributes.PENDING -> "PEND"
+          Location.Attributes.REF_REVIEW -> "REF/REVIEW"
+          Location.Attributes.REFUSED_NO_REVIEW -> "REFUSED"
+          Location.Attributes.STANDARD -> "STANDARD"
+          Location.Attributes.FEMALE_RESTRICTED -> "Q"
+          Location.Attributes.FEMALE_CLOSED -> "R"
+          Location.Attributes.FEMALE_SEMI -> "S"
+          Location.Attributes.FEMALE_OPEN -> "T"
+          Location.Attributes.UN_SENTENCED -> "U"
+          Location.Attributes.YES -> "Y"
+          Location.Attributes.NO -> "N"
+          else -> null
+        }
+        ProfileRequest.ProfileType.SUP_LVL_TYPE
+      }
+
+      ResidentialAttributeType.NON_ASSOCIATIONS -> throw RuntimeException("non-association attribute type not supported")
+    }
+
+    return ProfileRequest(type, code ?: throw RuntimeException("Attribute $attribute not recognised"))
+  }
+
+  private fun toUsage(u: NonResidentialUsageDto) = UsageRequest(
+    internalLocationUsageType = when (u.usageType) {
+      NonResidentialUsageDto.UsageType.APPOINTMENT -> UsageRequest.InternalLocationUsageType.APP
+      NonResidentialUsageDto.UsageType.VISIT -> UsageRequest.InternalLocationUsageType.VISIT
+      NonResidentialUsageDto.UsageType.MOVEMENT -> UsageRequest.InternalLocationUsageType.MOVEMENT
+      NonResidentialUsageDto.UsageType.OCCURRENCE -> UsageRequest.InternalLocationUsageType.OCCUR
+      NonResidentialUsageDto.UsageType.ADJUDICATION_HEARING -> UsageRequest.InternalLocationUsageType.OIC
+      NonResidentialUsageDto.UsageType.OTHER -> UsageRequest.InternalLocationUsageType.OTHER
+      NonResidentialUsageDto.UsageType.PROGRAMMES_ACTIVITIES -> UsageRequest.InternalLocationUsageType.PROG
+      NonResidentialUsageDto.UsageType.PROPERTY -> UsageRequest.InternalLocationUsageType.PROP
+    },
+    usageLocationType = null,
+    sequence = u.sequence,
+    capacity = u.capacity,
   )
 
   private suspend fun toUpdateLocationRequest(instance: Location) = UpdateLocationRequest(
@@ -181,7 +322,7 @@ class LocationsService(
 
   private fun toLocationType(locationtype: Location.LocationType) = when (locationtype) {
     Location.LocationType.WING -> "WING"
-    Location.LocationType.SPUR, // TODO coming later
+    Location.LocationType.SPUR -> "SPUR"
     Location.LocationType.TIER, // TODO not sure about this yet
     Location.LocationType.LANDING,
     -> "LAND"
@@ -225,6 +366,7 @@ class LocationsService(
   }
 
   private fun toDeactivateRequest(location: Location): DeactivateRequest = DeactivateRequest(
+    deactivateDate = location.deactivatedDate,
     reasonCode = when (location.deactivatedReason) {
       Location.DeactivatedReason.NEW_BUILDING -> DeactivateRequest.ReasonCode.A
       Location.DeactivatedReason.CELL_RECLAIMS -> DeactivateRequest.ReasonCode.B
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/locations/LocationsServiceTest.kt
@@ -0,0 +1,150 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.microsoft.applicationinsights.TelemetryClient
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.groups.Tuple
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.check
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations.model.Capacity
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations.model.Certification
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations.model.Location
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.locations.model.NonResidentialUsageDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.LocationMappingDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.CreateLocationRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.LocationIdResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.ProfileRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UsageRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.NomisApiService
+import java.time.LocalDateTime
+import java.util.UUID
+
+private const val DPS_LOCATION_ID = "abcdef01-2345-6789-abcd-ef0123456789"
+private const val PARENT_DPS_LOCATION_ID = "12345678-2345-6789-abcd-ef0123456789"
+private const val DPS_KEY = "LEI-B-3-003"
+private const val NOMIS_LOCATION_ID = 4567L
+private const val PARENT_NOMIS_LOCATION_ID = 1234L
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class LocationsServiceTest {
+
+  private val locationsApiService: LocationsApiService = mock()
+  private val nomisApiService: NomisApiService = mock()
+  private val locationsMappingService: LocationsMappingService = mock()
+  private val locationsUpdateQueueService: LocationsRetryQueueService = mock()
+  private val telemetryClient: TelemetryClient = mock()
+  private val objectMapper: ObjectMapper = mock()
+
+  private val locationsService = LocationsService(
+    locationsApiService,
+    nomisApiService,
+    locationsMappingService,
+    locationsUpdateQueueService,
+    telemetryClient,
+    objectMapper,
+  )
+
+  @Test
+  fun `Create maps to nomis correctly`() = runTest {
+    whenever(
+      locationsMappingService.getMappingGivenDpsId(PARENT_DPS_LOCATION_ID),
+    ).thenReturn(
+      LocationMappingDto(
+        dpsLocationId = PARENT_DPS_LOCATION_ID,
+        nomisLocationId = PARENT_NOMIS_LOCATION_ID,
+        mappingType = LocationMappingDto.MappingType.LOCATION_CREATED,
+      ),
+    )
+
+    callService()
+    verify(nomisApiService).createLocation(
+      check {
+        assertThat(it.locationType).isEqualTo(CreateLocationRequest.LocationType.CELL)
+        assertThat(it.prisonId).isEqualTo("LEI")
+        assertThat(it.locationCode).isEqualTo("003")
+        assertThat(it.description).isEqualTo(DPS_KEY)
+        assertThat(it.parentLocationId).isEqualTo(PARENT_NOMIS_LOCATION_ID)
+        assertThat(it.userDescription).isEqualTo("description")
+        assertThat(it.unitType).isEqualTo(CreateLocationRequest.UnitType.NA)
+        assertThat(it.listSequence).isEqualTo(4)
+        assertThat(it.comment).isEqualTo("comments")
+        assertThat(it.certified).isTrue()
+        assertThat(it.cnaCapacity).isEqualTo(13)
+        assertThat(it.capacity).isEqualTo(14)
+        assertThat(it.operationalCapacity).isEqualTo(12)
+        assertThat(it.profiles).extracting("profileType", "profileCode").containsExactly(
+          Tuple.tuple(ProfileRequest.ProfileType.HOU_UNIT_ATT, "GC"),
+          Tuple.tuple(ProfileRequest.ProfileType.HOU_USED_FOR, "7"),
+        )
+        assertThat(it.usages).extracting("internalLocationUsageType", "sequence", "capacity").containsExactly(
+          Tuple.tuple(UsageRequest.InternalLocationUsageType.MOVEMENT, 3, 11),
+        )
+      },
+    )
+  }
+
+  private suspend fun callService() {
+    whenever(locationsApiService.getLocation(DPS_LOCATION_ID)).thenReturn(
+      newLocation(),
+    )
+    whenever(nomisApiService.createLocation(any())).thenReturn(LocationIdResponse(NOMIS_LOCATION_ID))
+
+    val location = LocationDomainEvent(
+      "TYPE",
+      "version",
+      "description",
+      LocalDateTime.now(),
+      LocationAdditionalInformation(id = DPS_LOCATION_ID, key = DPS_KEY),
+    )
+    locationsService.createLocation(location)
+  }
+
+  private fun newLocation(): Location {
+    return Location(
+      id = UUID.fromString(DPS_LOCATION_ID),
+      prisonId = "LEI",
+      code = "003",
+      pathHierarchy = "B-3-003",
+      locationType = Location.LocationType.CELL,
+      active = true,
+      topLevelId = UUID.fromString(PARENT_DPS_LOCATION_ID),
+      key = DPS_KEY,
+      isResidential = true,
+      residentialHousingType = Location.ResidentialHousingType.NORMAL_ACCOMMODATION,
+      description = "description",
+      comments = "comments",
+      capacity = Capacity(
+        operationalCapacity = 12,
+        capacity = 14,
+      ),
+      certification = Certification(
+        certified = true,
+        capacityOfCertifiedCell = 13,
+      ),
+      attributes = listOf(
+        Location.Attributes.GATED_CELL,
+        Location.Attributes.VULNERABLE_PRISONER_UNIT,
+      ),
+      usage = listOf(
+        NonResidentialUsageDto(
+          usageType = NonResidentialUsageDto.UsageType.MOVEMENT,
+          sequence = 3,
+          capacity = 11,
+        ),
+      ),
+      orderWithinParentLocation = 4,
+      //    deactivatedDate = ,
+      //    deactivatedReason = ,
+      //    reactivatedDate = ,
+      parentId = UUID.fromString(PARENT_DPS_LOCATION_ID),
+      //      parentLocation = ,
+      //      childLocations = ,
+    )
+  }
+}
