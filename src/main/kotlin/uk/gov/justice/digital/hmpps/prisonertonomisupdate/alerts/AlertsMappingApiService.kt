55d10680b222faa1d14d36d9f3e59a4e13afee98
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-03-08 09:46:54 +0100

    SDIT-1573 Integration for Mapping and NOMIS API for create alerts

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsMappingApiService.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.alerts
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBodilessEntity
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.AlertMappingDto
+
+@Service
+class AlertsMappingApiService(@Qualifier("mappingWebClient") private val webClient: WebClient) {
+  suspend fun getOrNullByDpsId(alertId: String): AlertMappingDto? = webClient.get()
+    .uri(
+      "/mapping/alerts/dps-alert-id/{alertId}",
+      alertId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun createMapping(alertMappingDto: AlertMappingDto) {
+    webClient.post()
+      .uri("/mapping/alerts")
+      .bodyValue(alertMappingDto)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+}
