cee694b0eccaf4dcd018acc7a705717f239ce0bd
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-03-08 15:30:51 +0100

    SDIT-1573 Not so happy path for Alerts created in DPS and NOMIS

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsService.kt
@@ -1,10 +1,14 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.alerts
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.AlertMappingDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.CreateMappingRetryMessage
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.CreateMappingRetryable
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.synchronise
 
 @Service
 class AlertsService(
@@ -12,11 +16,9 @@ class AlertsService(
   private val dpsApiService: AlertsDpsApiService,
   private val nomisApiService: AlertsNomisApiService,
   private val mappingApiService: AlertsMappingApiService,
+  private val alertsRetryQueueService: AlertsRetryQueueService,
+  private val objectMapper: ObjectMapper,
 ) : CreateMappingRetryable {
-  override suspend fun retryCreateMapping(message: String) {
-    TODO("Not yet implemented")
-  }
-
   suspend fun createAlert(alertEvent: AlertEvent) {
     val dpsAlertId = alertEvent.additionalInformation.alertUuid
     val offenderNo = alertEvent.additionalInformation.prisonNumber
@@ -27,23 +29,45 @@ class AlertsService(
     )
 
     if (alertEvent.wasCreatedInDPS()) {
-      val dpsAlert = dpsApiService.getAlert(dpsAlertId)
-      val nomisAlert = nomisApiService.createAlert(offenderNo, dpsAlert.toNomisCreateRequest())
-      val mapping = AlertMappingDto(
-        dpsAlertId = dpsAlertId,
-        nomisBookingId = nomisAlert.bookingId,
-        nomisAlertSequence = nomisAlert.alertSequence,
-        mappingType = AlertMappingDto.MappingType.DPS_CREATED,
-      )
-      mappingApiService.createMapping(mapping)
-      telemetryClient.trackEvent(
-        "alert-create-success",
-        telemetryMap + mapOf("nomisBookingId" to mapping.nomisBookingId.toString(), "nomisAlertSequence" to mapping.nomisAlertSequence.toString()),
-      )
+      synchronise {
+        name = "alert"
+        telemetryClient = this@AlertsService.telemetryClient
+        retryQueueService = alertsRetryQueueService
+        eventTelemetry = telemetryMap
+
+        checkMappingDoesNotExist {
+          mappingApiService.getOrNullByDpsId(dpsAlertId)
+        }
+        transform {
+          dpsApiService.getAlert(dpsAlertId)
+            .let { dpsAlert ->
+              nomisApiService.createAlert(offenderNo, dpsAlert.toNomisCreateRequest()).let { nomisAlert ->
+                AlertMappingDto(
+                  dpsAlertId = dpsAlertId,
+                  nomisBookingId = nomisAlert.bookingId,
+                  nomisAlertSequence = nomisAlert.alertSequence,
+                  mappingType = AlertMappingDto.MappingType.DPS_CREATED,
+                )
+              }
+            }
+        }
+        saveMapping { mappingApiService.createMapping(it) }
+      }
     } else {
       telemetryClient.trackEvent("alert-create-ignored", telemetryMap)
     }
   }
+
+  suspend fun createMapping(message: CreateMappingRetryMessage<AlertMappingDto>) =
+    mappingApiService.createMapping(message.mapping).also {
+      telemetryClient.trackEvent(
+        "alert-create-success",
+        message.telemetryAttributes,
+        null,
+      )
+    }
+
+  override suspend fun retryCreateMapping(message: String) = createMapping(message.fromJson())
 
   fun updateAlert(alertEvent: AlertEvent) {
     TODO("Not yet implemented")
@@ -52,6 +76,9 @@ class AlertsService(
   fun deleteAlert(alertEvent: AlertEvent) {
     TODO("Not yet implemented")
   }
+
+  private inline fun <reified T> String.fromJson(): T =
+    objectMapper.readValue(this)
 }
 
 data class AlertEvent(
