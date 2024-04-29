cbfe3dfe8fa34d3683890090b4cbfc705b6b2e91
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-04-29 17:55:27 +0100

    SDIT-1700: 🩺 Upgrade to reduce health false positive alerts (#571)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,10 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.4")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.data:spring-data-commons:3.2.5")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.3")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
@@ -35,10 +35,10 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
-  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
+  testImplementation("org.wiremock:wiremock-standalone:3.5.4")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.testcontainers:localstack:1.19.7")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.704")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.710")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
