91561245cc888006504b82c02271fc9bdbbbeca5
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-05-10 12:32:37 +0100

    SDIT-1730 Reconciliation processing for alerts
    CRON job added but disabled until we are ready

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsReconciliationService.kt
@@ -0,0 +1,94 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.alerts
+
+import com.microsoft.applicationinsights.TelemetryClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.ActivePrisonerId
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.NomisApiService
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.asPages
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.doApiCallWithRetries
+
+@Service
+class AlertsReconciliationService(
+  private val telemetryClient: TelemetryClient,
+  private val dpsAlertsApiService: AlertsDpsApiService,
+  private val nomisAlertsApiService: AlertsNomisApiService,
+  private val nomisApiService: NomisApiService,
+  @Value("\${reports.alerts.reconciliation.page-size}")
+  private val pageSize: Long = 20,
+) {
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  suspend fun generateReconciliationReport(activePrisonersCount: Long): List<MismatchAlerts> {
+    return activePrisonersCount.asPages(pageSize).flatMap { page ->
+      val activePrisoners = getActivePrisonersForPage(page)
+
+      withContext(Dispatchers.Unconfined) {
+        activePrisoners.map { async { checkActiveAlertsMatch(it) } }
+      }.awaitAll().filterNotNull()
+    }
+  }
+
+  private suspend fun getActivePrisonersForPage(page: Pair<Long, Long>) =
+    runCatching { nomisApiService.getActivePrisoners(page.first, page.second).content }
+      .onFailure {
+        telemetryClient.trackEvent(
+          "alerts-reports-reconciliation-mismatch-page-error",
+          mapOf(
+            "page" to page.first.toString(),
+          ),
+        )
+        log.error("Unable to match entire page of prisoners: $page", it)
+      }
+      .getOrElse { emptyList() }
+      .also { log.info("Page requested: $page, with ${it.size} active prisoners") }
+
+  suspend fun checkActiveAlertsMatch(prisonerId: ActivePrisonerId): MismatchAlerts? = runCatching {
+    val nomisAlerts = doApiCallWithRetries { nomisAlertsApiService.getAlertsForReconciliation(prisonerId.offenderNo) }.let { it.latestBookingAlerts + it.previousBookingsAlerts }
+      .map { it.alertCode.code }.toSortedSet()
+    val dpsAlerts = doApiCallWithRetries { dpsAlertsApiService.getActiveAlertsForPrisoner(prisonerId.offenderNo) }.map { it.alertCode.code }.toSortedSet()
+
+    val missingFromNomis = dpsAlerts - nomisAlerts
+    val missingFromDps = nomisAlerts - dpsAlerts
+    return if (missingFromNomis.isNotEmpty() || missingFromDps.isNotEmpty()) {
+      MismatchAlerts(offenderNo = prisonerId.offenderNo, missingFromDps = missingFromDps, missingFromNomis = missingFromNomis).also { mismatch ->
+        log.info("Alerts Mismatch found  $mismatch")
+        telemetryClient.trackEvent(
+          "alerts-reports-reconciliation-mismatch",
+          mapOf(
+            "offenderNo" to mismatch.offenderNo,
+            "bookingId" to prisonerId.bookingId.toString(),
+            "missingFromDps" to (mismatch.missingFromDps.joinToString()),
+            "missingFromNomis" to (mismatch.missingFromNomis.joinToString()),
+          ),
+        )
+      }
+    } else {
+      null
+    }
+  }.onFailure {
+    log.error("Unable to match alerts for prisoner with ${prisonerId.offenderNo} booking: ${prisonerId.bookingId}", it)
+    telemetryClient.trackEvent(
+      "alerts-reports-reconciliation-mismatch-error",
+      mapOf(
+        "offenderNo" to prisonerId.offenderNo,
+        "bookingId" to prisonerId.bookingId.toString(),
+      ),
+    )
+  }.getOrNull()
+}
+
+data class MismatchAlerts(
+  val offenderNo: String,
+  val missingFromDps: Set<String>,
+  val missingFromNomis: Set<String>,
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsResourceIntTest.kt
@@ -0,0 +1,248 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.alerts
+
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.untilAsserted
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.kotlin.any
+import org.mockito.kotlin.check
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.isNull
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.alerts.AlertsDpsApiExtension.Companion.alertsDpsApi
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.PrisonerAlertsResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.NomisApiExtension.Companion.nomisApi
+
+class AlertsResourceIntTest : IntegrationTestBase() {
+
+  @Captor
+  lateinit var telemetryCaptor: ArgumentCaptor<Map<String, String>>
+
+  @Autowired
+  private lateinit var alertsNomisApi: AlertsNomisApiMockServer
+
+  @DisplayName("PUT /alerts/reports/reconciliation")
+  @Nested
+  inner class GenerateAlertsReconciliationReport {
+    @BeforeEach
+    fun setUp() {
+      reset(telemetryClient)
+      val numberOfActivePrisoners = 34L
+      nomisApi.stubGetActivePrisonersInitialCount(numberOfActivePrisoners)
+      nomisApi.stubGetActivePrisonersPage(numberOfActivePrisoners, 0)
+      nomisApi.stubGetActivePrisonersPage(numberOfActivePrisoners, 1)
+      nomisApi.stubGetActivePrisonersPage(numberOfActivePrisoners, 2)
+      nomisApi.stubGetActivePrisonersPage(numberOfActivePrisoners, 3, 4)
+
+      // mock non-matching for first, second and last prisoners
+      alertsDpsApi.stubGetActiveAlertsForPrisoner(
+        "A0001TZ",
+        dpsAlert().copy(alertCode = dpsAlertCode("HPI")),
+        dpsAlert().copy(alertCode = dpsAlertCode("XA")),
+      )
+      alertsNomisApi.stubGetAlertsForReconciliation(
+        "A0001TZ",
+        response = PrisonerAlertsResponse(
+          latestBookingAlerts = listOf(alertResponse().copy(alertCode = alertCode("HPI"))),
+          previousBookingsAlerts = emptyList(),
+        ),
+      )
+
+      alertsDpsApi.stubGetActiveAlertsForPrisoner(
+        "A0002TZ",
+        dpsAlert().copy(alertCode = dpsAlertCode("HPI")),
+        dpsAlert().copy(alertCode = dpsAlertCode("XA")),
+      )
+      alertsNomisApi.stubGetAlertsForReconciliation(
+        "A0002TZ",
+        response = PrisonerAlertsResponse(
+          latestBookingAlerts = emptyList(),
+          previousBookingsAlerts = listOf(alertResponse().copy(alertCode = alertCode("HPI"))),
+        ),
+      )
+
+      alertsDpsApi.stubGetActiveAlertsForPrisoner(
+        "A0034TZ",
+        dpsAlert().copy(alertCode = dpsAlertCode("HPI")),
+        dpsAlert().copy(alertCode = dpsAlertCode("HA1")),
+      )
+      alertsNomisApi.stubGetAlertsForReconciliation(
+        "A0034TZ",
+        response = PrisonerAlertsResponse(
+          latestBookingAlerts = listOf(alertResponse().copy(alertCode = alertCode("HPI")), alertResponse().copy(alertCode = alertCode("HA2"))),
+          previousBookingsAlerts = listOf(alertResponse().copy(alertCode = alertCode("XA"))),
+        ),
+      )
+
+      // all others have alerts
+      (3..<numberOfActivePrisoners).forEach {
+        val offenderNo = "A${it.toString().padStart(4, '0')}TZ"
+        alertsDpsApi.stubGetActiveAlertsForPrisoner(
+          offenderNo,
+          dpsAlert().copy(alertCode = dpsAlertCode("HPI")),
+        )
+        alertsNomisApi.stubGetAlertsForReconciliation(
+          offenderNo,
+          response = PrisonerAlertsResponse(
+            latestBookingAlerts = listOf(alertResponse().copy(alertCode = alertCode("HPI"))),
+            previousBookingsAlerts = emptyList(),
+          ),
+        )
+      }
+    }
+
+    @Test
+    fun `will output report requested telemetry`() {
+      webTestClient.put().uri("/alerts/reports/reconciliation")
+        .exchange()
+        .expectStatus().isAccepted
+
+      verify(telemetryClient).trackEvent(eq("alerts-reports-reconciliation-requested"), check { assertThat(it).containsEntry("active-prisoners", "34") }, isNull())
+
+      awaitReportFinished()
+    }
+
+    @Test
+    fun `should execute batches of prisoners`() {
+      webTestClient.put().uri("/alerts/reports/reconciliation")
+        .exchange()
+        .expectStatus().isAccepted
+
+      awaitReportFinished()
+      nomisApi.verify(
+        WireMock.getRequestedFor(urlPathEqualTo("/prisoners/ids"))
+          .withQueryParam("size", WireMock.equalTo("1"))
+          .withQueryParam("active", WireMock.equalTo("true")),
+      )
+      nomisApi.verify(
+        // 34 prisoners will be spread over 4 pages of 10 prisoners each
+        4,
+        WireMock.getRequestedFor(urlPathEqualTo("/prisoners/ids"))
+          .withQueryParam("size", WireMock.equalTo("10"))
+          .withQueryParam("active", WireMock.equalTo("true")),
+      )
+    }
+
+    @Test
+    fun `should emit a mismatched custom event for each mismatch along with a summary`() {
+      webTestClient.put().uri("/alerts/reports/reconciliation")
+        .exchange()
+        .expectStatus().isAccepted
+
+      awaitReportFinished()
+
+      verify(telemetryClient).trackEvent(
+        eq("alerts-reports-reconciliation-report"),
+        check {
+          assertThat(it).containsEntry("mismatch-count", "3")
+          assertThat(it).containsEntry("A0001TZ", "missing-dps=0:missing-nomis=1")
+          assertThat(it).containsEntry("A0002TZ", "missing-dps=0:missing-nomis=1")
+          assertThat(it).containsEntry("A0034TZ", "missing-dps=2:missing-nomis=1")
+        },
+        isNull(),
+      )
+
+      verify(telemetryClient, times(3)).trackEvent(
+        eq("alerts-reports-reconciliation-mismatch"),
+        telemetryCaptor.capture(),
+        isNull(),
+      )
+
+      val mismatchedRecords = telemetryCaptor.allValues.map { it["offenderNo"] }
+
+      assertThat(mismatchedRecords).containsOnly("A0001TZ", "A0002TZ", "A0034TZ")
+      with(telemetryCaptor.allValues.find { it["offenderNo"] == "A0001TZ" }) {
+        assertThat(this).containsEntry("bookingId", "1")
+        assertThat(this).containsEntry("missingFromDps", "")
+        assertThat(this).containsEntry("missingFromNomis", "XA")
+      }
+      with(telemetryCaptor.allValues.find { it["offenderNo"] == "A0002TZ" }) {
+        assertThat(this).containsEntry("bookingId", "2")
+        assertThat(this).containsEntry("missingFromDps", "")
+        assertThat(this).containsEntry("missingFromNomis", "XA")
+      }
+      with(telemetryCaptor.allValues.find { it["offenderNo"] == "A0034TZ" }) {
+        assertThat(this).containsEntry("bookingId", "34")
+        assertThat(this).containsEntry("missingFromDps", "HA2, XA")
+        assertThat(this).containsEntry("missingFromNomis", "HA1")
+      }
+    }
+
+    @Test
+    fun `will attempt to complete a report even if some of the checks fail`() {
+      alertsDpsApi.stubGetActiveAlertsForPrisoner("A0002TZ", HttpStatus.INTERNAL_SERVER_ERROR)
+
+      webTestClient.put().uri("/alerts/reports/reconciliation")
+        .exchange()
+        .expectStatus().isAccepted
+
+      awaitReportFinished()
+
+      verify(telemetryClient, times(1)).trackEvent(
+        eq("alerts-reports-reconciliation-mismatch-error"),
+        any(),
+        isNull(),
+      )
+
+      verify(telemetryClient).trackEvent(
+        eq("alerts-reports-reconciliation-report"),
+        check {
+          assertThat(it).containsEntry("mismatch-count", "2")
+          assertThat(it).containsKeys("A0001TZ", "A0034TZ")
+        },
+        isNull(),
+      )
+    }
+
+    @Test
+    fun `when initial prison count fails the whole report fails`() {
+      nomisApi.stubGetActivePrisonersPageWithError(0, 500)
+
+      webTestClient.put().uri("/alerts/reports/reconciliation")
+        .exchange()
+        .expectStatus().is5xxServerError
+    }
+
+    @Test
+    fun `will attempt to complete a report even if whole pages of the checks fail`() {
+      nomisApi.stubGetActivePrisonersPageWithError(2, 500)
+
+      webTestClient.put().uri("/alerts/reports/reconciliation")
+        .exchange()
+        .expectStatus().isAccepted
+
+      awaitReportFinished()
+
+      verify(telemetryClient).trackEvent(
+        eq("alerts-reports-reconciliation-mismatch-page-error"),
+        any(),
+        isNull(),
+      )
+
+      verify(telemetryClient).trackEvent(
+        eq("alerts-reports-reconciliation-report"),
+        check {
+          assertThat(it).containsEntry("mismatch-count", "3")
+          assertThat(it).containsKeys("A0001TZ", "A0002TZ", "A0034TZ")
+        },
+        isNull(),
+      )
+    }
+  }
+
+  private fun awaitReportFinished() {
+    await untilAsserted { verify(telemetryClient).trackEvent(eq("alerts-reports-reconciliation-report"), any(), isNull()) }
+  }
+}
