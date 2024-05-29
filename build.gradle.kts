3a521e8ce74422b2e469bcfaa069f7df710594cd
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-05-29 14:56:13 +0100

    SDIT-1788: ⬆️ Upgrade to latest kotlin lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,10 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.data:spring-data-commons:3.3.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
