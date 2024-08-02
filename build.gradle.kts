1ab585dbac45fdcfe7d76206208b8d44b0f022b4
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-08-02 10:27:25 +0100

    SDIT-1919 Use Gradle Spring Boot plugin version 6.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.2"
   kotlin("plugin.spring") version "2.0.0"
-  id("org.openapi.generator") version "7.6.0"
+  id("org.openapi.generator") version "7.7.0"
 }
 
 configurations {
@@ -19,14 +19,14 @@ configurations {
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.data:spring-data-commons:3.3.1")
+  implementation("org.springframework.data:spring-data-commons:3.3.2")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.1.0")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk9")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.4.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.6.0")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22") {
@@ -34,10 +34,10 @@ dependencies {
   }
   testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.22")
 
-  testImplementation("org.wiremock:wiremock-standalone:3.7.0")
+  testImplementation("org.wiremock:wiremock-standalone:3.9.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.20.0")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.748")
+  testImplementation("org.testcontainers:localstack:1.20.1")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.767")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
