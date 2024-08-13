b19bb061532f1b984251a2907766084a7c9d96c6
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-08-13 12:54:01 +0100

    SDIT-1826 Add client for NOMIS API (#651)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonNomisApiService.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.prisonperson
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpsertPhysicalAttributesRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpsertPhysicalAttributesResponse
+
+@Service
+class PrisonPersonNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
+  suspend fun upsertPhysicalAttributes(offenderNo: String, height: Int?, weight: Int?): UpsertPhysicalAttributesResponse =
+    webClient.put()
+      .uri("/prisoners/{offenderNo}/physical-attributes", offenderNo)
+      .bodyValue(UpsertPhysicalAttributesRequest(height, weight))
+      .retrieve()
+      .awaitBody()
+}
