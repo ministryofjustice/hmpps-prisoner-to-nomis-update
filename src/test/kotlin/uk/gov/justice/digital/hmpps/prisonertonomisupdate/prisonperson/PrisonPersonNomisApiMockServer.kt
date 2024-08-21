0539cbd71899c393d0093a32c96fa50f162cb7bd
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2024-08-21 15:35:34 +0100

    SDIT-1828 Add prison person get reconciliation client (#668)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonNomisApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonNomisApiMockServer.kt
@@ -2,12 +2,15 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.prisonperson
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.put
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.urlMatching
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.PrisonPersonReconciliationResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpsertPhysicalAttributesResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.NomisApiExtension.Companion.nomisApi
 import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
@@ -36,6 +39,30 @@ class PrisonPersonNomisApiMockServer(private val objectMapper: ObjectMapper) {
           .withHeader("Content-Type", "application/json")
           .withStatus(errorStatus.value())
           .withBody(objectMapper.writeValueAsString(ErrorResponse(status = errorStatus, userMessage = "some error"))),
+      ),
+    )
+  }
+
+  fun stubGetReconciliation(
+    offenderNo: String = "A1234AA",
+    height: Int = 180,
+    weight: Int = 80,
+  ) = nomisApi.stubFor(
+    get(urlEqualTo("/prisoners/$offenderNo/physical-attributes/reconciliation")).willReturn(
+      aResponse()
+        .withHeader("Content-Type", "application/json")
+        .withStatus(HttpStatus.OK.value())
+        .withBody(objectMapper.writeValueAsString(PrisonPersonReconciliationResponse(offenderNo, height, weight))),
+    ),
+  )
+
+  fun stubGetReconciliation(status: HttpStatus, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    nomisApi.stubFor(
+      get(urlPathMatching("/prisoners/\\w+/physical-attributes/reconciliation")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(status.value())
+          .withBody(objectMapper.writeValueAsString(error)),
       ),
     )
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonNomisApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonNomisApiServiceTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.prisonperson
 
 import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
 import com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.matchingJsonPath
 import com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
@@ -9,8 +10,11 @@ import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.SpringAPIServiceTest
 
 @SpringAPIServiceTest
@@ -56,6 +60,50 @@ class PrisonPersonNomisApiServiceTest {
 
       assertThat(response.bookingId).isEqualTo(1234567)
       assertThat(response.created).isTrue()
+    }
+  }
+
+  @Nested
+  inner class GetReconciliation {
+    @Test
+    internal fun `will pass oath2 token to service`() = runTest {
+      prisonPersonNomisApiMockServer.stubGetReconciliation(offenderNo = "A1234KT")
+
+      apiService.getReconciliation(offenderNo = "A1234KT")
+
+      prisonPersonNomisApiMockServer.verify(
+        getRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    internal fun `will pass offender number to service`() = runTest {
+      prisonPersonNomisApiMockServer.stubGetReconciliation(offenderNo = "A1234KT")
+
+      apiService.getReconciliation(offenderNo = "A1234KT")
+
+      prisonPersonNomisApiMockServer.verify(
+        getRequestedFor(urlPathEqualTo("/prisoners/A1234KT/physical-attributes/reconciliation")),
+      )
+    }
+
+    @Test
+    fun `will return height and weight`() = runTest {
+      prisonPersonNomisApiMockServer.stubGetReconciliation(offenderNo = "A1234KT", height = 180, weight = 80)
+
+      val response = apiService.getReconciliation(offenderNo = "A1234KT")
+
+      assertThat(response.height).isEqualTo(180)
+      assertThat(response.weight).isEqualTo(80)
+    }
+
+    @Test
+    fun `will throw error when API returns an error`() = runTest {
+      prisonPersonNomisApiMockServer.stubGetReconciliation(INTERNAL_SERVER_ERROR)
+
+      assertThrows<WebClientResponseException.InternalServerError> {
+        apiService.getReconciliation(offenderNo = "A1234KT")
+      }
     }
   }
 }
