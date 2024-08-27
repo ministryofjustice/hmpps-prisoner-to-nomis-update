ee7d25db377a2854f0f37205b1dc281b3cdf4289
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-08-27 13:59:25 +0100

    SDIT-1829 Do not show difference values in telemetry

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonReconService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/prisonperson/PrisonPersonReconService.kt
@@ -48,10 +48,10 @@ class PrisonPersonReconService(
         async { doApiCallWithRetries { dpsApi.getPhysicalAttributes(offenderNo) } }
     }.awaitBoth()
 
-    val differences = findDifferences(offenderNo, nomisPrisoner, dpsPrisoner)
-    return if (differences.isNotEmpty()) {
+    val differenceTelemetry = findDifferenceTelemetry(offenderNo, nomisPrisoner, dpsPrisoner)
+    return if (differenceTelemetry.isNotEmpty()) {
       offenderNo.also {
-        telemetryClient.trackEvent("prison-person-reconciliation-prisoner-failed", differences)
+        telemetryClient.trackEvent("prison-person-reconciliation-prisoner-failed", differenceTelemetry)
       }
     } else {
       null
@@ -60,22 +60,21 @@ class PrisonPersonReconService(
     telemetryClient.trackEvent("prison-person-reconciliation-prisoner-error", mapOf("offenderNo" to offenderNo, "error" to (it.message ?: "unknown error")))
   }.getOrNull()
 
-  private fun findDifferences(offenderNo: String, nomisPrisoner: PrisonPersonReconciliationResponse?, dpsPrisoner: PhysicalAttributesDto?): MutableMap<String, String> {
+  private fun findDifferenceTelemetry(offenderNo: String, nomisPrisoner: PrisonPersonReconciliationResponse?, dpsPrisoner: PhysicalAttributesDto?): MutableMap<String, String> {
     val failureTelemetry = mutableMapOf<String, String>()
+    val differences = mutableListOf<String>()
 
-    // TODO SDIT-1829 Remove the values from failure telemetry and only mention which fields fail (then we don't have to worry about sensitive data in the future)
     if (nomisPrisoner?.height != dpsPrisoner?.height?.value) {
-      failureTelemetry["heightNomis"] = nomisPrisoner?.height.toString()
-      failureTelemetry["heightDps"] = dpsPrisoner?.height?.value.toString()
+      differences += "height"
     }
 
     if (nomisPrisoner?.weight != dpsPrisoner?.weight?.value) {
-      failureTelemetry["weightNomis"] = nomisPrisoner?.weight.toString()
-      failureTelemetry["weightDps"] = dpsPrisoner?.weight?.value.toString()
+      differences += "weight"
     }
 
-    if (failureTelemetry.isNotEmpty()) {
+    if (differences.size > 0) {
       failureTelemetry["offenderNo"] = offenderNo
+      failureTelemetry["differences"] = differences.joinToString()
     }
 
     // If we found any differences then advise if the prisoner is missing from either system
