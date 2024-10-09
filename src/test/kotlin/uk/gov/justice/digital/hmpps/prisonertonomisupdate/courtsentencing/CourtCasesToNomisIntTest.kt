fd797900a60b0b1796d83a0a297576386a0074b2
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2024-10-09 15:01:53 +0100

    SDIT-2034 ditch offence count and model changes (#732)
    * SDIT-2034 ditch offence count and model changes
    
    * SDIT-2034 ditch offence count and model changes
    
    ---------
    
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/courtsentencing/CourtCasesToNomisIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/courtsentencing/CourtCasesToNomisIntTest.kt
@@ -52,7 +52,6 @@ private const val DPS_COURT_CHARGE_2_ID = "9996aa44-642a-484a-a967-2d17b5c9c5a1"
 private const val DPS_COURT_CHARGE_3_ID = "4566aa44-642a-484a-a967-2d17b5c9c5a1"
 private const val DPS_COURT_CHARGE_4_ID = "1236aa44-642a-484a-a967-2d17b5c9c5a1"
 private const val OFFENDER_NO = "AB12345"
-private const val OFFENCES_COUNT = 1
 private const val DONCASTER_COURT_CODE = "DRBYYC"
 private const val PRISON_ID = "MDI"
 private const val CASE_REFERENCE = "G123456789"
@@ -727,12 +726,6 @@ class CourtCasesToNomisIntTest : SqsIntegrationTestBase() {
             )
             .withRequestBody(
               WireMock.matchingJsonPath(
-                "courtEventChargesToUpdate[0].offencesCount",
-                WireMock.equalTo("$OFFENCES_COUNT"),
-              ),
-            )
-            .withRequestBody(
-              WireMock.matchingJsonPath(
                 "courtEventChargesToUpdate[0].offenceEndDate",
                 WireMock.equalTo("$COURT_CHARGE_1_OFFENCE_END_DATE"),
               ),
@@ -753,12 +746,6 @@ class CourtCasesToNomisIntTest : SqsIntegrationTestBase() {
               WireMock.matchingJsonPath(
                 "courtEventChargesToUpdate[1].offenceCode",
                 WireMock.equalTo("$COURT_CHARGE_3_OFFENCE_CODE"),
-              ),
-            )
-            .withRequestBody(
-              WireMock.matchingJsonPath(
-                "courtEventChargesToUpdate[1].offencesCount",
-                WireMock.equalTo("$OFFENCES_COUNT"),
               ),
             )
             .withRequestBody(
@@ -787,12 +774,6 @@ class CourtCasesToNomisIntTest : SqsIntegrationTestBase() {
             )
             .withRequestBody(
               WireMock.matchingJsonPath(
-                "courtEventChargesToCreate[0].offencesCount",
-                WireMock.equalTo("$OFFENCES_COUNT"),
-              ),
-            )
-            .withRequestBody(
-              WireMock.matchingJsonPath(
                 "courtEventChargesToCreate[0].offenceEndDate",
                 WireMock.equalTo("$COURT_CHARGE_2_OFFENCE_END_DATE"),
               ),
@@ -813,12 +794,6 @@ class CourtCasesToNomisIntTest : SqsIntegrationTestBase() {
               WireMock.matchingJsonPath(
                 "courtEventChargesToCreate[1].offenceCode",
                 WireMock.equalTo("$COURT_CHARGE_4_OFFENCE_CODE"),
-              ),
-            )
-            .withRequestBody(
-              WireMock.matchingJsonPath(
-                "courtEventChargesToCreate[1].offencesCount",
-                WireMock.equalTo("$OFFENCES_COUNT"),
               ),
             )
             .withRequestBody(
