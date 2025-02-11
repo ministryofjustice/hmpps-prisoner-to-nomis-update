f9f2ba78655ffbda868e4b7cd8b875c2ae9b489c
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-02-11 11:14:58 +0000

    SDIT-2561: ⬆️  Upgrade hmpps spring boot plugin to 7.7.1 (#890)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.1"
   kotlin("plugin.spring") version "2.1.10"
   id("org.openapi.generator") version "7.11.0"
 }
