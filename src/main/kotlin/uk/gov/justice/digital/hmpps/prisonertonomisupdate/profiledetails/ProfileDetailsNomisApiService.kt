58ba04eb3cd16ec743ff4a929329a0b59cf48dcb
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-02-25 14:46:18 +0000

    SDIT-2576 Add NOMIS API client

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/profiledetails/ProfileDetailsNomisApiService.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.profiledetails
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpsertProfileDetailsRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomissync.model.UpsertProfileDetailsResponse
+
+@Service
+class ProfileDetailsNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
+  suspend fun upsertProfileDetails(offenderNo: String, profileType: String, profileCode: String?): UpsertProfileDetailsResponse = webClient.put()
+    .uri("/prisoners/{offenderNo}/profile-details", offenderNo)
+    .bodyValue(UpsertProfileDetailsRequest(profileType, profileCode))
+    .retrieve()
+    .awaitBody()
+}
