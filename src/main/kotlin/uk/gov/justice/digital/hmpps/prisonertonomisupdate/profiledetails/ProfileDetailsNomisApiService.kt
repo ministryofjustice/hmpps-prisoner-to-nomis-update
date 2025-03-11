f4474728e6e3151a19fbb39d28bfce8987de1d1f
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-03-11 14:37:54 +0000

    SDIT-2584 Add client for get profile details (#962)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/profiledetails/ProfileDetailsNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/profiledetails/ProfileDetailsNomisApiService.kt
@@ -4,14 +4,36 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.PrisonerProfileDetailsResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertProfileDetailsRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertProfileDetailsResponse
 
 @Service
 class ProfileDetailsNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
-  suspend fun upsertProfileDetails(offenderNo: String, profileType: String, profileCode: String?): UpsertProfileDetailsResponse = webClient.put()
+  suspend fun upsertProfileDetails(
+    offenderNo: String,
+    profileType: String,
+    profileCode: String?,
+  ): UpsertProfileDetailsResponse = webClient.put()
     .uri("/prisoners/{offenderNo}/profile-details", offenderNo)
     .bodyValue(UpsertProfileDetailsRequest(profileType, profileCode))
     .retrieve()
     .awaitBody()
+
+  suspend fun getProfileDetails(
+    offenderNo: String,
+    profileTypes: List<String>,
+    latestBookingOnly: Boolean = false,
+    bookingId: Long? = null,
+  ): PrisonerProfileDetailsResponse? = webClient.get()
+    .uri {
+      it.path("/prisoners/{offenderNo}/profile-details")
+        .queryParam("profileTypes", profileTypes)
+        .apply { bookingId?.run { queryParam("bookingId", "$bookingId") } }
+        .queryParam("latestBookingOnly", "$latestBookingOnly")
+        .build(offenderNo)
+    }
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/profiledetails/ProfileDetailsNomisApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/profiledetails/ProfileDetailsNomisApiMockServer.kt
@@ -2,15 +2,21 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.profiledetails
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.put
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.urlMatching
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.BookingProfileDetailsResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.PrisonerProfileDetailsResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.ProfileDetailsResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertProfileDetailsResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.NomisApiExtension.Companion.nomisApi
 import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+import java.time.LocalDateTime
 
 @Component
 class ProfileDetailsNomisApiMockServer(private val objectMapper: ObjectMapper) {
@@ -32,6 +38,60 @@ class ProfileDetailsNomisApiMockServer(private val objectMapper: ObjectMapper) {
   fun stubPutProfileDetails(errorStatus: HttpStatus) {
     nomisApi.stubFor(
       put(urlMatching("/prisoners/.*/profile-details")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(errorStatus.value())
+          .withBody(objectMapper.writeValueAsString(ErrorResponse(status = errorStatus, userMessage = "some error"))),
+      ),
+    )
+  }
+
+  fun stubGetProfileDetails(
+    offenderNo: String,
+    response: PrisonerProfileDetailsResponse = PrisonerProfileDetailsResponse(
+      offenderNo = offenderNo,
+      bookings = listOf(
+        BookingProfileDetailsResponse(
+          bookingId = 1,
+          startDateTime = LocalDateTime.parse("2024-02-03T12:34:56"),
+          latestBooking = true,
+          profileDetails = listOf(
+            ProfileDetailsResponse(
+              type = "MARITAL",
+              code = "M",
+              createDateTime = LocalDateTime.now(),
+              createdBy = "A_USER",
+              modifiedDateTime = LocalDateTime.now(),
+              modifiedBy = "ANOTHER_USER",
+              auditModuleName = "NOMIS",
+            ),
+            ProfileDetailsResponse(
+              type = "CHILD",
+              code = "3",
+              createDateTime = LocalDateTime.now(),
+              createdBy = "A_USER",
+              modifiedDateTime = LocalDateTime.now(),
+              modifiedBy = "ANOTHER_USER",
+              auditModuleName = "NOMIS",
+            ),
+          ),
+        ),
+      ),
+    ),
+  ) {
+    nomisApi.stubFor(
+      get(urlPathEqualTo("/prisoners/$offenderNo/profile-details")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value())
+          .withBody(objectMapper.writeValueAsString(response)),
+      ),
+    )
+  }
+
+  fun stubGetProfileDetails(offenderNo: String, errorStatus: HttpStatus) {
+    nomisApi.stubFor(
+      get(urlPathEqualTo("/prisoners/$offenderNo/profile-details")).willReturn(
         aResponse()
           .withHeader("Content-Type", "application/json")
           .withStatus(errorStatus.value())
