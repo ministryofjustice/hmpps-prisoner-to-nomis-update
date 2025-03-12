eca12b11e0024097d04d7e3c58d04a2033d43745
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-03-12 15:34:44 +0000

    SDIT-2584 Test reconciliation failures and errors (#970)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/personalrelationships/profiledetails/ContactPersonProfileDetailsReconciliationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/personalrelationships/profiledetails/ContactPersonProfileDetailsReconciliationIntTest.kt
@@ -18,9 +18,11 @@ import org.mockito.kotlin.eq
 import org.mockito.kotlin.isNull
 import org.mockito.kotlin.never
 import org.mockito.kotlin.reset
+import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.BAD_GATEWAY
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.ProfileDetailsResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.personalrelationships.profiledetails.ContactPersonProfileDetailsReconciliationService.Companion.TELEMETRY_PREFIX
@@ -195,7 +197,7 @@ class ContactPersonProfileDetailsReconciliationIntTest(
         ),
       )
       dpsApi.stubGetDomesticStatus(prisonerNumber = "A1234BC", domesticStatus(domesticStatusCode = "M"))
-      dpsApi.stubGetNumberOfChildren(prisonerNumber = "A1234BC", HttpStatus.BAD_GATEWAY)
+      dpsApi.stubGetNumberOfChildren(prisonerNumber = "A1234BC", BAD_GATEWAY)
 
       service.checkPrisoner("A1234BC").also {
         assertThat(it).isNull()
@@ -314,6 +316,242 @@ class ContactPersonProfileDetailsReconciliationIntTest(
         .also {
           await untilAsserted { verify(telemetryClient).trackEvent(eq("$TELEMETRY_PREFIX-report-$it"), any(), isNull()) }
         }
+    }
+
+    @Nested
+    inner class Failures {
+      @Test
+      fun `should handle a mismatched prisoner`() {
+        reset(telemetryClient)
+        stubPages()
+        forEachPrisoner { prisonerNumber ->
+          dpsApi.stubGetDomesticStatus(prisonerNumber, domesticStatus(domesticStatusCode = "M"))
+          dpsApi.stubGetNumberOfChildren(prisonerNumber, numberOfChildren(numberOfChildren = "2"))
+          // Stub a mismatch on the 4th prisoner
+          if (prisonerNumber == "A0004TZ") {
+            stubGetProfileDetails(
+              prisonerNumber,
+              listOf(
+                profileDetails(type = "MARITAL", code = "D"),
+                profileDetails(type = "CHILD", code = "3"),
+              ),
+            )
+          } else {
+            stubGetProfileDetails(
+              prisonerNumber,
+              listOf(
+                profileDetails(type = "MARITAL", code = "M"),
+                profileDetails(type = "CHILD", code = "2"),
+              ),
+            )
+          }
+        }
+
+        runReconciliation(expectSuccess = false)
+
+        // should publish failure telemetry for prisoner
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-prisoner-failed"),
+          check {
+            assertThat(it).containsAllEntriesOf(
+              mapOf(
+                "offenderNo" to "A0004TZ",
+                "differences" to "domestic-status, number-of-children",
+              ),
+            )
+          },
+          isNull(),
+        )
+
+        // should publish failure telemetry for report
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-report-failed"),
+          check {
+            assertThat(it).containsEntry("active-prisoners", noActivePrisoners.toString())
+            assertThat(it).containsEntry("mismatch-count", "1")
+            assertThat(it).containsEntry("mismatch-prisoners", "[A0004TZ]")
+          },
+          isNull(),
+        )
+      }
+    }
+
+    @Nested
+    inner class Errors {
+      @Test
+      fun `API call should fail if error getting prisoner count`() {
+        reset(telemetryClient)
+        NomisApiExtension.nomisApi.stubGetActivePrisonersPageWithError(pageNumber = 0, pageSize = 1, responseCode = 502)
+
+        webTestClient.put().uri("/contact-person/profile-details/reports/reconciliation")
+          .exchange()
+          .expectStatus().is5xxServerError
+      }
+
+      @Test
+      fun `should handle an error getting one of the prisoner pages`() {
+        reset(telemetryClient)
+        NomisApiExtension.nomisApi.apply {
+          stubGetActivePrisonersInitialCount(noActivePrisoners)
+          stubGetActivePrisonersPage(noActivePrisoners, pageNumber = 0, pageSize = pageSize, numberOfElements = pageSize)
+          // fail to retrieve page 1
+          stubGetActivePrisonersPageWithError(pageNumber = 1, pageSize = pageSize, responseCode = 500)
+          stubGetActivePrisonersPage(noActivePrisoners, pageNumber = 2, pageSize = pageSize, numberOfElements = 1)
+        }
+        forEachPrisoner { prisonerNumber ->
+          stubGetProfileDetails(
+            prisonerNumber,
+            listOf(
+              profileDetails(type = "MARITAL", code = "M"),
+              profileDetails(type = "CHILD", code = "2"),
+            ),
+          )
+          dpsApi.stubGetDomesticStatus(prisonerNumber, domesticStatus(domesticStatusCode = "M"))
+          dpsApi.stubGetNumberOfChildren(prisonerNumber, numberOfChildren(numberOfChildren = "2"))
+        }
+
+        runReconciliation()
+
+        // should call DPS and NOMIS for each prisoner on successful pages
+        forEachPrisoner { prisonerNumber ->
+          // for prisoners from the 2nd page, we should not call the APIs
+          val count = if (listOf("A0004TZ", "A0005TZ", "A0006TZ").contains(prisonerNumber)) 0 else 1
+          nomisApi.verify(count, getRequestedFor(urlPathEqualTo("/prisoners/$prisonerNumber/profile-details")))
+          dpsApi.verify(count, getRequestedFor(urlPathEqualTo("/sync/$prisonerNumber/domestic-status")))
+          dpsApi.verify(count, getRequestedFor(urlPathEqualTo("/sync/$prisonerNumber/number-of-children")))
+        }
+
+        // should publish error telemetry for page
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-page-error"),
+          check {
+            assertThat(it).containsEntry("page", "1")
+            assertThat(it).containsEntry(
+              "error",
+              "500 Internal Server Error from GET http://localhost:8082/prisoners/ids/active",
+            )
+          },
+          isNull(),
+        )
+
+        // should publish success telemetry for report
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-report-success"),
+          check {
+            assertThat(it).containsEntry("active-prisoners", noActivePrisoners.toString())
+            assertThat(it).containsEntry("mismatch-count", "0")
+          },
+          isNull(),
+        )
+      }
+
+      @Test
+      fun `should handle an error getting a single prisoner`() {
+        reset(telemetryClient)
+        stubPages()
+        forEachPrisoner { prisonerNumber ->
+          dpsApi.stubGetDomesticStatus(prisonerNumber, domesticStatus(domesticStatusCode = "M"))
+          dpsApi.stubGetNumberOfChildren(prisonerNumber, numberOfChildren(numberOfChildren = "2"))
+          // the 4th prisoner's NOMIS call returns an error
+          if (prisonerNumber == "A0004TZ") {
+            nomisApi.stubGetProfileDetails(prisonerNumber, BAD_GATEWAY)
+          } else {
+            stubGetProfileDetails(
+              prisonerNumber,
+              listOf(
+                profileDetails(type = "MARITAL", code = "M"),
+                profileDetails(type = "CHILD", code = "2"),
+              ),
+            )
+          }
+        }
+
+        runReconciliation()
+
+        // should publish error telemetry for prisoner
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-prisoner-error"),
+          check {
+            assertThat(it).containsEntry("offenderNo", "A0004TZ")
+            assertThat(it).containsEntry(
+              "error",
+              "502 Bad Gateway from GET http://localhost:8082/prisoners/A0004TZ/profile-details",
+            )
+          },
+          isNull(),
+        )
+
+        // should publish success telemetry for report despite error
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-report-success"),
+          check {
+            assertThat(it).containsEntry("active-prisoners", noActivePrisoners.toString())
+            assertThat(it).containsEntry("mismatch-count", "0")
+          },
+          isNull(),
+        )
+      }
+
+      @Test
+      fun `should handle handle connection failures and retry both DPS and NOMIS`() {
+        reset(telemetryClient)
+        stubPages()
+        forEachPrisoner { prisonerNumber ->
+          // the 4th prisoner needs a retry on both NOMIS and DPS endpoints
+          if (prisonerNumber == "A0004TZ") {
+            nomisApi.stubGetProfileDetailsAfterRetry(
+              prisonerNumber,
+              profileDetailsResponse(
+                offenderNo = prisonerNumber,
+                bookings = listOf(
+                  booking(
+                    profileDetails = listOf(
+                      profileDetails(type = "MARITAL", code = "M"),
+                      profileDetails(type = "CHILD", code = "2"),
+                    ),
+                  ),
+                ),
+              ),
+            )
+            dpsApi.stubGetDomesticStatusAfterRetry(prisonerNumber, domesticStatus(domesticStatusCode = "M"))
+            dpsApi.stubGetNumberOfChildrenAfterRetry(prisonerNumber, numberOfChildren(numberOfChildren = "2"))
+          } else {
+            stubGetProfileDetails(
+              prisonerNumber,
+              listOf(
+                profileDetails(type = "MARITAL", code = "M"),
+                profileDetails(type = "CHILD", code = "2"),
+              ),
+            )
+            dpsApi.stubGetDomesticStatus(prisonerNumber, domesticStatus(domesticStatusCode = "M"))
+            dpsApi.stubGetNumberOfChildren(prisonerNumber, numberOfChildren(numberOfChildren = "2"))
+          }
+        }
+
+        runReconciliation()
+
+        // should retry the NOMIS and DPS calls for the failed prisoner
+        nomisApi.verify(2, getRequestedFor(urlPathEqualTo("/prisoners/A0004TZ/profile-details")))
+        dpsApi.verify(2, getRequestedFor(urlPathEqualTo("/sync/A0004TZ/domestic-status")))
+        dpsApi.verify(2, getRequestedFor(urlPathEqualTo("/sync/A0004TZ/number-of-children")))
+
+        // should NOT publish error telemetry for prisoner
+        verify(telemetryClient, times(0)).trackEvent(
+          eq("$TELEMETRY_PREFIX-prisoner-error"),
+          anyMap(),
+          isNull(),
+        )
+
+        // should publish success telemetry
+        verify(telemetryClient).trackEvent(
+          eq("$TELEMETRY_PREFIX-report-success"),
+          check {
+            assertThat(it).containsEntry("active-prisoners", noActivePrisoners.toString())
+            assertThat(it).containsEntry("mismatch-count", "0")
+          },
+          isNull(),
+        )
+      }
     }
   }
 
