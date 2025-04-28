ca3ed86c62c0d816554866dcbaa37f90f9187a03
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-04-28 16:21:14 +0100

    SDIT-2733: ⬆️  Upgrade localstack to v4 (#1098)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/integration/LocalStackContainer.kt
@@ -27,7 +27,7 @@ object LocalStackContainer {
     log.info("Creating a localstack instance")
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("3"),
+      DockerImageName.parse("localstack/localstack").withTag("4"),
     ).apply {
       withServices(LocalStackContainer.Service.SNS, LocalStackContainer.Service.SQS)
       withEnv("DEFAULT_REGION", "eu-west-2")
