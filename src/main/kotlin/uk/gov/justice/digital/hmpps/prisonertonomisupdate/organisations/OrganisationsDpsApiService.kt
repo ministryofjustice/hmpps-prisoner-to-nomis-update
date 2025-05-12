19c2aea5141efbd0dd6b332f408dc68cb13e6cf0
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-05-12 16:28:09 +0100

    SDIT-2413: ✨ Add in calls to DPS sync endpoints

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsDpsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsDpsApiService.kt
@@ -8,6 +8,13 @@ import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNul
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyWithRetry
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.OrganisationDetails
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.PagedModelSyncOrganisationId
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncAddressPhoneResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncAddressResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncEmailResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncOrganisationResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncPhoneResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncTypesResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.model.SyncWebResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
 
 @Service
@@ -34,6 +41,41 @@ class OrganisationsDpsApiService(
         .queryParam("size", pageSize)
         .build()
     }
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisation(organisationId: Long): SyncOrganisationResponse = webClient.get()
+    .uri("/sync/organisation/{organisationId}", organisationId)
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisationWeb(organisationWebId: Long): SyncWebResponse = webClient.get()
+    .uri("/sync/organisation-web/{organisationWebId}", organisationWebId)
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisationTypes(organisationId: Long): SyncTypesResponse = webClient.get()
+    .uri("/sync/organisation-types/{organisationId}", organisationId)
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisationPhone(organisationPhoneId: Long): SyncPhoneResponse = webClient.get()
+    .uri("/sync/organisation-phone/{organisationPhoneId}", organisationPhoneId)
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisationEmail(organisationEmailId: Long): SyncEmailResponse = webClient.get()
+    .uri("/sync/organisation-email/{organisationEmailId}", organisationEmailId)
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisationAddress(organisationAddressId: Long): SyncAddressResponse = webClient.get()
+    .uri("/sync/organisation-address/{organisationAddressId}", organisationAddressId)
+    .retrieve()
+    .awaitBodyWithRetry(backoffSpec)
+
+  suspend fun getSyncOrganisationAddressPhone(organisationAddressPhoneId: Long): SyncAddressPhoneResponse = webClient.get()
+    .uri("/sync/organisation-address-phone/{organisationAddressPhoneId}", organisationAddressPhoneId)
     .retrieve()
     .awaitBodyWithRetry(backoffSpec)
 }
