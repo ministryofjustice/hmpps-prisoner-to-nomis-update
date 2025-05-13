312f2866c840e3a44567b488b7169277b2652320
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-05-13 14:16:22 +0100

    SDIT-2413: ✨ Add in calls to NOMIS sync endpoints (#1150)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsNomisApiService.kt
@@ -6,10 +6,25 @@ import org.springframework.data.domain.PageImpl
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBodilessEntity
+import org.springframework.web.reactive.function.client.awaitBody
 import reactor.util.context.Context
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyWithRetry
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CorporateOrganisation
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CorporateOrganisationIdResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateAddressRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateAddressResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateEmailRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateEmailResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateOrganisationRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporatePhoneRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporatePhoneResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateWebAddressRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateCorporateWebAddressResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateCorporateAddressRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateCorporateEmailRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateCorporateOrganisationRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateCorporatePhoneRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateCorporateWebAddressRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RestResponsePage
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.typeReference
@@ -49,6 +64,188 @@ class OrganisationsNomisApiService(
   suspend fun deleteCorporateOrganisation(corporateId: Long) {
     webClient.delete()
       .uri("/corporates/{corporateId}", corporateId)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun createCorporate(request: CreateCorporateOrganisationRequest) {
+    webClient.post()
+      .uri(
+        "/corporates",
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun updateCorporate(corporateId: Long, request: UpdateCorporateOrganisationRequest) {
+    webClient.put()
+      .uri(
+        "/corporates/{corporateId}",
+        corporateId,
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun deleteCorporate(corporateId: Long) {
+    webClient.delete()
+      .uri(
+        "/corporates/{corporateId}",
+        corporateId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun createCorporateWebAddress(corporateId: Long, request: CreateCorporateWebAddressRequest): CreateCorporateWebAddressResponse = webClient.post()
+    .uri(
+      "/corporates/{corporateId}/web-address",
+      corporateId,
+    )
+    .bodyValue(request)
+    .retrieve()
+    .awaitBody()
+  suspend fun updateCorporateWebAddress(corporateId: Long, webAddressId: Long, request: UpdateCorporateWebAddressRequest) {
+    webClient.put()
+      .uri(
+        "/corporates/{corporateId}/web-address/{webAddressId}",
+        corporateId,
+        webAddressId,
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun deleteCorporateWebAddress(corporateId: Long, webAddressId: Long) {
+    webClient.delete()
+      .uri(
+        "/corporates/{corporateId}/web-address/{webAddressId}",
+        corporateId,
+        webAddressId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun createCorporatePhone(corporateId: Long, request: CreateCorporatePhoneRequest): CreateCorporatePhoneResponse = webClient.post()
+    .uri(
+      "/corporates/{corporateId}/phone",
+      corporateId,
+    )
+    .bodyValue(request)
+    .retrieve()
+    .awaitBody()
+  suspend fun updateCorporatePhone(corporateId: Long, phoneId: Long, request: UpdateCorporatePhoneRequest) {
+    webClient.put()
+      .uri(
+        "/corporates/{corporateId}/phone/{phoneId}",
+        corporateId,
+        phoneId,
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun deleteCorporatePhone(corporateId: Long, phoneId: Long) {
+    webClient.delete()
+      .uri(
+        "/corporates/{corporateId}/phone/{phoneId}",
+        corporateId,
+        phoneId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun createCorporateEmail(corporateId: Long, request: CreateCorporateEmailRequest): CreateCorporateEmailResponse = webClient.post()
+    .uri(
+      "/corporates/{corporateId}/email",
+      corporateId,
+    )
+    .bodyValue(request)
+    .retrieve()
+    .awaitBody()
+  suspend fun updateCorporateEmail(corporateId: Long, emailId: Long, request: UpdateCorporateEmailRequest) {
+    webClient.put()
+      .uri(
+        "/corporates/{corporateId}/email/{emailId}",
+        corporateId,
+        emailId,
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun deleteCorporateEmail(corporateId: Long, emailId: Long) {
+    webClient.delete()
+      .uri(
+        "/corporates/{corporateId}/email/{emailId}",
+        corporateId,
+        emailId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun createCorporateAddress(corporateId: Long, request: CreateCorporateAddressRequest): CreateCorporateAddressResponse = webClient.post()
+    .uri(
+      "/corporates/{corporateId}/address",
+      corporateId,
+    )
+    .bodyValue(request)
+    .retrieve()
+    .awaitBody()
+  suspend fun updateCorporateAddress(corporateId: Long, addressId: Long, request: UpdateCorporateAddressRequest) {
+    webClient.put()
+      .uri(
+        "/corporates/{corporateId}/address/{addressId}",
+        corporateId,
+        addressId,
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun deleteCorporateAddress(corporateId: Long, addressId: Long) {
+    webClient.delete()
+      .uri(
+        "/corporates/{corporateId}/address/{addressId}",
+        corporateId,
+        addressId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun createCorporateAddressPhone(corporateId: Long, addressId: Long, request: CreateCorporatePhoneRequest): CreateCorporatePhoneResponse = webClient.post()
+    .uri(
+      "/corporates/{corporateId}/address/{addressId}/phone",
+      corporateId,
+      addressId,
+    )
+    .bodyValue(request)
+    .retrieve()
+    .awaitBody()
+  suspend fun updateCorporateAddressPhone(corporateId: Long, addressId: Long, phoneId: Long, request: UpdateCorporatePhoneRequest) {
+    webClient.put()
+      .uri(
+        "/corporates/{corporateId}/address/{addressId}/phone/{phoneId}",
+        corporateId,
+        addressId,
+        phoneId,
+      )
+      .bodyValue(request)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+  suspend fun deleteCorporateAddressPhone(corporateId: Long, addressId: Long, phoneId: Long) {
+    webClient.delete()
+      .uri(
+        "/corporates/{corporateId}/address/{addressId}/phone/{phoneId}",
+        corporateId,
+        addressId,
+        phoneId,
+      )
       .retrieve()
       .awaitBodilessEntity()
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsNomisApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsNomisApiServiceTest.kt
@@ -4,6 +4,8 @@ import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
 import com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
@@ -13,6 +15,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Import
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.SpringAPIServiceTest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CorporateOrganisationIdResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporateAddressRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporateAddressResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporateEmailRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporateEmailResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporatePhoneRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporatePhoneResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporateWebAddressRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.createCorporateWebAddressResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.updateCorporateAddressRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.updateCorporateEmailRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.updateCorporatePhoneRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.organisations.OrganisationsNomisApiMockServer.Companion.updateCorporateWebAddressRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
 
 @SpringAPIServiceTest
@@ -109,27 +123,439 @@ class OrganisationsNomisApiServiceTest {
   }
 
   @Nested
-  inner class DeleteCorporateOrganisation {
-    @Test
-    internal fun `will pass oath2 token to service`() = runTest {
-      mockServer.stubDeleteCorporateOrganisation(corporateId = 1234567)
+  inner class WebAddresses {
+    @Nested
+    inner class CreateCorporateWebAddress {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubCreateCorporateWebAddress(corporateId = 1234567)
 
-      apiService.deleteCorporateOrganisation(corporateId = 1234567)
+        apiService.createCorporateWebAddress(corporateId = 1234567, request = createCorporateWebAddressRequest())
 
-      mockServer.verify(
-        deleteRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
-      )
+        mockServer.verify(
+          postRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call create endpoint`() = runTest {
+        mockServer.stubCreateCorporateWebAddress(corporateId = 1234567)
+
+        apiService.createCorporateWebAddress(corporateId = 1234567, request = createCorporateWebAddressRequest())
+
+        mockServer.verify(
+          postRequestedFor(urlPathEqualTo("/corporates/1234567/web-address")),
+        )
+      }
+
+      @Test
+      fun `will return webAddress id `() = runTest {
+        mockServer.stubCreateCorporateWebAddress(corporateId = 1234567, response = createCorporateWebAddressResponse().copy(id = 123456))
+
+        val response = apiService.createCorporateWebAddress(corporateId = 1234567, request = createCorporateWebAddressRequest())
+
+        assertThat(response.id).isEqualTo(123456)
+      }
     }
 
-    @Test
-    internal fun `will pass nomis corporate Id to service`() = runTest {
-      mockServer.stubDeleteCorporateOrganisation(corporateId = 1234567)
+    @Nested
+    inner class UpdateCorporateWebAddress {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubUpdateCorporateWebAddress(corporateId = 1234567, webAddressId = 7655)
 
-      apiService.deleteCorporateOrganisation(corporateId = 1234567)
+        apiService.updateCorporateWebAddress(corporateId = 1234567, webAddressId = 7655, request = updateCorporateWebAddressRequest())
 
-      mockServer.verify(
-        deleteRequestedFor(urlPathEqualTo("/corporates/1234567")),
-      )
+        mockServer.verify(
+          putRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call update endpoint`() = runTest {
+        mockServer.stubUpdateCorporateWebAddress(corporateId = 1234567, webAddressId = 7655)
+
+        apiService.updateCorporateWebAddress(corporateId = 1234567, webAddressId = 7655, request = updateCorporateWebAddressRequest())
+
+        mockServer.verify(
+          putRequestedFor(urlPathEqualTo("/corporates/1234567/web-address/7655")),
+        )
+      }
+    }
+
+    @Nested
+    inner class DeleteCorporateWebAddress {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubDeleteCorporateWebAddress(corporateId = 1234567, webAddressId = 7655)
+
+        apiService.deleteCorporateWebAddress(corporateId = 1234567, webAddressId = 7655)
+
+        mockServer.verify(
+          deleteRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call delete endpoint`() = runTest {
+        mockServer.stubDeleteCorporateWebAddress(corporateId = 1234567, webAddressId = 7655)
+
+        apiService.deleteCorporateWebAddress(corporateId = 1234567, webAddressId = 7655)
+
+        mockServer.verify(
+          deleteRequestedFor(urlPathEqualTo("/corporates/1234567/web-address/7655")),
+        )
+      }
+    }
+  }
+
+  @Nested
+  inner class Phones {
+
+    @Nested
+    inner class CreateCorporatePhone {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubCreateCorporatePhone(corporateId = 1234567)
+
+        apiService.createCorporatePhone(corporateId = 1234567, request = createCorporatePhoneRequest())
+
+        mockServer.verify(
+          postRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call create endpoint`() = runTest {
+        mockServer.stubCreateCorporatePhone(corporateId = 1234567)
+
+        apiService.createCorporatePhone(corporateId = 1234567, request = createCorporatePhoneRequest())
+
+        mockServer.verify(
+          postRequestedFor(urlPathEqualTo("/corporates/1234567/phone")),
+        )
+      }
+
+      @Test
+      fun `will return phone id `() = runTest {
+        mockServer.stubCreateCorporatePhone(corporateId = 1234567, response = createCorporatePhoneResponse().copy(id = 123456))
+
+        val response = apiService.createCorporatePhone(corporateId = 1234567, request = createCorporatePhoneRequest())
+
+        assertThat(response.id).isEqualTo(123456)
+      }
+    }
+
+    @Nested
+    inner class UpdateCorporatePhone {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubUpdateCorporatePhone(corporateId = 1234567, phoneId = 65543)
+
+        apiService.updateCorporatePhone(corporateId = 1234567, phoneId = 65543, updateCorporatePhoneRequest())
+
+        mockServer.verify(
+          putRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call update endpoint`() = runTest {
+        mockServer.stubUpdateCorporatePhone(corporateId = 1234567, phoneId = 65543)
+
+        apiService.updateCorporatePhone(corporateId = 1234567, phoneId = 65543, updateCorporatePhoneRequest())
+
+        mockServer.verify(
+          putRequestedFor(urlPathEqualTo("/corporates/1234567/phone/65543")),
+        )
+      }
+    }
+
+    @Nested
+    inner class DeleteCorporatePhone {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubDeleteCorporatePhone(corporateId = 1234567, phoneId = 65543)
+
+        apiService.deleteCorporatePhone(corporateId = 1234567, phoneId = 65543)
+
+        mockServer.verify(
+          deleteRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call delete endpoint`() = runTest {
+        mockServer.stubDeleteCorporatePhone(corporateId = 1234567, phoneId = 65543)
+
+        apiService.deleteCorporatePhone(corporateId = 1234567, phoneId = 65543)
+
+        mockServer.verify(
+          deleteRequestedFor(urlPathEqualTo("/corporates/1234567/phone/65543")),
+        )
+      }
+    }
+  }
+
+  @Nested
+  inner class Emails {
+    @Nested
+    inner class CreateCorporateEmail {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubCreateCorporateEmail(corporateId = 1234567)
+
+        apiService.createCorporateEmail(corporateId = 1234567, request = createCorporateEmailRequest())
+
+        mockServer.verify(
+          postRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call create endpoint`() = runTest {
+        mockServer.stubCreateCorporateEmail(corporateId = 1234567)
+
+        apiService.createCorporateEmail(corporateId = 1234567, request = createCorporateEmailRequest())
+
+        mockServer.verify(
+          postRequestedFor(urlPathEqualTo("/corporates/1234567/email")),
+        )
+      }
+
+      @Test
+      fun `will return email id `() = runTest {
+        mockServer.stubCreateCorporateEmail(corporateId = 1234567, response = createCorporateEmailResponse().copy(id = 123456))
+
+        val response = apiService.createCorporateEmail(corporateId = 1234567, request = createCorporateEmailRequest())
+
+        assertThat(response.id).isEqualTo(123456)
+      }
+    }
+
+    @Nested
+    inner class UpdateCorporateEmail {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubUpdateCorporateEmail(corporateId = 1234567, emailId = 7655)
+
+        apiService.updateCorporateEmail(corporateId = 1234567, emailId = 7655, request = updateCorporateEmailRequest())
+
+        mockServer.verify(
+          putRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call update endpoint`() = runTest {
+        mockServer.stubUpdateCorporateEmail(corporateId = 1234567, emailId = 7655)
+
+        apiService.updateCorporateEmail(corporateId = 1234567, emailId = 7655, request = updateCorporateEmailRequest())
+
+        mockServer.verify(
+          putRequestedFor(urlPathEqualTo("/corporates/1234567/email/7655")),
+        )
+      }
+    }
+
+    @Nested
+    inner class DeleteCorporateEmail {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubDeleteCorporateEmail(corporateId = 1234567, emailId = 7655)
+
+        apiService.deleteCorporateEmail(corporateId = 1234567, emailId = 7655)
+
+        mockServer.verify(
+          deleteRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call delete endpoint`() = runTest {
+        mockServer.stubDeleteCorporateEmail(corporateId = 1234567, emailId = 7655)
+
+        apiService.deleteCorporateEmail(corporateId = 1234567, emailId = 7655)
+
+        mockServer.verify(
+          deleteRequestedFor(urlPathEqualTo("/corporates/1234567/email/7655")),
+        )
+      }
+    }
+  }
+
+  @Nested
+  inner class Addresses {
+    @Nested
+    inner class CreateCorporateAddress {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubCreateCorporateAddress(corporateId = 1234567)
+
+        apiService.createCorporateAddress(corporateId = 1234567, request = createCorporateAddressRequest())
+
+        mockServer.verify(
+          postRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call create endpoint`() = runTest {
+        mockServer.stubCreateCorporateAddress(corporateId = 1234567)
+
+        apiService.createCorporateAddress(corporateId = 1234567, request = createCorporateAddressRequest())
+
+        mockServer.verify(
+          postRequestedFor(urlPathEqualTo("/corporates/1234567/address")),
+        )
+      }
+
+      @Test
+      fun `will return address id `() = runTest {
+        mockServer.stubCreateCorporateAddress(corporateId = 1234567, response = createCorporateAddressResponse().copy(id = 123456))
+
+        val response = apiService.createCorporateAddress(corporateId = 1234567, request = createCorporateAddressRequest())
+
+        assertThat(response.id).isEqualTo(123456)
+      }
+    }
+
+    @Nested
+    inner class UpdateCorporateAddress {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubUpdateCorporateAddress(corporateId = 1234567, addressId = 654321)
+
+        apiService.updateCorporateAddress(corporateId = 1234567, addressId = 654321, request = updateCorporateAddressRequest())
+
+        mockServer.verify(
+          putRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call update endpoint`() = runTest {
+        mockServer.stubUpdateCorporateAddress(corporateId = 1234567, addressId = 654321)
+
+        apiService.updateCorporateAddress(corporateId = 1234567, addressId = 654321, request = updateCorporateAddressRequest())
+
+        mockServer.verify(
+          putRequestedFor(urlPathEqualTo("/corporates/1234567/address/654321")),
+        )
+      }
+    }
+
+    @Nested
+    inner class DeleteCorporateAddress {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubDeleteCorporateAddress(corporateId = 1234567, addressId = 654321)
+
+        apiService.deleteCorporateAddress(corporateId = 1234567, addressId = 654321)
+
+        mockServer.verify(
+          deleteRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call delete endpoint`() = runTest {
+        mockServer.stubDeleteCorporateAddress(corporateId = 1234567, addressId = 654321)
+
+        apiService.deleteCorporateAddress(corporateId = 1234567, addressId = 654321)
+
+        mockServer.verify(
+          deleteRequestedFor(urlPathEqualTo("/corporates/1234567/address/654321")),
+        )
+      }
+    }
+  }
+
+  @Nested
+  inner class AddressPhones {
+
+    @Nested
+    inner class CreateCorporateAddressPhone {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubCreateCorporateAddressPhone(corporateId = 1234567, addressId = 67890)
+
+        apiService.createCorporateAddressPhone(corporateId = 1234567, addressId = 67890, request = createCorporatePhoneRequest())
+
+        mockServer.verify(
+          postRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call create endpoint`() = runTest {
+        mockServer.stubCreateCorporateAddressPhone(corporateId = 1234567, addressId = 67890)
+
+        apiService.createCorporateAddressPhone(corporateId = 1234567, addressId = 67890, request = createCorporatePhoneRequest())
+
+        mockServer.verify(
+          postRequestedFor(urlPathEqualTo("/corporates/1234567/address/67890/phone")),
+        )
+      }
+
+      @Test
+      fun `will return phone id `() = runTest {
+        mockServer.stubCreateCorporateAddressPhone(corporateId = 1234567, addressId = 67890, response = createCorporatePhoneResponse().copy(id = 123456))
+
+        val response = apiService.createCorporateAddressPhone(corporateId = 1234567, addressId = 67890, request = createCorporatePhoneRequest())
+
+        assertThat(response.id).isEqualTo(123456)
+      }
+    }
+
+    @Nested
+    inner class UpdateCorporateAddressPhone {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubUpdateCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755)
+
+        apiService.updateCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755, request = updateCorporatePhoneRequest())
+
+        mockServer.verify(
+          putRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call update endpoint`() = runTest {
+        mockServer.stubUpdateCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755)
+
+        apiService.updateCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755, request = updateCorporatePhoneRequest())
+
+        mockServer.verify(
+          putRequestedFor(urlPathEqualTo("/corporates/1234567/address/67890/phone/8755")),
+        )
+      }
+    }
+
+    @Nested
+    inner class DeleteCorporateAddressPhone {
+      @Test
+      fun `will pass oath2 token to service`() = runTest {
+        mockServer.stubDeleteCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755)
+
+        apiService.deleteCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755)
+
+        mockServer.verify(
+          deleteRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+        )
+      }
+
+      @Test
+      fun `will call delete endpoint`() = runTest {
+        mockServer.stubDeleteCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755)
+
+        apiService.deleteCorporateAddressPhone(corporateId = 1234567, addressId = 67890, phoneId = 8755)
+
+        mockServer.verify(
+          deleteRequestedFor(urlPathEqualTo("/corporates/1234567/address/67890/phone/8755")),
+        )
+      }
     }
   }
 }
