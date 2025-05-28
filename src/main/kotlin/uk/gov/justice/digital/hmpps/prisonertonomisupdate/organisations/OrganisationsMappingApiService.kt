4d37a863983de0fe383f6666e234f77a00c380be
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-05-28 13:36:10 +0100

    SDIT-2453: ✨ Add in organisation mapping endpoints

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsMappingApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsMappingApiService.kt
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBodilessEntity
+import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodilessEntityOrThrowOnConflict
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.OrganisationsMappingDto
 
@@ -20,6 +22,166 @@ class OrganisationsMappingApiService(@Qualifier("mappingWebClient") val webClien
   suspend fun deleteByDpsOrganisationId(dpsOrganisationId: String) {
     webClient.delete()
       .uri("/mapping/corporate/organisation/dps-organisation-id/{dpsOrganisationId}", dpsOrganisationId)
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun getByDpsAddressIdOrNull(dpsAddressId: Long): OrganisationsMappingDto? = webClient.get()
+    .uri(
+      "/mapping/corporate/address/dps-address-id/{dpsAddressId}",
+      dpsAddressId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun getByDpsAddressId(dpsAddressId: Long): OrganisationsMappingDto = webClient.get()
+    .uri(
+      "/mapping/corporate/address/dps-address-id/{dpsAddressId}",
+      dpsAddressId,
+    )
+    .retrieve()
+    .awaitBody()
+
+  suspend fun createAddressMapping(mappings: OrganisationsMappingDto) = webClient.post()
+    .uri("/mapping/corporate/address")
+    .bodyValue(mappings)
+    .retrieve()
+    .awaitBodilessEntityOrThrowOnConflict()
+
+  suspend fun deleteByNomisAddressId(nomisAddressId: Long) {
+    webClient.delete()
+      .uri(
+        "/mapping/corporate/address/nomis-address-id/{nomisAddressId}",
+        nomisAddressId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun getByDpsPhoneIdOrNull(dpsPhoneId: Long): OrganisationsMappingDto? = webClient.get()
+    .uri(
+      "/mapping/corporate/phone/dps-phone-id/{dpsPhoneId}",
+      dpsPhoneId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun getByDpsPhoneId(dpsPhoneId: Long): OrganisationsMappingDto = webClient.get()
+    .uri(
+      "/mapping/corporate/phone/dps-phone-id/{dpsPhoneId}",
+      dpsPhoneId,
+    )
+    .retrieve()
+    .awaitBody()
+
+  suspend fun createPhoneMapping(mappings: OrganisationsMappingDto) = webClient.post()
+    .uri("/mapping/corporate/phone")
+    .bodyValue(mappings)
+    .retrieve()
+    .awaitBodilessEntityOrThrowOnConflict()
+
+  suspend fun deleteByNomisPhoneId(nomisPhoneId: Long) {
+    webClient.delete()
+      .uri(
+        "/mapping/corporate/phone/nomis-phone-id/{nomisPhoneId}",
+        nomisPhoneId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun getByDpsEmailIdOrNull(dpsEmailId: Long): OrganisationsMappingDto? = webClient.get()
+    .uri(
+      "/mapping/corporate/email/dps-email-id/{dpsEmailId}",
+      dpsEmailId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun getByDpsEmailId(dpsEmailId: Long): OrganisationsMappingDto = webClient.get()
+    .uri(
+      "/mapping/corporate/email/dps-email-id/{dpsEmailId}",
+      dpsEmailId,
+    )
+    .retrieve()
+    .awaitBody()
+
+  suspend fun createEmailMapping(mappings: OrganisationsMappingDto) = webClient.post()
+    .uri("/mapping/corporate/email")
+    .bodyValue(mappings)
+    .retrieve()
+    .awaitBodilessEntityOrThrowOnConflict()
+
+  suspend fun deleteByNomisEmailId(nomisEmailId: Long) {
+    webClient.delete()
+      .uri(
+        "/mapping/corporate/email/nomis-internet-address-id/{nomisEmailId}",
+        nomisEmailId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun getByDpsWebIdOrNull(dpsWebId: Long): OrganisationsMappingDto? = webClient.get()
+    .uri(
+      "/mapping/corporate/web/dps-web-id/{dpsWebId}",
+      dpsWebId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun getByDpsWebId(dpsWebId: Long): OrganisationsMappingDto = webClient.get()
+    .uri(
+      "/mapping/corporate/web/dps-web-id/{dpsWebId}",
+      dpsWebId,
+    )
+    .retrieve()
+    .awaitBody()
+
+  suspend fun createWebMapping(mappings: OrganisationsMappingDto) = webClient.post()
+    .uri("/mapping/corporate/web")
+    .bodyValue(mappings)
+    .retrieve()
+    .awaitBodilessEntityOrThrowOnConflict()
+
+  suspend fun deleteByNomisWebId(nomisWebId: Long) {
+    webClient.delete()
+      .uri(
+        "/mapping/corporate/web/nomis-internet-address-id/{nomisWebId}",
+        nomisWebId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+
+  suspend fun getByDpsAddressPhoneIdOrNull(dpsAddressPhoneId: Long): OrganisationsMappingDto? = webClient.get()
+    .uri(
+      "/mapping/corporate/address-phone/dps-address-phone-id/{dpsAddressPhoneId}",
+      dpsAddressPhoneId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun getByDpsAddressPhoneId(dpsAddressPhoneId: Long): OrganisationsMappingDto = webClient.get()
+    .uri(
+      "/mapping/corporate/address-phone/dps-address-phone-id/{dpsAddressPhoneId}",
+      dpsAddressPhoneId,
+    )
+    .retrieve()
+    .awaitBody()
+
+  suspend fun createAddressPhoneMapping(mappings: OrganisationsMappingDto) = webClient.post()
+    .uri("/mapping/corporate/address-phone")
+    .bodyValue(mappings)
+    .retrieve()
+    .awaitBodilessEntityOrThrowOnConflict()
+
+  suspend fun deleteByNomisAddressPhoneId(nomisAddressPhoneId: Long) {
+    webClient.delete()
+      .uri(
+        "/mapping/corporate/address-phone/nomis-phone-id/{nomisAddressPhoneId}",
+        nomisAddressPhoneId,
+      )
       .retrieve()
       .awaitBodilessEntity()
   }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsMappingApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/organisations/OrganisationsMappingApiMockServer.kt
@@ -6,11 +6,14 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.delete
 import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.post
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.urlMatching
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
+import com.github.tomakehurst.wiremock.stubbing.Scenario
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.DuplicateMappingErrorResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.ErrorResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.OrganisationsMappingDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.MappingExtension.Companion.mappingServer
@@ -62,6 +65,484 @@ class OrganisationsMappingApiMockServer(private val objectMapper: ObjectMapper) 
           .withHeader("Content-Type", "application/json")
           .withStatus(HttpStatus.NO_CONTENT.value()),
       ),
+    )
+  }
+
+  fun stubGetByDpsAddressIdOrNull(
+    dpsAddressId: Long = 123456,
+    mapping: OrganisationsMappingDto? = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsAddressId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/address/dps-address-id/$dpsAddressId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(objectMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/address/dps-address-id/$dpsAddressId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(objectMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+  fun stubGetByDpsAddressId(
+    dpsAddressId: Long = 123456,
+    mapping: OrganisationsMappingDto = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsAddressId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) = stubGetByDpsAddressIdOrNull(dpsAddressId, mapping)
+
+  fun stubDeleteByNomisAddressId(
+    nomisAddressId: Long = 123456,
+  ) {
+    mappingServer.stubFor(
+      delete(urlEqualTo("/mapping/corporate/address/nomis-address-id/$nomisAddressId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value()),
+      ),
+    )
+  }
+
+  fun stubCreateAddressMapping() {
+    mappingServer.stubFor(
+      post("/mapping/corporate/address").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(201),
+      ),
+    )
+  }
+
+  fun stubCreateAddressMapping(error: DuplicateMappingErrorResponse) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/address").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(409)
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubCreateAddressMappingFollowedBySuccess(status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/address")
+        .inScenario("Retry Mapping Address Scenario")
+        .whenScenarioStateIs(Scenario.STARTED)
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
+            .withBody(objectMapper.writeValueAsString(error)),
+        ).willSetStateTo("Cause Mapping Address Success"),
+    )
+
+    mappingServer.stubFor(
+      post("/mapping/corporate/address")
+        .inScenario("Retry Mapping Address Scenario")
+        .whenScenarioStateIs("Cause Mapping Address Success")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201),
+
+        ).willSetStateTo(Scenario.STARTED),
+    )
+  }
+
+  fun stubGetByDpsPhoneIdOrNull(
+    dpsPhoneId: Long = 123456,
+    mapping: OrganisationsMappingDto? = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsPhoneId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/phone/dps-phone-id/$dpsPhoneId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(objectMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/phone/dps-phone-id/$dpsPhoneId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(objectMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+  fun stubGetByDpsPhoneId(
+    dpsPhoneId: Long = 123456,
+    mapping: OrganisationsMappingDto = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsPhoneId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) = stubGetByDpsPhoneIdOrNull(dpsPhoneId, mapping)
+
+  fun stubDeleteByNomisPhoneId(
+    nomisPhoneId: Long = 123456,
+  ) {
+    mappingServer.stubFor(
+      delete(urlEqualTo("/mapping/corporate/phone/nomis-phone-id/$nomisPhoneId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value()),
+      ),
+    )
+  }
+
+  fun stubCreatePhoneMapping() {
+    mappingServer.stubFor(
+      post("/mapping/corporate/phone").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(201),
+      ),
+    )
+  }
+
+  fun stubCreatePhoneMapping(error: DuplicateMappingErrorResponse) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/phone").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(409)
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubCreatePhoneMappingFollowedBySuccess(status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/phone")
+        .inScenario("Retry Mapping Phone Scenario")
+        .whenScenarioStateIs(Scenario.STARTED)
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
+            .withBody(objectMapper.writeValueAsString(error)),
+        ).willSetStateTo("Cause Mapping Phone Success"),
+    )
+
+    mappingServer.stubFor(
+      post("/mapping/corporate/phone")
+        .inScenario("Retry Mapping Phone Scenario")
+        .whenScenarioStateIs("Cause Mapping Phone Success")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201),
+
+        ).willSetStateTo(Scenario.STARTED),
+    )
+  }
+
+  fun stubGetByDpsEmailIdOrNull(
+    dpsEmailId: Long = 123456,
+    mapping: OrganisationsMappingDto? = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsEmailId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/email/dps-email-id/$dpsEmailId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(objectMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/email/dps-email-id/$dpsEmailId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(objectMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+  fun stubGetByDpsEmailId(
+    dpsEmailId: Long = 123456,
+    mapping: OrganisationsMappingDto = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsEmailId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) = stubGetByDpsEmailIdOrNull(dpsEmailId, mapping)
+
+  fun stubDeleteByNomisEmailId(
+    nomisEmailId: Long = 123456,
+  ) {
+    mappingServer.stubFor(
+      delete(urlEqualTo("/mapping/corporate/email/nomis-internet-address-id/$nomisEmailId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value()),
+      ),
+    )
+  }
+
+  fun stubCreateEmailMapping() {
+    mappingServer.stubFor(
+      post("/mapping/corporate/email").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(201),
+      ),
+    )
+  }
+
+  fun stubCreateEmailMapping(error: DuplicateMappingErrorResponse) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/email").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(409)
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubCreateEmailMappingFollowedBySuccess(status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/email")
+        .inScenario("Retry Mapping Email Scenario")
+        .whenScenarioStateIs(Scenario.STARTED)
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
+            .withBody(objectMapper.writeValueAsString(error)),
+        ).willSetStateTo("Cause Mapping Email Success"),
+    )
+
+    mappingServer.stubFor(
+      post("/mapping/corporate/email")
+        .inScenario("Retry Mapping Email Scenario")
+        .whenScenarioStateIs("Cause Mapping Email Success")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201),
+
+        ).willSetStateTo(Scenario.STARTED),
+    )
+  }
+
+  fun stubGetByDpsWebIdOrNull(
+    dpsWebId: Long = 123456,
+    mapping: OrganisationsMappingDto? = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsWebId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/web/dps-web-id/$dpsWebId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(objectMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/web/dps-web-id/$dpsWebId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(objectMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+  fun stubGetByDpsWebId(
+    dpsWebId: Long = 123456,
+    mapping: OrganisationsMappingDto = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsWebId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) = stubGetByDpsWebIdOrNull(dpsWebId, mapping)
+
+  fun stubDeleteByNomisWebId(
+    nomisWebId: Long = 123456,
+  ) {
+    mappingServer.stubFor(
+      delete(urlEqualTo("/mapping/corporate/web/nomis-internet-address-id/$nomisWebId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value()),
+      ),
+    )
+  }
+
+  fun stubCreateWebMapping() {
+    mappingServer.stubFor(
+      post("/mapping/corporate/web").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(201),
+      ),
+    )
+  }
+
+  fun stubCreateWebMapping(error: DuplicateMappingErrorResponse) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/web").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(409)
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubCreateWebMappingFollowedBySuccess(status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/web")
+        .inScenario("Retry Mapping Web Scenario")
+        .whenScenarioStateIs(Scenario.STARTED)
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
+            .withBody(objectMapper.writeValueAsString(error)),
+        ).willSetStateTo("Cause Mapping Web Success"),
+    )
+
+    mappingServer.stubFor(
+      post("/mapping/corporate/web")
+        .inScenario("Retry Mapping Web Scenario")
+        .whenScenarioStateIs("Cause Mapping Web Success")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201),
+
+        ).willSetStateTo(Scenario.STARTED),
+    )
+  } fun stubGetByDpsAddressPhoneIdOrNull(
+    dpsAddressPhoneId: Long = 123456,
+    mapping: OrganisationsMappingDto? = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsAddressPhoneId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/address-phone/dps-address-phone-id/$dpsAddressPhoneId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(objectMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/corporate/address-phone/dps-address-phone-id/$dpsAddressPhoneId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(objectMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+  fun stubGetByDpsAddressPhoneId(
+    dpsAddressPhoneId: Long = 123456,
+    mapping: OrganisationsMappingDto = OrganisationsMappingDto(
+      nomisId = 654321,
+      dpsId = dpsAddressPhoneId.toString(),
+      mappingType = OrganisationsMappingDto.MappingType.MIGRATED,
+    ),
+  ) = stubGetByDpsAddressPhoneIdOrNull(dpsAddressPhoneId, mapping)
+
+  fun stubDeleteByNomisAddressPhoneId(
+    nomisAddressPhoneId: Long = 123456,
+  ) {
+    mappingServer.stubFor(
+      delete(urlEqualTo("/mapping/corporate/address-phone/nomis-phone-id/$nomisAddressPhoneId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value()),
+      ),
+    )
+  }
+
+  fun stubCreateAddressPhoneMapping() {
+    mappingServer.stubFor(
+      post("/mapping/corporate/address-phone").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(201),
+      ),
+    )
+  }
+
+  fun stubCreateAddressPhoneMapping(error: DuplicateMappingErrorResponse) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/address-phone").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(409)
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubCreateAddressPhoneMappingFollowedBySuccess(status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    mappingServer.stubFor(
+      post("/mapping/corporate/address-phone")
+        .inScenario("Retry Mapping AddressPhone Scenario")
+        .whenScenarioStateIs(Scenario.STARTED)
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
+            .withBody(objectMapper.writeValueAsString(error)),
+        ).willSetStateTo("Cause Mapping AddressPhone Success"),
+    )
+
+    mappingServer.stubFor(
+      post("/mapping/corporate/address-phone")
+        .inScenario("Retry Mapping AddressPhone Scenario")
+        .whenScenarioStateIs("Cause Mapping AddressPhone Success")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201),
+
+        ).willSetStateTo(Scenario.STARTED),
     )
   }
 
