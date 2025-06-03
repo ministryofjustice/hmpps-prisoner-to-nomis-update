77ae9148ff13c730ddc682bb4eb90740241c535c
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-06-03 10:47:12 +0100

    SDIT-2744: ✨ Add in incidents mapping (#1233)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsMappingApiService.kt
@@ -0,0 +1,45 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.incidents
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBodilessEntity
+import org.springframework.web.reactive.function.client.awaitBody
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodilessEntityOrThrowOnConflict
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.IncidentMappingDto
+
+@Service
+class IncidentsMappingApiService(@Qualifier("mappingWebClient") val webClient: WebClient) {
+  suspend fun getByDpsIncidentIdOrNull(dpsIncidentId: Long): IncidentMappingDto? = webClient.get()
+    .uri(
+      "/mapping/incidents/dps-incident-id/{dpsIncidentId}",
+      dpsIncidentId,
+    )
+    .retrieve()
+    .awaitBodyOrNullForNotFound()
+
+  suspend fun getByDpsIncidentId(dpsIncidentId: Long): IncidentMappingDto = webClient.get()
+    .uri(
+      "/mapping/incidents/dps-incident-id/{dpsIncidentId}",
+      dpsIncidentId,
+    )
+    .retrieve()
+    .awaitBody()
+
+  suspend fun createIncidentMapping(mappings: IncidentMappingDto) = webClient.post()
+    .uri("/mapping/incidents")
+    .bodyValue(mappings)
+    .retrieve()
+    .awaitBodilessEntityOrThrowOnConflict()
+
+  suspend fun deleteByNomisIncidentId(nomisIncidentId: Long) {
+    webClient.delete()
+      .uri(
+        "/mapping/incidents/nomis-incident-id/{nomisIncidentId}",
+        nomisIncidentId,
+      )
+      .retrieve()
+      .awaitBodilessEntity()
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsMappingApiMockServer.kt
@@ -0,0 +1,121 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.incidents
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.tomakehurst.wiremock.client.CountMatchingStrategy
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.delete
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.post
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
+import com.github.tomakehurst.wiremock.stubbing.Scenario
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.DuplicateMappingErrorResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.ErrorResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.IncidentMappingDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.MappingExtension.Companion.mappingServer
+
+@Component
+class IncidentsMappingApiMockServer(private val objectMapper: ObjectMapper) {
+
+  fun stubGetByDpsIncidentIdOrNull(
+    dpsIncidentId: Long = 123456,
+    mapping: IncidentMappingDto? = IncidentMappingDto(
+      nomisIncidentId = 654321,
+      dpsIncidentId = dpsIncidentId.toString(),
+      mappingType = IncidentMappingDto.MappingType.MIGRATED,
+    ),
+  ) {
+    mapping?.apply {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/incidents/dps-incident-id/$dpsIncidentId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.OK.value())
+            .withBody(objectMapper.writeValueAsString(mapping)),
+        ),
+      )
+    } ?: run {
+      mappingServer.stubFor(
+        get(urlEqualTo("/mapping/incidents/dps-incident-id/$dpsIncidentId")).willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(HttpStatus.NOT_FOUND.value())
+            .withBody(objectMapper.writeValueAsString(ErrorResponse(status = 404))),
+        ),
+      )
+    }
+  }
+  fun stubGetByDpsIncidentId(
+    dpsIncidentId: Long = 123456,
+    mapping: IncidentMappingDto = IncidentMappingDto(
+      nomisIncidentId = 654321,
+      dpsIncidentId = dpsIncidentId.toString(),
+      mappingType = IncidentMappingDto.MappingType.MIGRATED,
+    ),
+  ) = stubGetByDpsIncidentIdOrNull(dpsIncidentId, mapping)
+
+  fun stubDeleteByNomisIncidentId(
+    nomisIncidentId: Long = 123456,
+  ) {
+    mappingServer.stubFor(
+      delete(urlEqualTo("/mapping/incidents/nomis-incident-id/$nomisIncidentId")).willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(HttpStatus.OK.value()),
+      ),
+    )
+  }
+
+  fun stubCreateIncidentMapping() {
+    mappingServer.stubFor(
+      post("/mapping/incidents").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(201),
+      ),
+    )
+  }
+
+  fun stubCreateIncidentMapping(error: DuplicateMappingErrorResponse) {
+    mappingServer.stubFor(
+      post("/mapping/incidents").willReturn(
+        aResponse()
+          .withHeader("Content-Type", "application/json")
+          .withStatus(409)
+          .withBody(objectMapper.writeValueAsString(error)),
+      ),
+    )
+  }
+
+  fun stubCreateIncidentMappingFollowedBySuccess(status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR, error: ErrorResponse = ErrorResponse(status = status.value())) {
+    mappingServer.stubFor(
+      post("/mapping/incidents")
+        .inScenario("Retry Mapping Incident Scenario")
+        .whenScenarioStateIs(Scenario.STARTED)
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
+            .withBody(objectMapper.writeValueAsString(error)),
+        ).willSetStateTo("Cause Mapping Incident Success"),
+    )
+
+    mappingServer.stubFor(
+      post("/mapping/incidents")
+        .inScenario("Retry Mapping Incident Scenario")
+        .whenScenarioStateIs("Cause Mapping Incident Success")
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201),
+
+        ).willSetStateTo(Scenario.STARTED),
+    )
+  }
+
+  fun verify(pattern: RequestPatternBuilder) = mappingServer.verify(pattern)
+  fun verify(count: Int, pattern: RequestPatternBuilder) = mappingServer.verify(count, pattern)
+  fun verify(count: CountMatchingStrategy, pattern: RequestPatternBuilder) = mappingServer.verify(count, pattern)
+}
