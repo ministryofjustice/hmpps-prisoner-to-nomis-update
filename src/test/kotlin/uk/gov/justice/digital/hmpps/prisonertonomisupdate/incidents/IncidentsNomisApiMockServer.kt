bcea19d93034035d4a11dad7a0c652bbf158f8dd
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-06-09 13:17:30 +0100

    SDIT-2802: ♻️ Change to upsert (#1257)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsNomisApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsNomisApiMockServer.kt
@@ -1,23 +1,20 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.incidents
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.delete
-import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.put
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateIncidentRequest
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.NomisAudit
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertIncidentRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.NomisApiExtension.Companion.nomisApi
 import java.time.LocalDateTime
 
 @Component
-class IncidentsNomisApiMockServer(private val objectMapper: ObjectMapper) {
+class IncidentsNomisApiMockServer {
   companion object {
-    fun createIncidentRequest(): CreateIncidentRequest = CreateIncidentRequest(
+    fun upsertIncidentRequest(): UpsertIncidentRequest = UpsertIncidentRequest(
       title = "An incident occurred",
       description = "Fighting and shouting occurred in the prisoner's cell and a chair was thrown.",
       location = "BXI",
@@ -32,14 +29,14 @@ class IncidentsNomisApiMockServer(private val objectMapper: ObjectMapper) {
   fun verify(pattern: RequestPatternBuilder) = nomisApi.verify(pattern)
   fun verify(count: Int, pattern: RequestPatternBuilder) = nomisApi.verify(count, pattern)
 
-  fun stubCreateIncident(
+  fun stubUpsertIncident(
     incidentId: Long = 123456,
   ) {
     nomisApi.stubFor(
       put(urlEqualTo("/incidents/$incidentId")).willReturn(
         aResponse()
           .withHeader("Content-Type", "application/json")
-          .withStatus(HttpStatus.CREATED.value()),
+          .withStatus(HttpStatus.OK.value()),
       ),
     )
   }
@@ -56,8 +53,3 @@ class IncidentsNomisApiMockServer(private val objectMapper: ObjectMapper) {
     )
   }
 }
-
-fun nomisAudit() = NomisAudit(
-  createDatetime = LocalDateTime.now(),
-  createUsername = "Q1251T",
-)
