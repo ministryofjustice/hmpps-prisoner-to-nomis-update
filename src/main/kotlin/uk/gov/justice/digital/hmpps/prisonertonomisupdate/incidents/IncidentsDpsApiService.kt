96113a0d6c668d08b09c4082ef47226cfc4a0533
karenmillermoj <karen.miller3@justice.gov.uk>
2025-06-27 08:54:59 +0100

    SDIT-2821 Set up incident reconciliation mocks

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsDpsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsDpsApiService.kt
@@ -5,11 +5,38 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.incidents.model.ReportWithDetails
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.incidents.model.SimplePageReportBasic
 
 @Service
 class IncidentsDpsApiService(@Qualifier("incidentsApiWebClient") private val webClient: WebClient) {
+  companion object {
+    // TODO this will change
+    val openStatusValues = listOf("AWAITING_REVIEW", "NEEDS_UPDATING", "ON_HOLD", "POST_INCIDENT_UPDATE", "UPDATED")
+    val closedStatusValues = listOf("CLOSED", "DUPLICATE")
+  }
+
   suspend fun getIncident(incidentId: String): ReportWithDetails = webClient.get()
     .uri("/incident-reports/{incidentId}/with-details", incidentId)
     .retrieve()
     .awaitBody()
+
+  suspend fun getIncidentsByAgencyAndStatus(agencyId: String, statusValues: List<String>): SimplePageReportBasic = webClient.get()
+    .uri {
+      it.path("/incident-reports")
+        .queryParam("prisonId", agencyId)
+        .queryParam("status", statusValues)
+        .queryParam("size", 1)
+        .build()
+    }
+    .retrieve()
+    .awaitBody()
+
+  suspend fun getIncidentDetailsByNomisId(nomisIncidentId: Long): ReportWithDetails = webClient.get()
+    .uri("/incident-reports/reference/{nomisIncidentId}/with-details", nomisIncidentId)
+    .retrieve()
+    .awaitBody()
+
+  suspend fun getOpenIncidentsCount(agencyId: String) = getIncidentsByAgencyAndStatus(agencyId, openStatusValues).totalElements
+
+  suspend fun getClosedIncidentsCount(agencyId: String) = getIncidentsByAgencyAndStatus(agencyId, closedStatusValues).totalElements
 }
