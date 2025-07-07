9b38669685c0a6f32f40254269a7071e5357a21d
andymarke <andy.marke@digital.justice.gov.uk>
2025-07-07 11:08:11 +0100

    SDIT-2928 Simplified SQS sending

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incentives/IncentivesUpdateQueueService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incentives/IncentivesUpdateQueueService.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.incentives
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.QueueService
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryQueueService
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 
@@ -10,12 +10,12 @@ import uk.gov.justice.hmpps.sqs.HmppsQueueService
 class IncentivesUpdateQueueService(
   hmppsQueueService: HmppsQueueService,
   telemetryClient: TelemetryClient,
-  objectMapper: ObjectMapper,
+  queueService: QueueService,
 ) : RetryQueueService(
   queueId = "incentive",
   hmppsQueueService = hmppsQueueService,
   telemetryClient = telemetryClient,
-  objectMapper = objectMapper,
+  queueService = queueService,
 )
 
 data class IncentiveMapping(val nomisBookingId: Long, val nomisIncentiveSequence: Int, val incentiveId: Long)
