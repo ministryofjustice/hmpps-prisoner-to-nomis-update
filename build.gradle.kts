85a7b0b8d34ad3fd04b3bb1bc16e6a6c96b9e5d0
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-07-09 11:48:55 +0100

    SDIT-2935: ⬆️  Upgrade plugin, kotlin etc. (#1379)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0-beta"
-  kotlin("plugin.spring") version "2.1.21"
-  id("org.openapi.generator") version "7.13.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.1"
+  kotlin("plugin.spring") version "2.2.0"
+  id("org.openapi.generator") version "7.14.0"
 }
 
 configurations {
@@ -21,10 +21,10 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.7")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework.data:spring-data-commons:3.5.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
+  implementation("org.springframework.data:spring-data-commons:3.5.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.6")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.9")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
@@ -33,19 +33,19 @@ dependencies {
   // Leaving at 2.9.0 to match the version used in App Insights https://github.com/microsoft/ApplicationInsights-Java/blob/3.6.2/dependencyManagement/build.gradle.kts#L16
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.16.0")
   // Leaving at 1.43.0 to match the version used in App Insights https://github.com/microsoft/ApplicationInsights-Java/blob/3.6.2/dependencyManagement/build.gradle.kts#L14
-  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.51.0")
+  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.50.0")
   implementation("com.google.guava:guava:33.4.8-jre")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.6")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.29") {
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.7")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.31") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.32")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.34")
 
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.21.1")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.786")
+  testImplementation("org.testcontainers:localstack:1.21.3")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.787")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 }
