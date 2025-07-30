b7d48bc673f9da147d9125ea50c8a9191df0cf40
andymarke <andy.marke@digital.justice.gov.uk>
2025-07-30 13:06:49 +0100

    SDIT-2974 Handle prisoner restriction created in DPS

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/personalrelationships/PrisonerRestrictionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/personalrelationships/PrisonerRestrictionsService.kt
@@ -3,24 +3,57 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.personalrelationships
 import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.PrisonerRestrictionMappingDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.PrisonerRestrictionMappingDto.MappingType.DPS_CREATED
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreatePrisonerRestrictionRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.personalrelationships.ContactPersonService.Companion.MappingTypes.PRISONER_RESTRICTION
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.personalrelationships.model.SyncPrisonerRestriction
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.synchronise
 
 @Service
 class PrisonerRestrictionsService(
   private val telemetryClient: TelemetryClient,
+  private val contactPersonRetryQueueService: ContactPersonRetryQueueService,
+  private val mappingApiService: ContactPersonMappingApiService,
+  private val dpsApiService: ContactPersonDpsApiService,
+  private val nomisApiService: ContactPersonNomisApiService,
 
 ) {
   suspend fun restrictionCreated(event: PrisonerRestrictionEvent) {
     val entityName = PRISONER_RESTRICTION.entityName
 
     val dpsRestrictionId = event.additionalInformation.prisonerRestrictionId
+    val offenderNo = event.prisonerNumber()
     val telemetryMap = mutableMapOf(
       "dpsRestrictionId" to dpsRestrictionId.toString(),
-      "offenderNo" to event.prisonerNumber(),
+      "offenderNo" to offenderNo,
     )
 
     if (event.didOriginateInDPS()) {
-      throw UnsupportedOperationException("DPS created restrictions are not currently supported")
+      synchronise {
+        name = entityName
+        telemetryClient = this@PrisonerRestrictionsService.telemetryClient
+        retryQueueService = contactPersonRetryQueueService
+        eventTelemetry = telemetryMap
+
+        checkMappingDoesNotExist {
+          mappingApiService.getByDpsPrisonerRestrictionIdOrNull(dpsRestrictionId)
+        }
+        transform {
+          val dpsRestriction = dpsApiService.getPrisonerRestriction(dpsRestrictionId)
+          nomisApiService.createPrisonerRestriction(offenderNo = offenderNo, dpsRestriction.toNomisCreateRequest()).also {
+            telemetryMap["nomisRestrictionId"] = it.id.toString()
+          }.let {
+            PrisonerRestrictionMappingDto(
+              dpsId = dpsRestrictionId.toString(),
+              nomisId = it.id,
+              offenderNo = offenderNo,
+              mappingType = DPS_CREATED,
+            )
+          }
+        }
+        saveMapping { mappingApiService.createPrisonerRestrictionMapping(it) }
+      }
     } else {
       telemetryClient.trackEvent("$entityName-create-ignored", telemetryMap)
     }
@@ -29,9 +62,10 @@ class PrisonerRestrictionsService(
     val entityName = PRISONER_RESTRICTION.entityName
 
     val dpsRestrictionId = event.additionalInformation.prisonerRestrictionId
+    val offenderNo = event.prisonerNumber()
     val telemetryMap = mutableMapOf(
       "dpsRestrictionId" to dpsRestrictionId.toString(),
-      "offenderNo" to event.prisonerNumber(),
+      "offenderNo" to offenderNo,
     )
 
     if (event.didOriginateInDPS()) {
@@ -58,3 +92,12 @@ class PrisonerRestrictionsService(
 }
 
 private fun PrisonerRestrictionEvent.didOriginateInDPS() = this.additionalInformation.source == "DPS"
+
+private fun SyncPrisonerRestriction.toNomisCreateRequest() = CreatePrisonerRestrictionRequest(
+  typeCode = this.restrictionType,
+  effectiveDate = this.effectiveDate,
+  expiryDate = this.expiryDate,
+  comment = this.commentText,
+  enteredStaffUsername = this.createdBy,
+  authorisedStaffUsername = this.authorisedUsername,
+)
