2b54fa1b6af7edea19814ba2bc06a187ef6a0176
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-08-19 13:38:31 +0100

    SDIT-2804 Tidy up the batch manager and tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/batch/BatchManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/batch/BatchManager.kt
@@ -3,11 +3,11 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.batch
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.beans.factory.getBeanProvider
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.activities.ActivitiesReconService
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.DomainEventListener
@@ -16,12 +16,13 @@ import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.DomainEventLi
 @Service
 class BatchManager(
   @Value($$"${batch.type}") private val batchType: String,
+  private val beanDestroyer: BeanDestroyer,
   private val activitiesReconService: ActivitiesReconService,
 ) {
 
   @EventListener
   fun onApplicationEvent(event: ContextRefreshedEvent) = runBlocking {
-    removeQueueListeners(event)
+    beanDestroyer.destroyBeans(DomainEventListener::class.java)
 
     when (batchType) {
       "ALLOCATION_RECON" -> activitiesReconService.allocationReconciliationReport()
@@ -29,17 +30,19 @@ class BatchManager(
     }
   }.also { event.closeApplication() }
 
-  private fun removeQueueListeners(event: ContextRefreshedEvent) {
-    with(event.applicationContext.autowireCapableBeanFactory) {
-      getBeanProvider<DomainEventListener>().forEach {
-        this.destroyBean(it)
-      }
-    }
-  }
-
   private fun ContextRefreshedEvent.closeApplication() = (this.applicationContext as ConfigurableApplicationContext).close()
 
   companion object {
     val log = LoggerFactory.getLogger(this::class.java)
+  }
+}
+
+@ConditionalOnProperty(name = ["batch.enabled"], havingValue = "true")
+@Component
+class BeanDestroyer(applicationContext: ConfigurableApplicationContext) {
+  private val beanFactory = applicationContext.autowireCapableBeanFactory
+
+  fun destroyBeans(vararg types: Class<*>) = types.forEach { type ->
+    beanFactory.getBeanProvider(type).forEach { beanFactory.destroyBean(it) }
   }
 }
