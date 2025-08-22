7d0de3720267b7d69edaf6c51599f81b44a94058
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2025-08-22 11:48:34 +0100

    SDIT-2962-Transaction-reconciliation (#1528)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/finance/TransactionNomisApiService.kt
@@ -0,0 +1,62 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.finance
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import reactor.util.context.Context
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyWithRetry
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.GeneralLedgerTransactionDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.OffenderTransactionDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
+import java.time.LocalDate
+
+@Service
+class TransactionNomisApiService(
+  @Qualifier("nomisApiWebClient") private val webClient: WebClient,
+  retryApiService: RetryApiService,
+) {
+  private val backoffSpec = retryApiService.getBackoffSpec().withRetryContext(
+    Context.of("api", "TransactionNomisApiService"),
+  )
+
+  suspend fun getTransactions(transactionId: Long): List<OffenderTransactionDto> = webClient
+    .get()
+    .uri("/transactions/{transactionId}", transactionId)
+    .retrieve()
+    .awaitBody()
+
+  suspend fun getGLTransactions(transactionId: Long): List<GeneralLedgerTransactionDto> = webClient
+    .get()
+    .uri("/transactions/{transactionId}/general-ledger", transactionId)
+    .retrieve()
+    .awaitBody()
+
+  suspend fun getGLTransactionsForPrisoner(offenderNo: String): PrisonerTransactionLists = webClient
+    .get()
+    .uri("/transactions/prisoners/{offenderNo}", offenderNo)
+    .retrieve()
+    .awaitBodyWithRetry(retrySpec = backoffSpec)
+
+  suspend fun getFirstTransactionOn(date: LocalDate): Long = webClient
+    .get()
+    .uri("/transactions/{date}/first", date)
+    .retrieve()
+    .awaitBodyWithRetry(retrySpec = backoffSpec)
+
+  suspend fun getGLTransactionsForRange(
+    transactionId: Long,
+    transactionEntrySequence: Int,
+    generalLedgerEntrySequence: Int,
+    pageSize: Int,
+  ): List<GeneralLedgerTransactionDto> = webClient
+    .get()
+    .uri("/transactions/from/{transactionId}/{transactionEntrySequence}/{generalLedgerEntrySequence}?pageSize=$pageSize", transactionId)
+    .retrieve()
+    .awaitBodyWithRetry(retrySpec = backoffSpec)
+}
+
+data class PrisonerTransactionLists(
+  val offenderTransactions: List<OffenderTransactionDto>,
+  val orphanGlTransactions: List<GeneralLedgerTransactionDto>,
+)
