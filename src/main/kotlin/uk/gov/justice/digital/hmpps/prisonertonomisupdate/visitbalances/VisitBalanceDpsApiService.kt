281dc6a130d7668423bac81a282d04591441f0e2
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-09-18 14:32:31 +0100

    SDIT-3051: ✨ Use generated api to call visit balance (#1609)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/visitbalances/VisitBalanceDpsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/visitbalances/VisitBalanceDpsApiService.kt
@@ -4,23 +4,28 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.visit.balance.api.BalanceControllerApi
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.visit.balance.api.NomisControllerApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.visit.balance.model.PrisonerBalanceDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.visit.balance.model.VisitAllocationPrisonerAdjustmentResponseDto
 
 @Service
 class VisitBalanceDpsApiService(
-  private val visitBalanceApiWebClient: WebClient,
+  visitBalanceApiWebClient: WebClient,
 ) {
-  suspend fun getVisitBalance(prisonNumber: String): PrisonerBalanceDto? = visitBalanceApiWebClient.get()
-    .uri("/visits/allocation/prisoner/{prisonNumber}/balance", prisonNumber)
+  private val balanceApi = BalanceControllerApi(visitBalanceApiWebClient)
+  private val adjustmentApi = NomisControllerApi(visitBalanceApiWebClient)
+
+  suspend fun getVisitBalance(prisonNumber: String): PrisonerBalanceDto? = balanceApi
+    .prepare(balanceApi.getPrisonerBalanceRequestConfig(prisonNumber))
     .retrieve()
     .awaitBodyOrNullForNotFound()
 
   suspend fun getVisitBalanceAdjustment(
     prisonNumber: String,
     visitBalanceAdjustmentId: String,
-  ): VisitAllocationPrisonerAdjustmentResponseDto = visitBalanceApiWebClient.get()
-    .uri("/visits/allocation/prisoner/{prisonNumber}/adjustments/{visitBalanceAdjustmentId}", prisonNumber, visitBalanceAdjustmentId)
+  ): VisitAllocationPrisonerAdjustmentResponseDto = adjustmentApi
+    .prepare(adjustmentApi.getPrisonerAdjustmentRequestConfig(prisonNumber, visitBalanceAdjustmentId))
     .retrieve()
     .awaitBody()
 }
