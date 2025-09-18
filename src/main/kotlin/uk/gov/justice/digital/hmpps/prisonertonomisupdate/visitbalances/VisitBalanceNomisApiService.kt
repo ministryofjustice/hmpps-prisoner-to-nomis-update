0a15583890843c0bde70619e1132a933656e287f
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-09-18 11:26:30 +0100

    SDIT-3051: ✨ Use generated api to call nomis prisoner

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/visitbalances/VisitBalanceNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/visitbalances/VisitBalanceNomisApiService.kt
@@ -6,28 +6,28 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBodilessEntity
 import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.api.VisitBalanceResourceApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateVisitBalanceAdjustmentRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateVisitBalanceAdjustmentResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateVisitBalanceRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.VisitBalanceResponse
 
 @Service
-class VisitBalanceNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
-  suspend fun getVisitBalance(prisonNumber: String): VisitBalanceResponse? = webClient.get()
-    .uri("/prisoners/{prisonNumber}/visit-balance", prisonNumber)
+class VisitBalanceNomisApiService(@Qualifier("nomisApiWebClient") webClient: WebClient) {
+  private val api = VisitBalanceResourceApi(webClient)
+
+  suspend fun getVisitBalance(prisonNumber: String): VisitBalanceResponse? = api
+    .prepare(api.getVisitBalanceForPrisonerRequestConfig(prisonNumber))
     .retrieve()
     .awaitBodyOrNullForNotFound()
 
-  suspend fun createVisitBalanceAdjustment(prisonNumber: String, visitBalanceAdjustment: CreateVisitBalanceAdjustmentRequest): CreateVisitBalanceAdjustmentResponse = webClient.post()
-    .uri("/prisoners/{prisonNumber}/visit-balance-adjustments", prisonNumber)
-    .bodyValue(visitBalanceAdjustment)
+  suspend fun createVisitBalanceAdjustment(prisonNumber: String, visitBalanceAdjustment: CreateVisitBalanceAdjustmentRequest): CreateVisitBalanceAdjustmentResponse = api
+    .prepare(api.createVisitBalanceAdjustmentRequestConfig(prisonNumber, visitBalanceAdjustment))
     .retrieve()
     .awaitBody()
 
   suspend fun updateVisitBalance(prisonNumber: String, visitBalance: UpdateVisitBalanceRequest) {
-    webClient.put()
-      .uri("/prisoners/{prisonNumber}/visit-balance", prisonNumber)
-      .bodyValue(visitBalance)
+    api.prepare(api.updateVisitBalanceRequestConfig(prisonNumber, visitBalance))
       .retrieve()
       .awaitBodilessEntity()
   }
