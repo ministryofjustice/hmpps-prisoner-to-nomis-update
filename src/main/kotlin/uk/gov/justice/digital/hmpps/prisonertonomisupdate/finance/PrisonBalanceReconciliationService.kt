2ad5c9330fa5805386b483f68d09cd28efff4d3a
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2025-10-14 12:07:10 +0100

    SDIT-3081 Prison Balance reconciliation (#1713)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/finance/PrisonBalanceReconciliationService.kt
@@ -0,0 +1,165 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.finance
+
+import com.microsoft.applicationinsights.TelemetryClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.finance.model.GeneralLedgerBalanceDetails
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.PrisonAccountBalanceDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.doApiCallWithRetries
+import java.math.BigDecimal
+import kotlin.collections.plus
+import kotlin.collections.toSet
+
+@Service
+class PrisonBalanceReconciliationService(
+  private val telemetryClient: TelemetryClient,
+  private val dpsApiService: FinanceDpsApiService,
+  private val nomisApiService: FinanceNomisApiService,
+) {
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  suspend fun generateReconciliationReport() {
+    val prisonIds = nomisApiService.getPrisonBalanceIds()
+
+    telemetryClient.trackEvent(
+      "prison-balance-reports-reconciliation-requested",
+      mapOf("prisons" to prisonIds.size.toString()),
+    )
+    log.info("Prison balance reconciliation report requested for ${prisonIds.size} prisons")
+
+    runCatching { generateReconciliationReport(prisonIds) }
+      .onSuccess {
+        log.info("Prison balance reconciliation report completed with ${it.size} mismatches")
+        telemetryClient.trackEvent(
+          "prison-balance-reports-reconciliation-report",
+          mapOf(
+            "mismatch-count" to it.size.toString(),
+            "success" to "true",
+          ),
+        )
+      }
+      .onFailure {
+        telemetryClient.trackEvent("prison-balance-reports-reconciliation-report", mapOf("success" to "false"))
+        log.error("Prison balance reconciliation report failed", it)
+      }
+  }
+
+  suspend fun generateReconciliationReport(prisonIds: List<String>): List<MismatchPrisonBalance> = withContext(Dispatchers.Unconfined) {
+    prisonIds.map { async { checkPrisonBalanceMatch(it) } }
+  }.awaitAll().filterNotNull()
+
+  suspend fun checkPrisonBalanceMatch(prisonId: String): MismatchPrisonBalance? = runCatching {
+    val nomisPrisonBalances =
+      doApiCallWithRetries { nomisApiService.getPrisonBalance(prisonId) }.accountBalances.map { it.toPrisonSummary() }
+    val dpsPrisonBalances =
+      doApiCallWithRetries { dpsApiService.getPrisonAccounts(prisonId) }.items.map { it.toPrisonSummary() }
+
+    val telemetry = mutableMapOf(
+      "prisonId" to prisonId,
+      "nomisAccountCount" to (nomisPrisonBalances.size.toString()),
+      "dpsAccountCount" to (dpsPrisonBalances.size.toString()),
+    )
+
+    if (nomisPrisonBalances.size != dpsPrisonBalances.size) {
+      return MismatchPrisonBalance(
+        prisonId,
+        nomisAccountCount = nomisPrisonBalances.size,
+        dpsAccountCount = dpsPrisonBalances.size,
+        verdict = "different-number-of-accounts",
+      ).also { mismatch ->
+        log.info("Prison account sizes do not match  $mismatch")
+        telemetryClient.trackEvent(
+          "prison-balance-reports-reconciliation-mismatch",
+          telemetry + mapOf(
+            "reason" to "different-number-of-accounts",
+          ),
+        )
+      }
+    }
+
+    val (accountCodesInDpsOnly, accountCodesInNomisOnly) =
+      findMissingPrisonBalances(nomisPrisonBalances, dpsPrisonBalances)
+    if (accountCodesInDpsOnly.isNotEmpty() || accountCodesInNomisOnly.isNotEmpty()) {
+      return MismatchPrisonBalance(
+        prisonId,
+        nomisAccountCount = nomisPrisonBalances.size,
+        dpsAccountCount = dpsPrisonBalances.size,
+        missingFromNomis = accountCodesInDpsOnly,
+        missingFromDps = accountCodesInNomisOnly,
+        verdict = "different-account-codes",
+      ).also {
+        telemetryClient.trackEvent(
+          "prison-balance-reports-reconciliation-mismatch",
+          telemetry +
+            mapOf(
+              "reason" to "different-account-codes",
+              "missingFromNomis" to accountCodesInDpsOnly.toString(),
+              "missingFromDps" to accountCodesInNomisOnly.toString(),
+            ),
+        )
+      }
+    }
+
+    return nomisPrisonBalances.filter { !dpsPrisonBalances.contains(it) }.map {
+      telemetryClient.trackEvent(
+        "prison-balance-reports-reconciliation-mismatch",
+        telemetry + mapOf(
+          "reason" to "different-prison-account-balance",
+          "nomisPrisonBalances" to nomisPrisonBalances,
+          "dpsPrisonBalances" to dpsPrisonBalances,
+        ),
+      )
+      MismatchPrisonBalance(
+        prisonId,
+        nomisAccountCount = nomisPrisonBalances.size,
+        dpsAccountCount = dpsPrisonBalances.size,
+        // TODO add balances
+        verdict = "different-prison-account-balance",
+      )
+    }.firstOrNull()
+  }.onFailure {
+    log.error("Unable to match prison balance for prison with id $prisonId", it)
+    telemetryClient.trackEvent(
+      "prison-balance-reports-reconciliation-mismatch-error",
+      mapOf(
+        "prisonId" to prisonId,
+      ),
+    )
+  }.getOrNull()
+}
+
+private fun findMissingPrisonBalances(nomisAccountSummaries: List<AccountSummary>, dpsAccountSummaries: List<AccountSummary>): Pair<List<Int>, List<Int>> {
+  val nomisAccountCodes = nomisAccountSummaries.map { it.accountCode }.toSet()
+  val dpsAccountCodes = dpsAccountSummaries.map { it.accountCode }.toSet()
+
+  val missingFromDps = nomisAccountCodes - dpsAccountCodes
+  val missingFromNomis = dpsAccountCodes - nomisAccountCodes
+
+  return missingFromNomis.toList() to missingFromDps.toList()
+}
+
+fun PrisonAccountBalanceDto.toPrisonSummary() = AccountSummary(accountCode.toInt(), balance)
+fun GeneralLedgerBalanceDetails.toPrisonSummary() = AccountSummary(accountCode, balance)
+
+data class AccountSummary(
+  val accountCode: Int,
+  val balance: BigDecimal,
+)
+
+data class MismatchPrisonBalance(
+  val prisonId: String,
+  val dpsAccountCount: Int,
+  val nomisAccountCount: Int,
+  val missingFromNomis: List<Int> = listOf(),
+  val missingFromDps: List<Int> = listOf(),
+  // TODO val balanceDifference: List<MismatchAccountBalance> = listOf(),
+  val verdict: String,
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/finance/PrisonBalanceResource.kt
@@ -0,0 +1,56 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.finance
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+@RestController
+class PrisonBalanceResource(private val reconciliationService: PrisonBalanceReconciliationService) {
+
+  @PreAuthorize("hasRole('PRISONER_TO_NOMIS__UPDATE__RW')")
+  @GetMapping("/prison-balance/reconciliation/{prisonId}")
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(
+    summary = "Run the prison balance reconciliation for this prison id",
+    description = """Retrieves the account balance differences for a prison. Empty response returned if no differences found. 
+      Requires ROLE_PRISONER_TO_NOMIS__UPDATE__RW""",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Reconciliation differences returned",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden to access this endpoint. Requires ROLE_PRISONER_TO_NOMIS__UPDATE__RW",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  suspend fun generateReconciliationReportForPrison(
+    @Schema(description = "Prison Id", example = "MDI")
+    @PathVariable prisonId: String,
+  ): MismatchPrisonBalance? = reconciliationService.checkPrisonBalanceMatch(prisonId)
+}
