ed1c4b6932ddf40511a5611b7ff750b15f8be41a
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-10-16 09:57:35 +0100

    SDIT-3102: 🎨 Make domain mandatory (#1730)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/listeners/EventFeatureSwitch.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/listeners/EventFeatureSwitch.kt
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class EventFeatureSwitch(private val environment: Environment) {
-  fun isEnabled(eventType: String, domain: String? = null): Boolean = isEnabled("feature.event.$eventType") &&
-    (domain == null || isEnabled("feature.event.$domain.$eventType"))
+  fun isEnabled(eventType: String, domain: String): Boolean = isEnabled("feature.event.$eventType") && isEnabled("feature.event.$domain.$eventType")
 
   private fun isEnabled(property: String): Boolean = environment.getProperty(property, Boolean::class.java, true)
 }
