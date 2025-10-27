867808ed18171f2f62f2c068857da18cce8f4385
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-10-27 11:23:53 +0000

    SDIT-3127 Try out Spring Boot 3.5.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ import kotlin.io.path.name
 import kotlin.io.path.Path as KotlinPath
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
   kotlin("plugin.spring") version "2.2.20"
   id("org.openapi.generator") version "7.16.0"
 }
