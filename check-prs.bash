b604d7a94048859bac14b9d69483c9d520c430dc
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-11-06 14:22:39 +0000

    SDIT-3162: ✨ Including failing branches in failure slack alert

--- check-prs.bash
+++ check-prs.bash
@@ -8,8 +8,11 @@ else
   OUTPUT_COMPARISON=false
 fi
 
-FAILURE_COUNT=$(gh pr ls --search 'head:api-docs-' --json 'statusCheckRollup,url' --jq '. | map(select(.statusCheckRollup[].conclusion == "FAILURE")) | .[].url' | sort -u | wc -l | awk '{print $1}')
+FAILURES=$(gh pr ls --search 'head:api-docs-' --json 'statusCheckRollup,headRefName' --jq '. | map(select(.statusCheckRollup[].conclusion == "FAILURE")) | .[].headRefName' | sort -u)
+FAILURE_COUNT=$(echo "$FAILURES" | wc -l | awk '{print $1}')
+FAILURE_BRANCHES=$(echo "$FAILURES" | paste -sd' ' -)
 echo "failure_count=$FAILURE_COUNT" >>"$GITHUB_OUTPUT"
+echo "failures=$FAILURE_BRANCHES" >>"$GITHUB_OUTPUT"
 
 COMBINED_PR=$(gh pr ls --search 'head:combined-prs-branch' --json 'url' --jq '.[].url' | sort -u)
 echo "combined_pr=$COMBINED_PR" >>"$GITHUB_OUTPUT"
