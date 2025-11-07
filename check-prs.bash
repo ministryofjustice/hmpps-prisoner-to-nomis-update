4a3224334cca978979ff31386498e834ad3ce4ba
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-11-07 10:02:49 +0000

    SDIT-3162: 🐛 Fix failure count (#1818)

--- check-prs.bash
+++ check-prs.bash
@@ -9,7 +9,8 @@ else
 fi
 
 FAILURES=$(gh pr ls --search 'head:api-docs-' --json 'statusCheckRollup,headRefName' --jq '. | map(select(.statusCheckRollup[].conclusion == "FAILURE")) | .[].headRefName' | sort -u)
-FAILURE_COUNT=$(echo "$FAILURES" | wc -l | awk '{print $1}')
+# grep -c counts matching lines and the test ensures that grep exit status on non matches is ignored
+FAILURE_COUNT=$(echo -n "$FAILURES" | { grep "^.*$" -c || test $? = 1; })
 FAILURE_BRANCHES=$(echo "$FAILURES" | paste -sd' ' -)
 echo "failure_count=$FAILURE_COUNT" >>"$GITHUB_OUTPUT"
 echo "failures=$FAILURE_BRANCHES" >>"$GITHUB_OUTPUT"
@@ -17,7 +18,7 @@ echo "failures=$FAILURE_BRANCHES" >>"$GITHUB_OUTPUT"
 COMBINED_PR=$(gh pr ls --search 'head:combined-prs-branch' --json 'url' --jq '.[].url' | sort -u)
 echo "combined_pr=$COMBINED_PR" >>"$GITHUB_OUTPUT"
 
-PR_COUNT=$(gh pr ls --search 'head:api-docs-' --json 'url' --jq '.[].url' | sort -u | wc -l | awk '{print $1}')
+PR_COUNT=$(gh pr ls --search 'head:api-docs-' --json 'url' --jq '.[].url' | sort -u | { grep "^.*$" -c || test $? = 1; })
 echo "pr_count=$PR_COUNT" >>"$GITHUB_OUTPUT"
 
 if [[ $OUTPUT_COMPARISON = true ]]; then
