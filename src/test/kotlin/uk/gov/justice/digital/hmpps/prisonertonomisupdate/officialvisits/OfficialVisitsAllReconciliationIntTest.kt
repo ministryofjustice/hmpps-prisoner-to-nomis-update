2e24f02c5ff5c2edbd1023ecdbe73f54d744e9b6
andymarke <andy.marke@digital.justice.gov.uk>
2025-12-23 13:12:16 +0000

    SDIT-3138 Official visits reconciliation
    Now checks the existence of the mapping and DPS visit.
    
    TODO - compare NOMIS and DPS visits

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/officialvisits/OfficialVisitsAllReconciliationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/officialvisits/OfficialVisitsAllReconciliationIntTest.kt
@@ -16,11 +16,15 @@ import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.OfficialVisitMappingDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.VisitIdResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.officialvisits.OfficialVisitsDpsApiMockServer.Companion.syncOfficialVisit
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.officialvisits.OfficialVisitsNomisApiMockServer.Companion.officialVisitResponse
 
 class OfficialVisitsAllReconciliationIntTest(
   @Autowired private val reconciliationService: OfficialVisitsAllReconciliationService,
   @Autowired private val nomisApi: OfficialVisitsNomisApiMockServer,
+  @Autowired private val mappingApi: OfficialVisitsMappingApiMockServer,
 ) : IntegrationTestBase() {
 
   private val dpsApi = OfficialVisitsDpsApiExtension.dpsOfficialVisitsServer
@@ -31,8 +35,37 @@ class OfficialVisitsAllReconciliationIntTest(
     @BeforeEach
     fun setUp() {
       reset(telemetryClient)
-      nomisApi.stubGetOfficialVisitIds(content = listOf(VisitIdResponse(1L), VisitIdResponse(2L)))
-      dpsApi.stubGetOfficialVisitIds(content = listOf(SyncOfficialVisitId(100L)))
+      nomisApi.stubGetOfficialVisitIds(content = listOf(VisitIdResponse(1), VisitIdResponse(2), VisitIdResponse(3)))
+      nomisApi.stubGetOfficialVisitIdsByLastId(content = listOf(VisitIdResponse(1), VisitIdResponse(2), VisitIdResponse(3)))
+      dpsApi.stubGetOfficialVisitIds(content = listOf(SyncOfficialVisitId(100)))
+
+      mappingApi.stubGetByNomisIdsOrNull(
+        nomisVisitId = 1,
+        mapping = OfficialVisitMappingDto(
+          dpsId = "100",
+          nomisId = 1,
+          mappingType = OfficialVisitMappingDto.MappingType.MIGRATED,
+        ),
+      )
+      nomisApi.stubGetOfficialVisit(visitId = 1, response = officialVisitResponse().copy(visitId = 1))
+      dpsApi.stubGetOfficialVisit(officialVisitId = 100, response = syncOfficialVisit())
+
+      mappingApi.stubGetByNomisIdsOrNull(
+        nomisVisitId = 2,
+        mapping = null,
+      )
+      nomisApi.stubGetOfficialVisit(visitId = 2, response = officialVisitResponse().copy(visitId = 2, offenderNo = "A1234KT"))
+
+      mappingApi.stubGetByNomisIdsOrNull(
+        nomisVisitId = 3,
+        mapping = OfficialVisitMappingDto(
+          dpsId = "300",
+          nomisId = 3,
+          mappingType = OfficialVisitMappingDto.MappingType.MIGRATED,
+        ),
+      )
+      nomisApi.stubGetOfficialVisit(visitId = 3, response = officialVisitResponse().copy(visitId = 3, offenderNo = "A4321KT"))
+      dpsApi.stubGetOfficialVisit(officialVisitId = 300, response = null)
     }
 
     @Test
@@ -56,9 +89,9 @@ class OfficialVisitsAllReconciliationIntTest(
       verify(telemetryClient).trackEvent(
         eq("official-visits-all-reconciliation-report"),
         check {
-          assertThat(it).containsEntry("mismatch-count", "0")
-          assertThat(it).containsEntry("pages-count", "0")
-          assertThat(it).containsEntry("visit-count", "0")
+          assertThat(it).containsEntry("mismatch-count", "2")
+          assertThat(it).containsEntry("pages-count", "1")
+          assertThat(it).containsEntry("visit-count", "3")
         },
         isNull(),
       )
@@ -73,8 +106,45 @@ class OfficialVisitsAllReconciliationIntTest(
         eq("official-visits-all-reconciliation-mismatch-totals"),
         eq(
           mapOf(
-            "nomisTotal" to "2",
+            "nomisTotal" to "3",
             "dpsTotal" to "1",
+          ),
+        ),
+        isNull(),
+      )
+    }
+
+    @Test
+    fun `will output a mismatch for missing mapping`() = runTest {
+      reconciliationService.generateAllVisitsReconciliationReportBatch()
+      awaitReportFinished()
+
+      verify(telemetryClient).trackEvent(
+        eq("official-visits-all-reconciliation-mismatch"),
+        eq(
+          mapOf(
+            "nomisVisitId" to "2",
+            "offenderNo" to "A1234KT",
+            "reason" to "official-visit-mapping-missing",
+          ),
+        ),
+        isNull(),
+      )
+    }
+
+    @Test
+    fun `will output a mismatch for missing DPS visit when mapping exists`() = runTest {
+      reconciliationService.generateAllVisitsReconciliationReportBatch()
+      awaitReportFinished()
+
+      verify(telemetryClient).trackEvent(
+        eq("official-visits-all-reconciliation-mismatch"),
+        eq(
+          mapOf(
+            "nomisVisitId" to "3",
+            "dpsVisitId" to "300",
+            "offenderNo" to "A4321KT",
+            "reason" to "dps-record-missing",
           ),
         ),
         isNull(),
