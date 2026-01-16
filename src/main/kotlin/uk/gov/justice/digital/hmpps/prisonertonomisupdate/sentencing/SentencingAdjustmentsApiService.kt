04f8a2a5e2eaab7eb00d6199467a6364ff5c486d
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-01-16 13:16:20 +0000

    SDIT-3322: ✨ Use sentencing api and model (#2051)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/sentencing/SentencingAdjustmentsApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/sentencing/SentencingAdjustmentsApiService.kt
@@ -1,35 +1,35 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.sentencing
 
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForStatus
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.sentencing.adjustments.api.AdjustmentControllerApi
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.sentencing.adjustments.api.LegacyControllerApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.sentencing.adjustments.model.AdjustmentDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.sentencing.adjustments.model.LegacyAdjustment
+import java.util.UUID
 
 @Service
 class SentencingAdjustmentsApiService(private val sentenceAdjustmentsApiWebClient: WebClient) {
-  companion object {
-    const val LEGACY_CONTENT_TYPE = "application/vnd.nomis-offence+json"
-  }
+  private val legacyControllerApi: LegacyControllerApi = LegacyControllerApi(sentenceAdjustmentsApiWebClient)
+  private val adjustmentControllerApi: AdjustmentControllerApi = AdjustmentControllerApi(sentenceAdjustmentsApiWebClient)
 
-  suspend fun getAdjustment(adjustmentId: String): LegacyAdjustment = sentenceAdjustmentsApiWebClient.get()
-    .uri("/legacy/adjustments/{adjustmentId}", adjustmentId)
-    .header("Content-Type", LEGACY_CONTENT_TYPE)
-    .retrieve()
-    .awaitBody()
+  suspend fun getAdjustment(adjustmentId: String): LegacyAdjustment = legacyControllerApi
+    .get(UUID.fromString(adjustmentId)).awaitSingle()
 
-  suspend fun getAdjustmentOrNull(adjustmentId: String): LegacyAdjustment? = sentenceAdjustmentsApiWebClient.get()
-    .uri("/legacy/adjustments/{adjustmentId}", adjustmentId)
-    .header("Content-Type", LEGACY_CONTENT_TYPE)
+  suspend fun getAdjustmentOrNull(adjustmentId: String): LegacyAdjustment? = legacyControllerApi.prepare(
+    legacyControllerApi.getRequestConfig(UUID.fromString(adjustmentId)),
+  )
     .retrieve()
     .awaitBodyOrNullForNotFound()
 
-  suspend fun getAdjustments(offenderNo: String): List<AdjustmentDto>? = sentenceAdjustmentsApiWebClient.get()
-    .uri("/adjustments?person={offenderNo}", offenderNo)
+  suspend fun getAdjustments(offenderNo: String): List<AdjustmentDto>? = adjustmentControllerApi.prepare(
+    adjustmentControllerApi.findByPersonRequestConfig(person = offenderNo),
+  )
     .retrieve()
     .awaitBodyOrNullForStatus(NOT_FOUND, HttpStatus.UNPROCESSABLE_CONTENT)
 }
