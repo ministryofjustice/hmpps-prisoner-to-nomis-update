97f2614caa3345ff5ba019a6c939fed64ab2d923
steverendell <steve.rendell@syscon.net>
2026-01-21 10:45:14 +0000

    SDIT-3337 StackOverflowError for DPS case notes with a lot of text or amendments

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/casenotes/CaseNotesReconciliationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/casenotes/CaseNotesReconciliationService.kt
@@ -366,23 +366,25 @@ private fun String.truncateToFixNomisMaxLength(): String = // encodedLength alwa
     substring(0, MAX_CASENOTE_LENGTH_BYTES - (Utf8.encodedLength(this) - length) - SEE_DPS_REPLACEMENT.length) + SEE_DPS_REPLACEMENT
   }
 
-// same NEW logic as in nomis prisoner api
+// NEW logic. NOTE: code copied from nomis prisoner api StringUtils
 fun String.truncateToUtf8Length(maxLength: Int, includeSeeDpsSuffix: Boolean = false): String {
   // ensure doesn't exceed the number of bytes Oracle can take - allowing for suffix to be added
-  return if (Utf8.encodedLength(this) <= maxLength) {
-    this
-  } else {
-    val suffix = if (includeSeeDpsSuffix) SEE_DPS_REPLACEMENT else ""
-    this.truncateByOneCharacterUntilFitToUtf8Length(maxLength - suffix.length) + suffix
-  }
-}
 
-private fun String.truncateByOneCharacterUntilFitToUtf8Length(maxLength: Int): String {
-  // so we don't cut into a double/triple byte character while truncating check
+  if (Utf8.encodedLength(this) <= maxLength) {
+    return this
+  }
+
+  var truncated: String = this
+  val suffix = if (includeSeeDpsSuffix) SEE_DPS_REPLACEMENT else ""
+  val checkLength = maxLength - suffix.length
+
+  // so we don't cut into a double/triple byte character while truncating check that
   // we still have a valid string before returning, even if it fits the number of bytes
-  if (this.isStillValid() && Utf8.encodedLength(this) <= maxLength) return this
-  // Keep knocking of the last character until it fits the number of bytes and remains a valid string
-  return this.take(this.length - 1).truncateByOneCharacterUntilFitToUtf8Length(maxLength)
+  while (!truncated.isStillValid() || Utf8.encodedLength(truncated) > checkLength) {
+    // Keep knocking off the last character until it fits the number of bytes and remains a valid string
+    truncated = truncated.take(truncated.length - 1)
+  }
+  return truncated + suffix
 }
 
 // Utf8.encodedLength will throw if the resulting String is cut at the incorrect boundary
