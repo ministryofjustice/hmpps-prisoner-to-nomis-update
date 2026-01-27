f30d6e15bbc6c0214038aadd3462943d75ebec81
karenmillermoj <karen.miller3@justice.gov.uk>
2026-01-27 13:16:49 +0000

    SDIT-3315 lint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsDpsApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsDpsApiServiceTest.kt
@@ -142,7 +142,7 @@ class IncidentsDpsApiServiceTest {
             .withQueryParam("location", equalTo("ASI"))
             .withQueryParam("status", havingExactly("AWAITING_REVIEW", "NEEDS_UPDATING", "ON_HOLD", "UPDATED"))
             .withQueryParam("size", equalTo("1"))
-            .withQueryParam("^(?!location$|status$|size$).+", absent())
+            .withQueryParam("^(?!location$|status$|size$).+", absent()),
         )
       }
 
@@ -181,7 +181,7 @@ class IncidentsDpsApiServiceTest {
           .withQueryParam("location", equalTo("ASI"))
           .withQueryParam("status", havingExactly("CLOSED", "DUPLICATE", "NOT_REPORTABLE", "REOPENED", "WAS_CLOSED"))
           .withQueryParam("size", equalTo("1"))
-          .withQueryParam("^(?!location$|status$|size$).+", absent())
+          .withQueryParam("^(?!location$|status$|size$).+", absent()),
       )
     }
 
