36cf33b6139eb6fd485576dadc1971f34b1830cd
Andy Marke <andy.marke@digital.justice.gov.uk>
2026-02-10 10:49:22 +0000

    SDIT-3441 Check time and visit slots (#2218)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/officialvisits/VisitSlotsReconciliationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/officialvisits/VisitSlotsReconciliationService.kt
@@ -9,7 +9,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.ReconciliationResult
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.VisitTimeSlotResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.officialvisits.model.SyncTimeSlotSummaryItem
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.awaitBoth
+import java.time.LocalDate
 
 @Service
 class VisitSlotsReconciliationService(
@@ -71,19 +74,58 @@ class VisitSlotsReconciliationService(
     val nomisSlotPerDay = nomisTimeSlots.timeSlots.groupBy { it.dayOfWeek.toString() }
     val dpsSlotPerDay = dpsTimeSlots.timeSlots.groupBy { it.timeSlot.dayCode.toString() }
 
-    val mismatches = listOf("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN").mapNotNull { dayOfWeek ->
+    val mismatches = listOf("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN").flatMap { dayOfWeek ->
       val nomisSlots = nomisSlotPerDay[dayOfWeek] ?: emptyList()
       val dpsSlots = dpsSlotPerDay[dayOfWeek] ?: emptyList()
       if (nomisSlots.size != dpsSlots.size) {
-        MismatchTimeSlot(prisonId = prisonId, dayOfWeek = dayOfWeek, reason = "time-slot-count-mismatch").also {
+        listOf(
+          MismatchTimeSlot(prisonId = prisonId, dayOfWeek = dayOfWeek, reason = "time-slot-count-mismatch").also {
+            telemetryClient.trackEvent(
+              "$TELEMETRY_VISIT_SLOTS_PREFIX-mismatch",
+              mapOf(
+                "prisonId" to prisonId,
+                "dayOfWeek" to dayOfWeek,
+                "nomisCount" to nomisSlots.size.toString(),
+                "dpsCount" to dpsSlots.size.toString(),
+                "reason" to "time-slot-count-mismatch",
+              ),
+            )
+          },
+        )
+      } else {
+        checkTimeSlotsAtPrisonOnDay(
+          prisonId = prisonId,
+          dayOfWeek = dayOfWeek,
+          nomisSlots = nomisSlots,
+          dpsSlots = dpsSlots,
+        )
+      }
+    }
+
+    return mismatches
+  }
+
+  private fun checkTimeSlotsAtPrisonOnDay(
+    prisonId: String,
+    dayOfWeek: String,
+    nomisSlots: List<VisitTimeSlotResponse>,
+    dpsSlots: List<SyncTimeSlotSummaryItem>,
+  ): List<MismatchTimeSlot> {
+    val dpsTimeSlotSummaries = dpsSlots.map { it.toTimeSlotSummary() }
+    return nomisSlots.mapNotNull { nomisNomisTimeSlotData ->
+      val nomisTimeSlot = nomisNomisTimeSlotData.toTimeSlotSummary()
+      val dpsTimeSlot = dpsTimeSlotSummaries.find { it == nomisTimeSlot }
+      if (dpsTimeSlot == null) {
+        MismatchTimeSlot(prisonId = prisonId, dayOfWeek = dayOfWeek, reason = "matching-time-slot-not-found").also {
           telemetryClient.trackEvent(
             "$TELEMETRY_VISIT_SLOTS_PREFIX-mismatch",
             mapOf(
               "prisonId" to prisonId,
               "dayOfWeek" to dayOfWeek,
-              "nomisCount" to nomisSlots.size.toString(),
-              "dpsCount" to dpsSlots.size.toString(),
-              "reason" to "time-slot-count-mismatch",
+              "startTime" to nomisTimeSlot.startTime,
+              "endTime" to nomisTimeSlot.endTime,
+              "nomisTimeSlotSequence" to nomisNomisTimeSlotData.timeSlotSequence,
+              "reason" to "matching-time-slot-not-found",
             ),
           )
         }
@@ -91,13 +133,50 @@ class VisitSlotsReconciliationService(
         null
       }
     }
-
-    return mismatches
   }
 }
+
+private fun VisitTimeSlotResponse.toTimeSlotSummary() = TimeSlotSummary(
+  startTime = this.startTime,
+  endTime = this.endTime,
+  expiryDate = this.expiryDate,
+  startDate = this.effectiveDate,
+  visitSlots = this.visitSlots.map {
+    VisitSlotSummary(
+      maxAdults = it.maxAdults ?: 0,
+      maxGroups = it.maxGroups ?: 0,
+    )
+  }.sortedWith(compareBy<VisitSlotSummary> { it.maxAdults }.thenBy { it.maxGroups }),
+)
+
+private fun SyncTimeSlotSummaryItem.toTimeSlotSummary() = TimeSlotSummary(
+  startTime = this.timeSlot.startTime,
+  endTime = this.timeSlot.endTime,
+  expiryDate = this.timeSlot.expiryDate,
+  startDate = this.timeSlot.effectiveDate,
+  visitSlots = this.visitSlots.map {
+    VisitSlotSummary(
+      maxAdults = it.maxAdults ?: 0,
+      maxGroups = it.maxGroups ?: 0,
+    )
+  }.sortedWith(compareBy<VisitSlotSummary> { it.maxAdults }.thenBy { it.maxGroups }),
+)
 
 data class MismatchTimeSlot(
   val prisonId: String,
   val dayOfWeek: String,
   val reason: String,
+)
+
+data class TimeSlotSummary(
+  val startTime: String,
+  val endTime: String,
+  val expiryDate: LocalDate?,
+  val startDate: LocalDate,
+  val visitSlots: List<VisitSlotSummary>,
+)
+
+data class VisitSlotSummary(
+  val maxAdults: Int,
+  val maxGroups: Int,
 )
