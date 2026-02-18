6467820c94b5b6d19a677a567ca8f8a0a18edc37
andymarke <andy.marke@digital.justice.gov.uk>
2026-02-18 11:21:09 +0000

    SDIT-3496 Sync create time slot
    TODO - check unhappy paths

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/officialvisits/OfficialVisitsRetryQueueService.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.prisonertonomisupdate.officialvisits
+
+import com.microsoft.applicationinsights.TelemetryClient
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.QueueService
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryQueueService
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+
+@Service
+class OfficialVisitsRetryQueueService(
+  hmppsQueueService: HmppsQueueService,
+  telemetryClient: TelemetryClient,
+  queueService: QueueService,
+) : RetryQueueService(
+  queueId = "officialvisits",
+  hmppsQueueService = hmppsQueueService,
+  telemetryClient = telemetryClient,
+  queueService = queueService,
+)
