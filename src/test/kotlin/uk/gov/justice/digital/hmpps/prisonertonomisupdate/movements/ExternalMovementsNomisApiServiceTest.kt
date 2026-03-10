ae6efaec5ed321563ad50e90d0c322eeba656d32
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-03-10 10:35:22 +0000

    SDIT-3309 Add client for booking TAP details (#2387)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/ExternalMovementsNomisApiServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/ExternalMovementsNomisApiServiceTest.kt
@@ -8,12 +8,15 @@ import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
 import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Import
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.SpringAPIServiceTest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.ExternalMovementsNomisApiMockServer.Companion.createTemporaryAbsenceRequest
@@ -234,6 +237,63 @@ class ExternalMovementsNomisApiServiceTest {
 
       assertThrows<WebClientResponseException.InternalServerError> {
         apiService.getTemporaryAbsenceSummary("A1234BC")
+      }
+    }
+  }
+
+  @Nested
+  inner class GetTemporaryAbsencesByBooking {
+    @Test
+    internal fun `will pass oath2 token to service`() = runTest {
+      mockServer.stubGetBookingTemporaryAbsences(bookingId = 12345)
+
+      apiService.getBookingTemporaryAbsences(bookingId = 12345)
+
+      mockServer.verify(
+        getRequestedFor(anyUrl()).withHeader("Authorization", equalTo("Bearer ABCDE")),
+      )
+    }
+
+    @Test
+    internal fun `will pass offender number to service`() = runTest {
+      mockServer.stubGetBookingTemporaryAbsences(bookingId = 12345)
+
+      apiService.getBookingTemporaryAbsences(bookingId = 12345)
+
+      mockServer.verify(
+        getRequestedFor(urlPathEqualTo("/movements/booking/12345/temporary-absences")),
+      )
+    }
+
+    @Test
+    fun `will return temporary absences`() = runTest {
+      mockServer.stubGetBookingTemporaryAbsences(bookingId = 12345)
+
+      val result = apiService.getBookingTemporaryAbsences(bookingId = 12345)!!
+
+      assertThat(result.bookingId).isEqualTo(12345L)
+      assertThat(result.temporaryAbsenceApplications).hasSize(1)
+      assertThat(result.temporaryAbsenceApplications[0].absences[0].scheduledTemporaryAbsence?.eventId).isEqualTo(1)
+      assertThat(result.temporaryAbsenceApplications[0].absences[0].scheduledTemporaryAbsenceReturn?.eventId).isEqualTo(2)
+      assertThat(result.temporaryAbsenceApplications[0].absences[0].temporaryAbsence?.sequence).isEqualTo(3)
+      assertThat(result.temporaryAbsenceApplications[0].absences[0].temporaryAbsenceReturn?.sequence).isEqualTo(4)
+      assertThat(result.unscheduledTemporaryAbsences[0].sequence).isEqualTo(1)
+      assertThat(result.unscheduledTemporaryAbsenceReturns[0].sequence).isEqualTo(2)
+    }
+
+    @Test
+    fun `will return null when offender does not exist`() = runTest {
+      mockServer.stubGetBookingTemporaryAbsences(bookingId = 12345, status = NOT_FOUND)
+
+      assertThat(apiService.getBookingTemporaryAbsences(bookingId = 12345)).isNull()
+    }
+
+    @Test
+    fun `will throw error when API returns an error`() = runTest {
+      mockServer.stubGetBookingTemporaryAbsences(bookingId = 12345, status = INTERNAL_SERVER_ERROR)
+
+      assertThrows<WebClientResponseException.InternalServerError> {
+        apiService.getBookingTemporaryAbsences(bookingId = 12345)
       }
     }
   }
