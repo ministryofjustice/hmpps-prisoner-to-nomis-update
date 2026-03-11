c0ea9ead37da2783231ab0d96ed3a7197079f600
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-03-11 08:01:32 +0000

    SDIT-3535: ✨ Initial framework for religions differences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/coreperson/CorePersonCprApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/coreperson/CorePersonCprApiService.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.util.context.Context
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.coreperson.api.HMPPSPersonAPIApi
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.coreperson.model.CanonicalRecord
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.coreperson.model.PrisonCanonicalRecord
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
 
@@ -19,7 +19,7 @@ class CorePersonCprApiService(
 
   private val personApi = HMPPSPersonAPIApi(corePersonApiWebClient)
 
-  suspend fun getCorePerson(prisonNumber: String): CanonicalRecord? = personApi
+  suspend fun getCorePerson(prisonNumber: String): PrisonCanonicalRecord? = personApi
     .prepare(personApi.getRecord1RequestConfig(prisonNumber))
     .retrieve()
     .awaitBodyOrNullForNotFound(backoffSpec)
