242a02ae24c27b17519a71a8be0ef4969e7e945b
Andy Marke <andy.marke@digital.justice.gov.uk>
2026-04-20 14:36:37 +0100

    SDIT-3660 Removed 409 create alert handling (#2584)
    We tried to create an alert mapping when a duplicate alert in NOMIS was detected. This would mean a double POST to NOMS API would be handled automatcially.
    
    This causes more issues then it solved; since you can the same alert in NOMIS multiple times but only one can be active.
    
    This caused timing issues where an alert can be made inactive and the same alert code created around the same time. If the events are processed out of order the mappings ends up wrong.
    
    Also refactored the webClient to use generated API.
    
    Also refactored test to handle DLQ scenarios better

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/alerts/AlertsNomisApiService.kt
@@ -1,12 +1,10 @@
 package uk.gov.justice.digital.hmpps.prisonertonomisupdate.alerts
 
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBodilessEntity
-import org.springframework.web.reactive.function.client.awaitBody
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.api.AlertsResourceApi
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.AlertId
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.AlertResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateAlertCode
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.CreateAlertRequest
@@ -16,8 +14,6 @@ import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.Pr
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateAlertCode
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateAlertRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpdateAlertType
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.SuccessOrDuplicate
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.awaitSuccessOrDuplicate
 
 @Service
 class AlertsNomisApiService(@Qualifier("nomisApiWebClient") private val webClient: WebClient) {
@@ -26,107 +22,57 @@ class AlertsNomisApiService(@Qualifier("nomisApiWebClient") private val webClien
   suspend fun createAlert(
     offenderNo: String,
     nomisAlert: CreateAlertRequest,
-  ): SuccessOrDuplicate<CreateAlertResponse, AlertId> = with(api) {
-    awaitSuccessOrDuplicate(
-      createAlertRequestConfig(
-        offenderNo = offenderNo,
-        createAlertRequest = nomisAlert,
-      ),
-    )
-  }
+  ): CreateAlertResponse = api.createAlert(
+    offenderNo = offenderNo,
+    createAlertRequest = nomisAlert,
+  ).awaitSingle()
 
-  suspend fun updateAlert(bookingId: Long, alertSequence: Long, nomisAlert: UpdateAlertRequest): AlertResponse = webClient.put().uri(
-    "/prisoners/booking-id/{bookingId}/alerts/{alertSequence}",
-    bookingId,
-    alertSequence,
-  )
-    .bodyValue(nomisAlert)
-    .retrieve()
-    .awaitBody()
+  suspend fun updateAlert(bookingId: Long, alertSequence: Long, nomisAlert: UpdateAlertRequest): AlertResponse = api.updateAlert(
+    bookingId = bookingId,
+    alertSequence = alertSequence,
+    updateAlertRequest = nomisAlert,
+  ).awaitSingle()
 
   suspend fun deleteAlert(bookingId: Long, alertSequence: Long) {
-    webClient.delete().uri(
-      "/prisoners/booking-id/{bookingId}/alerts/{alertSequence}",
-      bookingId,
-      alertSequence,
-    )
-      .retrieve()
-      .awaitBodilessEntity()
+    api.deleteAlert(bookingId, alertSequence).awaitSingle()
   }
 
-  suspend fun resynchroniseAlerts(offenderNo: String, nomisAlerts: List<CreateAlertRequest>): List<CreateAlertResponse> = webClient.post()
-    .uri(
-      "/prisoners/{offenderNo}/alerts/resynchronise",
-      offenderNo,
-    )
-    .bodyValue(nomisAlerts)
-    .retrieve()
-    .awaitBody()
+  suspend fun resynchroniseAlerts(offenderNo: String, nomisAlerts: List<CreateAlertRequest>): List<CreateAlertResponse> = api.resynchroniseAlerts(
+    offenderNo = offenderNo,
+    createAlertRequest = nomisAlerts,
+  ).awaitSingle()
 
-  suspend fun getAlertsForReconciliation(offenderNo: String): PrisonerAlertsResponse = webClient.get().uri(
-    "/prisoners/{offenderNo}/alerts/reconciliation",
-    offenderNo,
-  )
-    .retrieve()
-    .awaitBody()
+  suspend fun getAlertsForReconciliation(offenderNo: String): PrisonerAlertsResponse = api.getActiveAlertsForReconciliation(offenderNo).awaitSingle()
 
   suspend fun createAlertCode(alertCode: CreateAlertCode) {
-    webClient.post()
-      .uri("/alerts/codes")
-      .bodyValue(alertCode)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.createAlertCode(alertCode).awaitSingle()
   }
 
   suspend fun updateAlertCode(code: String, alertCode: UpdateAlertCode) {
-    webClient.put()
-      .uri("/alerts/codes/{code}", code)
-      .bodyValue(alertCode)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.updateAlertCode(code, alertCode).awaitSingle()
   }
 
   suspend fun deactivateAlertCode(code: String) {
-    webClient.put()
-      .uri("/alerts/codes/{code}/deactivate", code)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.deactivateAlertCode(code).awaitSingle()
   }
 
   suspend fun reactivateAlertCode(code: String) {
-    webClient.put()
-      .uri("/alerts/codes/{code}/reactivate", code)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.reactivateAlertCode(code).awaitSingle()
   }
 
   suspend fun createAlertType(alertType: CreateAlertType) {
-    webClient.post()
-      .uri("/alerts/types")
-      .bodyValue(alertType)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.createAlertType(alertType).awaitSingle()
   }
 
   suspend fun updateAlertType(code: String, alertType: UpdateAlertType) {
-    webClient.put()
-      .uri("/alerts/types/{code}", code)
-      .bodyValue(alertType)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.updateAlertType(code, alertType).awaitSingle()
   }
 
   suspend fun deactivateAlertType(code: String) {
-    webClient.put()
-      .uri("/alerts/types/{code}/deactivate", code)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.deactivateAlertType(code).awaitSingle()
   }
 
   suspend fun reactivateAlertType(code: String) {
-    webClient.put()
-      .uri("/alerts/types/{code}/reactivate", code)
-      .retrieve()
-      .awaitBodilessEntity()
+    api.reactivateAlertType(code).awaitSingle()
   }
 }
