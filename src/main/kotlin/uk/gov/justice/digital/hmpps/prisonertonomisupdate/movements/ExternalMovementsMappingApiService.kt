0fa56f25f003f680da5f0daef6649c1e6842a6ff
Mike Halma <mike.halma@digital.justice.gov.uk>
2026-04-22 12:00:42 +0100

    SDIT-2832 Switch to new Tap Movement mapping API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/ExternalMovementsMappingApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/ExternalMovementsMappingApiService.kt
@@ -7,10 +7,11 @@ import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodilessEntityOrThrowOnConflict
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.api.TapApplicationResourceApi
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.api.TapMovementResourceApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.api.TapScheduleResourceApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.api.TemporaryAbsenceResourceApi
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.ExternalMovementSyncMappingDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.TapApplicationMappingDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.TapMovementMappingDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.TapScheduleMappingDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.TemporaryAbsencesPrisonerMappingIdsDto
 import java.util.*
@@ -22,6 +23,7 @@ class ExternalMovementsMappingApiService(
   private val mappingApi = TemporaryAbsenceResourceApi(webClient)
   private val applicationApi = TapApplicationResourceApi(webClient)
   private val scheduleApi = TapScheduleResourceApi(webClient)
+  private val movementApi = TapMovementResourceApi(webClient)
 
   suspend fun createTapApplicationMapping(mapping: TapApplicationMappingDto) = applicationApi
     .prepare(applicationApi.createTapApplicationMappingRequestConfig(mapping))
@@ -47,13 +49,13 @@ class ExternalMovementsMappingApiService(
     .retrieve()
     .awaitBodyOrNullForNotFound()
 
-  suspend fun createExternalMovementMapping(mapping: ExternalMovementSyncMappingDto) = mappingApi
-    .prepare(mappingApi.createExternalMovementSyncMappingRequestConfig(mapping))
+  suspend fun createTapMovementMapping(mapping: TapMovementMappingDto) = movementApi
+    .prepare(movementApi.createTapMovementMappingRequestConfig(mapping))
     .retrieve()
     .awaitBodilessEntityOrThrowOnConflict()
 
-  suspend fun getExternalMovementMapping(dpsId: UUID): ExternalMovementSyncMappingDto? = mappingApi
-    .prepare(mappingApi.getExternalMovementSyncMappingByDpsIdRequestConfig(dpsId))
+  suspend fun getTapMovementMapping(dpsId: UUID): TapMovementMappingDto? = movementApi
+    .prepare(movementApi.getTapMovementMappingByDpsIdRequestConfig(dpsId))
     .retrieve()
     .awaitBodyOrNullForNotFound()
 
