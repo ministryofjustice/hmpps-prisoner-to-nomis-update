72d07485253ff7cd56c3e9372f82a37be94310d1
karenmillermoj <karen.miller3@justice.gov.uk>
2026-04-24 13:00:25 +0100

    SDIT-3678 Retry incident upsert if receive 423 status

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/incidents/IncidentsNomisApiService.kt
@@ -2,25 +2,38 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.incidents
 
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.util.context.Context
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyWithRetry
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.api.IncidentResourceApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.IncidentAgencyId
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.IncidentIdResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.IncidentResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.IncidentsReconciliationResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertIncidentRequest
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
 
 @Service
 class IncidentsNomisApiService(
   @Qualifier("nomisApiWebClient") private val webClient: WebClient,
+  retryApiService: RetryApiService,
 ) {
   private val nomisIncidentsApi = IncidentResourceApi(webClient)
+  private val retrySpecOnLocked = retryApiService.getBackoffSpec().filter { ex ->
+    ex is WebClientResponseException && ex.statusCode == HttpStatus.LOCKED
+  }
 
   suspend fun upsertIncident(nomisId: Long, request: UpsertIncidentRequest) {
     nomisIncidentsApi
       .upsertIncident(nomisId, request)
-      .awaitSingle()
+      .awaitBodyWithRetry(
+        retrySpecOnLocked.withRetryContext(
+          Context.of("api", "nomis-prisoner-api", "url", "/incidents/$nomisId"),
+        ),
+      )
   }
 
   suspend fun deleteIncident(nomisId: Long) {
