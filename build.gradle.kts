ab84d70223c303fa8c73c6e0fdee67c80289c7e6
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-05-18 10:09:04 +0100

    SDIT-3813: 🔇 Suppress azure false positive

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ configurations {
   }
 }
 
+dependencyCheck {
+  suppressionFiles.add("azure-dependency-check-suppress.xml")
+}
+
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.2.0")
   implementation("org.springframework.boot:spring-boot-starter-security")
