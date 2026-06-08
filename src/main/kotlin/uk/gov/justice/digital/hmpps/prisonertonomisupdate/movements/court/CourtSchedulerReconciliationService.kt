774f01acabeb756f5274bbbca580061a9d4d62e6
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-06-08 14:55:10 +0100

    SDIT-3851 Add court scheduler reconciliation for active prisoners only (#2820)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/court/CourtSchedulerReconciliationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/court/CourtSchedulerReconciliationService.kt
@@ -4,18 +4,10 @@ import com.microsoft.applicationinsights.TelemetryClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.courtscheduler.model.ReconciliationResponse
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.courtsentencing.CourtSentencingMappingService
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.ReconciliationErrorPageResult
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.ReconciliationPageResult
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.ReconciliationResult
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.ReconciliationSuccessPageResult
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.generateReconciliationReport
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.court.MismatchPrisonerCourtMovementDetails.Type.MOVEMENT_TIME
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.court.MismatchPrisonerCourtMovementDetails.Type.REASON
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.court.MismatchPrisonerCourtScheduleDetails.Type.COURT
@@ -25,14 +17,9 @@ import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.court.Mismat
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.court.MismatchedPrisonerCourtMovementIds.Type.MISSING_MAPPING_SCHEDULE
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.findMatches
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.findMissing
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.taps.TapActivePrisonersReconciliationService
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.taps.TapActivePrisonersReconciliationService.Companion.TELEMETRY_ACTIVE_TAPS
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.taps.TapAllPrisonersReconciliationService
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.CourtSchedulerPrisonerMappingIdsDto
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.BookingCourtMovements
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.OffenderCourtMovementsResponse
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.PrisonerId
-import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.NomisApiService
 import java.time.LocalDateTime
 import java.util.*
 
@@ -43,85 +30,12 @@ class CourtSchedulerReconciliationService(
   private val telemetryClient: TelemetryClient,
   private val nomisApiService: CourtSchedulerNomisApiService,
   private val dpsApiService: CourtSchedulerDpsApiService,
-  private val nomisPrisonerApiService: NomisApiService,
   private val mappingService: CourtSchedulerMappingApiService,
   private val courtSentencingMappingService: CourtSentencingMappingService,
-  @param:Value($$"${reports.temporary-absences.active-prisoners.reconciliation.page-size}") private val pageSize: Int = 100,
-  @param:Value($$"${reports.temporary-absences.active-prisoners.reconciliation.thread-count}") private val threadCount: Int = 15,
 ) {
-  internal companion object {
-    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  companion object {
     const val TELEMETRY_COURT_SCHEDULER = "court-scheduler-reconciliation"
   }
-
-  suspend fun generateCourtSchedulerReconciliationReportBatch() {
-    telemetryClient.trackEvent(
-      "$TELEMETRY_COURT_SCHEDULER-requested",
-      mapOf(),
-    )
-
-    runCatching { generateCourtSchedulerReconciliationReport() }
-      .onSuccess {
-        log.info("Court scheduler reconciliation report completed with ${it.mismatches.size} mismatches")
-        telemetryClient.trackEvent(
-          "$TELEMETRY_COURT_SCHEDULER-report",
-          mapOf(
-            "prisoners-count" to it.itemsChecked.toString(),
-            "pages-count" to it.pagesChecked.toString(),
-            "mismatch-count" to it.mismatches.size.toString(),
-            "success" to "true",
-          ) + it.mismatches.asMap(),
-        )
-      }
-      .onFailure {
-        telemetryClient.trackEvent("$TELEMETRY_ACTIVE_TAPS-report", mapOf("success" to "false"))
-        TapActivePrisonersReconciliationService.log.error("Temporary absences all prisoners reconciliation report failed", it)
-      }
-  }
-
-  private fun List<PrisonerId>.asMap(): Pair<String, String> = this
-    .sortedBy { it.offenderNo }.take(10).let { mismatch -> "offenderNos" to mismatch.joinToString { it.offenderNo } }
-
-  suspend fun generateCourtSchedulerReconciliationReport(): ReconciliationResult<PrisonerId> = generateReconciliationReport(
-    threadCount = threadCount,
-    pageSize = pageSize,
-    checkMatch = ::checkPrisonersMatch,
-    nextPage = ::getNextPrisonersForPage,
-  )
-
-  private suspend fun getNextPrisonersForPage(lastOffenderId: Long): ReconciliationPageResult<PrisonerId> = runCatching {
-    nomisPrisonerApiService.getAllPrisoners(fromId = lastOffenderId, pageSize = pageSize)
-  }.onFailure {
-    telemetryClient.trackEvent(
-      "$TELEMETRY_COURT_SCHEDULER-mismatch-page-error",
-      mapOf(
-        "offenderId" to lastOffenderId.toString(),
-      ),
-    )
-    log.error("Unable to match entire page of offenders from offender ID: $lastOffenderId", it)
-  }
-    .map {
-      ReconciliationSuccessPageResult(
-        ids = it.prisonerIds,
-        last = it.lastOffenderId,
-      )
-    }
-    .getOrElse { ReconciliationErrorPageResult(it) }
-    .also { TapAllPrisonersReconciliationService.log.info("Page requested from offender ID: $lastOffenderId, with $pageSize offenders") }
-
-  suspend fun checkPrisonersMatch(prisonerId: PrisonerId): PrisonerId? = runCatching {
-    checkPrisonersMatch(prisonerId.offenderNo).takeIf { it.isNotEmpty() }
-      ?.let { prisonerId }
-  }.onFailure {
-    TapActivePrisonersReconciliationService.log.error("Unable to match temporary absences for prisoner ${prisonerId.offenderNo}", it)
-    telemetryClient.trackEvent(
-      "$TELEMETRY_COURT_SCHEDULER-mismatch-error",
-      mapOf(
-        "offenderNo" to prisonerId.offenderNo,
-        "reason" to (it.message ?: "unknown"),
-      ),
-    )
-  }.getOrNull()
 
   suspend fun checkPrisonersMatch(offenderNo: String, suppressTelemetry: Boolean = false): List<MismatchedPrisonerCourtMovements> = withContext(Dispatchers.Unconfined) {
     val nomisMovements = async { nomisApiService.getOffenderCourtMovementsOrNull(offenderNo) }
