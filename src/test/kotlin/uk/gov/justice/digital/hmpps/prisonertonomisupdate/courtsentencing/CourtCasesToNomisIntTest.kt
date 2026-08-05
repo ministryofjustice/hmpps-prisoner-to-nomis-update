a4df834e58c666fe05448612751dce45b7906e6a
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2026-08-05 10:33:58 +0100

    sdit_3988 more logging to aid bug fix (#3060)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/courtsentencing/CourtCasesToNomisIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/courtsentencing/CourtCasesToNomisIntTest.kt
@@ -99,6 +99,8 @@ private const val OFFENDER_NO = "AB12345"
 private const val DONCASTER_COURT_CODE = "DRBYYC"
 private const val CASE_REFERENCE = "ABC4999"
 private const val APPEARANCE_VL = "VL"
+private const val DPS_SENTENCE_CATEGORY = "CAT"
+private const val DPS_CALC_TYPE = "CALC"
 
 class CourtCasesToNomisIntTest : SqsIntegrationTestBase() {
 
@@ -2355,6 +2357,8 @@ class CourtCasesToNomisIntTest : SqsIntegrationTestBase() {
             assertThat(it["dpsCourtAppearanceId"]).isEqualTo(DPS_COURT_APPEARANCE_ID)
             assertThat(it["dpsCourtCaseId"]).isEqualTo(COURT_CASE_ID_FOR_CREATION)
             assertThat(it["dpsChargeId"]).isEqualTo(DPS_COURT_CHARGE_ID)
+            assertThat(it["dpsSentenceCategory"]).isEqualTo(DPS_SENTENCE_CATEGORY)
+            assertThat(it["dpsSentenceCalcType"]).isEqualTo(DPS_CALC_TYPE)
             assertThat(it["nomisChargeId"]).isEqualTo(NOMIS_COURT_CHARGE_ID.toString())
             assertThat(it["nomisCourtCaseId"]).isEqualTo(NOMIS_COURT_CASE_ID_FOR_CREATION.toString())
             assertThat(it["offenderNo"]).isEqualTo(OFFENDER_NO)
