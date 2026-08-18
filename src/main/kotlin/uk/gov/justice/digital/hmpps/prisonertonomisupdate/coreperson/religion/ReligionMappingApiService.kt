d376038e59e91b6c62a75e84a3cbd078ed90c11b
Richard Popple <2195350+richardpopple@users.noreply.github.com>
2026-08-18 12:01:05 +0100

    SDIT-4058 fixing endpoint url

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/coreperson/religion/ReligionMappingApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/coreperson/religion/ReligionMappingApiService.kt
@@ -3,18 +3,22 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.coreperson.religion
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
+import reactor.util.context.Context
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyWithRetry
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.api.ReligionResourceApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.ReligionMappingDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.RetryApiService
 
 @Service
-class ReligionMappingApiService(@Qualifier("mappingWebClient") private val webClient: WebClient) {
-  suspend fun getByCprIds(crpReligionIds: List<String>): List<ReligionMappingDto> = webClient.get()
-    .uri {
-      it.path("/mapping/religion/cpr-ids").queryParam(
-        "ids",
-        *crpReligionIds.toTypedArray(),
-      ).build()
-    }
-    .retrieve()
-    .awaitBody()
+class ReligionMappingApiService(
+  @Qualifier("mappingWebClient") webClient: WebClient,
+  retryApiService: RetryApiService,
+) {
+  private val religionResourceApi = ReligionResourceApi(webClient)
+
+  private val backoffSpec = retryApiService.getBackoffSpec().withRetryContext(
+    Context.of("api", "TransactionNomisApiService"),
+  )
+
+  suspend fun getByCprIds(crpReligionIds: List<String>): List<ReligionMappingDto> = religionResourceApi.getReligionMappingsByCprIds(crpReligionIds).awaitBodyWithRetry(retrySpec = backoffSpec)
 }
