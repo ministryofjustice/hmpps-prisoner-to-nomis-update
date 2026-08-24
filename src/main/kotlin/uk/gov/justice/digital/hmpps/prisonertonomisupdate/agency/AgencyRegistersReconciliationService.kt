87fd9c7655b915743c8edd8fdf3c1b8b0de137e2
Andy Marke <andy.marke@digital.justice.gov.uk>
2026-08-24 11:12:57 +0100

    SDIT-4148 Agency reconciliation (#3151)
    Added multithreading and read of each agency

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/agency/AgencyRegistersReconciliationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/agency/AgencyRegistersReconciliationService.kt
@@ -2,7 +2,15 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.agency
 
 import com.microsoft.applicationinsights.TelemetryClient
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.toList
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
@@ -10,7 +18,9 @@ import uk.gov.justice.digital.hmpps.prisonertonomisupdate.config.trackEvent
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.ReconciliationResult
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.logger
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.services.awaitBoth
+import java.util.concurrent.atomic.AtomicInteger
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Service
 class AgencyRegistersReconciliationService(
   private val telemetryClient: TelemetryClient,
@@ -46,44 +56,69 @@ class AgencyRegistersReconciliationService(
       }
   }
 
-  suspend fun generateAgencyReconciliationReport(): ReconciliationResult<MismatchAgency> {
-    checkTotalsMatch()
+  suspend fun checkMatch(agencyId: String): MismatchAgency? {
+    val nomisAgency = runCatching { nomisApiService.getAgency(agencyId) }.getOrNull()
+    val dpsAgency = runCatching { dpsApiService.getAgency(agencyId) }.getOrNull()
 
-    return ReconciliationResult(
-      mismatches = emptyList(),
-      itemsChecked = 0,
-      pagesChecked = 0,
-    )
-  }
-
-  private suspend fun checkTotalsMatch() = runCatching {
-    val (nomisTotal, dpsTotal) = withContext(Dispatchers.Unconfined) {
-      async { nomisApiService.getAgencyIds().agencyIds.size } to
-        async { dpsApiService.getAgencyIds().agencyIds.size }
-    }.awaitBoth()
-
-    if (nomisTotal != dpsTotal) {
+    return when {
+      nomisAgency == null -> MismatchAgency(agencyId, "Missing in NOMIS")
+      dpsAgency == null -> MismatchAgency(agencyId, "Missing in DPS")
+      else -> null
+    }?.also {
       telemetryClient.trackEvent(
-        "$TELEMETRY_PREFIX-mismatch-totals",
+        "$TELEMETRY_PREFIX-mismatch",
         mapOf(
-          "nomisTotal" to nomisTotal.toString(),
-          "dpsTotal" to dpsTotal.toString(),
+          "agencyId" to it.agencyId,
+          "reason" to it.reason,
         ),
       )
     }
-  }.onFailure {
-    log.error("Unable to get agency totals", it)
-    telemetryClient.trackEvent(
-      "$TELEMETRY_PREFIX-mismatch-totals-error",
-      mapOf(),
-    )
+  }
+
+  suspend fun generateAgencyReconciliationReport(): ReconciliationResult<MismatchAgency> {
+    val (nomisAgencyIds, dpsAgencyIds) = withContext(Dispatchers.Unconfined) {
+      async { nomisApiService.getAgencyIds().agencyIds.map { it.agencyId } } to
+        async { dpsApiService.getAgencyIds().agencyIds.map { it.agencyId } }
+    }.awaitBoth()
+
+    val allIds = (nomisAgencyIds + dpsAgencyIds).toSet()
+
+    return coroutineScope {
+      val itemsCount = AtomicInteger(0)
+      val pagesCount = AtomicInteger(0)
+
+      val mismatchesChannel = Channel<MismatchAgency>(capacity = UNLIMITED)
+      val channel = produce {
+        allIds.forEach { send(it) }
+        channel.close()
+      }
+
+      val jobs = (1L..10).map {
+        launch {
+          for (item in channel) {
+            checkMatch(item)?.also { mismatchesChannel.send(it) }
+            itemsCount.incrementAndGet()
+          }
+        }
+      }
+
+      launch {
+        jobs.joinAll()
+        mismatchesChannel.close()
+      }
+
+      ReconciliationResult(
+        mismatches = mismatchesChannel.toList().also { log.info("Mismatch result: $it") },
+        itemsChecked = itemsCount.get(),
+        pagesChecked = pagesCount.get(),
+      )
+    }
   }
 }
-
 data class MismatchAgency(
   val agencyId: String,
   val reason: String,
 )
 
 private fun List<MismatchAgency>.asMap(): Pair<String, String> = this
-  .sortedBy { it.agencyId }.take(10).let { mismatch -> "agencyIds" to mismatch.map { it.agencyId }.joinToString() }
+  .sortedBy { it.agencyId }.take(10).let { mismatch -> "agencyIds" to mismatch.joinToString { it.agencyId } }
