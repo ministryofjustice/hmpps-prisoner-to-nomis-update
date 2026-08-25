d0c2734bb5b2f024331eb772a99adc1c9c94867b
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-08-25 17:52:33 +0100

    SDIT-4116 Reconcile missing transfers from NOMIS or DPS (#3171)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/transfer/TransferSchedulerReconciliationIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/transfer/TransferSchedulerReconciliationIntTest.kt
@@ -18,8 +18,19 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerDpsApiExtension.Companion.transferSchedulerDpsApiServer
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerDpsApiMockServer.Companion.reconciliation
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerDpsApiMockServer.Companion.transferMovement
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerDpsApiMockServer.Companion.transferSchedule
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerNomisApiMockServer.Companion.offenderTransferMovementsResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerNomisApiMockServer.Companion.transferMovementOutResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer.TransferSchedulerNomisApiMockServer.Companion.transferScheduleOutResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomismappings.model.TransferSchedulerPrisonerMappingIdsDto
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.BookingTransferSchedule
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.transferscheduler.model.ReconciliationTransfer
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.transferscheduler.model.SyncTransfer
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.NomisApiExtension
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.generateOffenderNo
+import java.util.*
 
 class TransferSchedulerReconciliationIntTest(
   @Autowired private val reconciliationService: TransferScheduleReconciliationService,
@@ -92,6 +103,205 @@ class TransferSchedulerReconciliationIntTest(
         isNull(),
       )
     }
+  }
+
+  @Nested
+  inner class AdditionalEntities {
+    private val offender = "A0001TZ"
+
+    @BeforeEach
+    fun setUp() {
+      reset(telemetryClient)
+      nomisPrisonerApi.stubGetAllPrisoners(
+        offenderId = 0,
+        pageSize = 100,
+        prisoners = listOf(generateOffenderNo(sequence = 1)),
+      )
+
+      stubEmptyResponses(offender)
+    }
+
+    @Nested
+    inner class AdditionalEntitiesInNomis {
+
+      @BeforeEach
+      fun `stub additional NOMIS entities and run report`() = runTest {
+        nomisApi.stubGetOffenderTransferMovements(
+          offenderNo = offender,
+          response = offenderTransferMovementsResponse(
+            offenderNo = offender,
+            schedules = listOf(
+              BookingTransferSchedule(
+                schedule = transferScheduleOutResponse(eventId = 123L),
+                movement = transferMovementOutResponse().copy(eventId = 123L, sequence = 3),
+              ),
+            ),
+            unscheduledMovements = listOf(transferMovementOutResponse().copy(transferScheduleOutId = null, sequence = 4)),
+          ),
+        )
+
+        reconciliationService.generateTransferSchedulerReconciliationReportBatch()
+        awaitReportFinished()
+      }
+
+      @Test
+      fun `should report extra NOMIS transfer schedule`() {
+        verify(telemetryClient).trackEvent(
+          eq("transfer-scheduler-reconciliation-mismatch"),
+          eq(
+            mapOf(
+              "offenderNo" to offender,
+              "type" to "SCHEDULE",
+              "nomisCount" to "1",
+              "dpsCount" to "0",
+              "unexpected-nomis-ids" to "[123]",
+              "unexpected-dps-ids" to "[]",
+            ),
+          ),
+          isNull(),
+        )
+      }
+
+      @Test
+      fun `should report extra NOMIS scheduled movement`() {
+        verify(telemetryClient).trackEvent(
+          eq("transfer-scheduler-reconciliation-mismatch"),
+          eq(
+            mapOf(
+              "offenderNo" to offender,
+              "type" to "SCHEDULED_MOVEMENT",
+              "nomisCount" to "1",
+              "dpsCount" to "0",
+              "unexpected-nomis-ids" to "[12345_3]",
+              "unexpected-dps-ids" to "[]",
+            ),
+          ),
+          isNull(),
+        )
+      }
+
+      @Test
+      fun `should report extra NOMIS unscheduled movement`() {
+        verify(telemetryClient).trackEvent(
+          eq("transfer-scheduler-reconciliation-mismatch"),
+          eq(
+            mapOf(
+              "offenderNo" to offender,
+              "type" to "UNSCHEDULED_MOVEMENT",
+              "nomisCount" to "1",
+              "dpsCount" to "0",
+              "unexpected-nomis-ids" to "[12345_4]",
+              "unexpected-dps-ids" to "[]",
+            ),
+          ),
+          isNull(),
+        )
+      }
+    }
+
+    @Nested
+    inner class AdditionalEntitiesInDps {
+      private val dpsScheduleId = UUID.randomUUID()
+      private val dpsScheduledMovementId = UUID.randomUUID()
+      private val dpsUnscheduledMovementId = UUID.randomUUID()
+
+      @BeforeEach
+      fun `stub additional DPS entities and run report`() = runTest {
+        dpsApi.stubGetTransferSchedulerReconciliation(
+          personIdentifier = offender,
+          response = reconciliation(
+            transfers = listOf(
+              ReconciliationTransfer(
+                transfer = SyncTransfer(dpsId = dpsScheduleId, schedule = transferSchedule()),
+                movement = transferMovement(dpsId = dpsScheduledMovementId),
+              ),
+            ),
+            unscheduledMovements = listOf(transferMovement(dpsId = dpsUnscheduledMovementId)),
+          ),
+        )
+
+        reconciliationService.generateTransferSchedulerReconciliationReportBatch()
+        awaitReportFinished()
+      }
+
+      @Test
+      fun `should report extra DPS transfer schedule`() {
+        verify(telemetryClient).trackEvent(
+          eq("transfer-scheduler-reconciliation-mismatch"),
+          eq(
+            mapOf(
+              "offenderNo" to offender,
+              "type" to "SCHEDULE",
+              "nomisCount" to "0",
+              "dpsCount" to "1",
+              "unexpected-nomis-ids" to "[]",
+              "unexpected-dps-ids" to "[$dpsScheduleId]",
+            ),
+          ),
+          isNull(),
+        )
+      }
+
+      @Test
+      fun `should report extra DPS scheduled movement`() {
+        verify(telemetryClient).trackEvent(
+          eq("transfer-scheduler-reconciliation-mismatch"),
+          eq(
+            mapOf(
+              "offenderNo" to offender,
+              "type" to "SCHEDULED_MOVEMENT",
+              "nomisCount" to "0",
+              "dpsCount" to "1",
+              "unexpected-nomis-ids" to "[]",
+              "unexpected-dps-ids" to "[$dpsScheduledMovementId]",
+            ),
+          ),
+          isNull(),
+        )
+      }
+
+      @Test
+      fun `should report extra DPS unscheduled movement`() {
+        verify(telemetryClient).trackEvent(
+          eq("transfer-scheduler-reconciliation-mismatch"),
+          eq(
+            mapOf(
+              "offenderNo" to offender,
+              "type" to "UNSCHEDULED_MOVEMENT",
+              "nomisCount" to "0",
+              "dpsCount" to "1",
+              "unexpected-nomis-ids" to "[]",
+              "unexpected-dps-ids" to "[$dpsUnscheduledMovementId]",
+            ),
+          ),
+          isNull(),
+        )
+      }
+    }
+  }
+
+  private fun stubEmptyResponses(offender: String = "A0001TZ") {
+    nomisApi.stubGetOffenderTransferMovements(
+      offenderNo = offender,
+      response = offenderTransferMovementsResponse(
+        schedules = listOf(),
+        unscheduledMovements = listOf(),
+      ),
+    )
+
+    dpsApi.stubGetTransferSchedulerReconciliation(
+      personIdentifier = offender,
+      response = reconciliation(listOf(), listOf()),
+    )
+
+    mappingApi.stubGetTransferSchedulerPrisonerMappingIds(
+      prisonerNumber = offender,
+      idMappings = TransferSchedulerPrisonerMappingIdsDto(
+        prisonerNumber = offender,
+        schedules = listOf(),
+        movements = listOf(),
+      ),
+    )
   }
 
   private fun awaitReportFinished() {
