4388748f971daab09c20794575ca2fa30d5d2c12
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-09-03 17:04:45 +0100

    SDIT-4109 Upsert transfer schedule client (#3227)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/transfer/TransferSchedulerNomisApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertonomisupdate/movements/transfer/TransferSchedulerNomisApiService.kt
@@ -3,9 +3,13 @@ package uk.gov.justice.digital.hmpps.prisonertonomisupdate.movements.transfer
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrLogAndRethrowBadRequest
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.helpers.awaitBodyOrNullForNotFound
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.api.OffenderTransferMovementsResourceApi
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.api.TransferScheduleResourceApi
 import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.OffenderTransferMovementsResponse
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertTransferScheduleOut
+import uk.gov.justice.digital.hmpps.prisonertonomisupdate.nomisprisoner.model.UpsertTransferScheduleOutResponse
 
 @Service
 class TransferSchedulerNomisApiService(
@@ -13,8 +17,11 @@ class TransferSchedulerNomisApiService(
 ) {
 
   private val offenderApi = OffenderTransferMovementsResourceApi(webClient)
+  private val scheduleApi = TransferScheduleResourceApi(webClient)
 
   suspend fun getOffenderTransferMovementsOrNull(offenderNo: String): OffenderTransferMovementsResponse? = offenderApi.prepare(offenderApi.getOffenderTransferMovementsRequestConfig(offenderNo))
     .retrieve()
     .awaitBodyOrNullForNotFound()
+
+  suspend fun upsertTransferSchedule(offenderNo: String, request: UpsertTransferScheduleOut): UpsertTransferScheduleOutResponse = scheduleApi.upsertTransferScheduleOut(offenderNo, request).awaitBodyOrLogAndRethrowBadRequest()
 }
